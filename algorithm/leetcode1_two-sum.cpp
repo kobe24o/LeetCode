09c3f92d11ce3c7965f1e7faf5973fb8fde61c73
kobe_24o <mingmingchen1989@163.com>
2019-07-24 22:58:51 +0800

    Update leetcode1_two-sum.cpp

--- algorithm/leetcode1_two-sum.cpp
+++ algorithm/leetcode1_two-sum.cpp
@@ -20,3 +20,28 @@ public:
         return ans;
     }
 };
+
+//哈希法
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        int i, d;
+        vector<int> ans;
+        unordered_map<int,int> unomap;//unordered_map是哈希表
+        for(i = 0; i < nums.size(); ++i)//建立哈希表
+            unomap[nums[i]] = i;//哈希数组下标是值，存的原来的下标
+        for(i = 0; i < nums.size(); ++i)
+        {
+            d = target - nums[i];
+            if(unomap.find(d) != unomap.end() && unomap[d]>i)
+            {//如果查找到了，且不是同一个元素
+                ans.push_back(i);
+                ans.push_back(unomap[d]);
+                break;
+            }
+        }
+        return ans;
+    }
+};
