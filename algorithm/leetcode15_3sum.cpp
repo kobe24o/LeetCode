56434cb8eccae2eb40f9e72be10af187b408e5db
kobe_24o <mingmingchen1989@163.com>
2019-08-11 00:14:41 +0800

    Update leetcode15_3sum.cpp

--- algorithm/leetcode15_3sum.cpp
+++ algorithm/leetcode15_3sum.cpp
@@ -3,6 +3,9 @@ class Solution
 public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
+    	vector<vector<int>> ans;
+    	vector<int> 3nums;
+    	nums.sort(nums.begin(), nums.end());
     	
     }
 };
