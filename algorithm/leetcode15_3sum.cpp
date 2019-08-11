1b56dd696987b8985436b0e187d7b5d7893f5d5a
kobe_24o <mingmingchen1989@163.com>
2019-08-12 00:17:47 +0800

    Update leetcode15_3sum.cpp

--- algorithm/leetcode15_3sum.cpp
+++ algorithm/leetcode15_3sum.cpp
@@ -7,16 +7,16 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) 
     {
     	vector<vector<int>> ans;
-    	sort(nums.begin(), nums.end());
-    	int n = nums.size(), i;
+    	sort(nums.begin(), nums.end());//排序
+    	int n = nums.size();
     	if(n < 3||nums.front()>0||nums.back()<0)
     		return ans;
     	int left, right, templeft, tempright, sum;
-    	for(i = 0; i < n-2; ++i)
+    	for(int i = 0; i < n-2; ++i)
     	{
-    		if(nums[i] > 0)
+    		if(nums[i] > 0)//全为正，找不到解了
     			break;
-    		if(i > 0 && nums[i-1] == nums[i])
+    		if(i > 0 && nums[i-1] == nums[i])//相同元素，跳过
                 continue;
     		left = i+1, right = n-1;
     		while(left < right)
@@ -33,11 +33,11 @@ public:
     				tempright = nums[right];
     				while(left < right && templeft == nums[++left])
     				{
-
+                        //跳过相同元素
     				}
     				while(left < right && tempright == nums[--right])
     				{
-
+                        //跳过相同元素
     				}
     			}
     		}
