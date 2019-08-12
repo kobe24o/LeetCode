b92b3b71e3c560a3cd1f7541770eb05cf9e7ca22
kobe_24o <mingmingchen1989@163.com>
2019-08-12 21:16:31 +0800

    updates

--- algorithm/leetcode23_merge-k-sorted-lists.cpp
+++ algorithm/leetcode23_merge-k-sorted-lists.cpp
@@ -1,11 +1,13 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <queue>
+#include <vector>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution 
 {
 public:
@@ -15,18 +17,19 @@ public:
     	{
     		return a->val > b->val;
     	}
-    }
+    };
     ListNode* mergeKLists(vector<ListNode*>& lists) 
     {
         int k = lists.size();
         priority_queue<ListNode*, vector<ListNode*>, cmp> queue;
+        ListNode *head = new ListNode(0);
         for(int i = 0; i < k; ++i)
         {
-        	queue.push_back(lists[i]);
+        	queue.push(lists[i]);
         }
         while(!queue.empty())
         {
-        	
+        	queue.
         }
     }
 };
