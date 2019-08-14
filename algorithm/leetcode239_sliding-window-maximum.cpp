104ca96c1c847a3a2f2640e3aca78abdd6d288d5
kobe_24o <mingmingchen1989@163.com>
2019-08-14 23:54:45 +0800

    Update leetcode239_sliding-window-maximum.cpp

--- algorithm/leetcode239_sliding-window-maximum.cpp
+++ algorithm/leetcode239_sliding-window-maximum.cpp
@@ -1,31 +1,42 @@
 #include <deque>
+#include <vector>
+#include <climits>
+
+using namespace std;
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(nums.size() == 0)
+            return {};
         deque<int> q;
         vector<int> ans;
-        int i, maxnum = INT_MIN;
-        auto it = q.begin();
+        int i;
         for(i = 0; i < k; ++i)
         {
-            q.push(nums[i]);
-            if(nums[i] > maxnum)
-                maxnum = nums[i];
+            while(!q.empty() && nums[i] >= nums[q.back()])
+            {
+                q.pop_back();// <=nums[i]的前面的存的，无意义，删掉
+            }
+            q.push_back(i);
         }
-        ans.push_back(maxnum);
+        ans.push_back(nums[q.front()]);
         for(i = k; i < nums.size(); ++i)
         {
-            
-            q.push(nums[i]);
-            q.erase(q.begin());
-            for(it = q.begin(); it != q.end(); ++it)
+            if(!q.empty() && i-k+1 > q.front())
+                q.pop_front();//过了窗口了，删除
+            while(!q.empty() && nums[i] >= nums[q.back()])
             {
-                maxnum = *it;
-                if(*it > maxnum)
-                    maxnum = *it;
+                q.pop_back();
             }
-            ans.push_back(maxnum);
+            q.push_back(i);
+            ans.push_back(nums[q.front()]);
         }
         return ans;
     }
 };
+int main()
+{
+    Solution s;
+    vector<int> vec = {1,3,-1,-3,5,3,6,7};
+    s.maxSlidingWindow(vec,3);
+}
