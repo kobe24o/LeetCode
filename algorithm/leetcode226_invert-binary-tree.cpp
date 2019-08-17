53f2f10101c40e52119bbc9b50a5e36f65d166dc
kobe_24o <mingmingchen1989@163.com>
2019-08-17 10:58:26 +0800

    Update leetcode226_invert-binary-tree.cpp

--- algorithm/leetcode226_invert-binary-tree.cpp
+++ algorithm/leetcode226_invert-binary-tree.cpp
@@ -1,20 +1,43 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
-class Solution {
+#include <iostream>
+#include <queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+//class Solution {  //DFS
+//public:
+//    TreeNode* invertTree(TreeNode* root) {
+//        if(root)
+//        {
+//            swap(root->left,root->right);
+//            invertTree(root->left);
+//            invertTree(root->right);
+//        }
+//        return root;
+//    }
+//};
+class Solution {//BFS
 public:
-    TreeNode* invertTree(TreeNode* root) {  
-        if(root)
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        TreeNode* temp;
+        q.push(root);
+        while(!q.empty())
         {
-            swap(root->left,root->right);
-            invertTree(root->left);
-            invertTree(root->right);
+            temp = q.front();
+            if(temp)
+            {
+                q.push(temp->left);
+                q.push(temp->right);
+            }
+            swap(temp->left, temp->right);
+            q.pop();
         }
         return root;
     }
