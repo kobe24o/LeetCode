c4867573c7e88bed805565ae41695bab07b40189
kobe_24o <mingmingchen1989@163.com>
2019-08-17 11:16:13 +0800

    Create leetcode104_maximum-depth-of-binary-tree.cpp

---
+++ algorithm/leetcode104_maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,14 @@
+#include <queue>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+    }
+};
