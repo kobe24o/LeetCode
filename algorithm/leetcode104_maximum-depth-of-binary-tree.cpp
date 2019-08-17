f22985748c836135cec4b80f60eb7b2e0544d7de
kobe_24o <mingmingchen1989@163.com>
2019-08-17 12:41:59 +0800

    Update leetcode104_maximum-depth-of-binary-tree.cpp

--- algorithm/leetcode104_maximum-depth-of-binary-tree.cpp
+++ algorithm/leetcode104_maximum-depth-of-binary-tree.cpp
@@ -6,9 +6,52 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+//class Solution {
+//public:
+//    int maxDepth(TreeNode* root) {
+//        int maxcount = 0;
+//        if(root)
+//        {
+//            calcTreeDepth(root, 0, maxcount);
+//        }
+//        return maxcount;
+//    }
+//    void calcTreeDepth(TreeNode* root, int height, int &maxcount)
+//    {
+//        if(root)
+//        {
+//            height++;
+//            if(height > maxcount)
+//                maxcount = height;
+//            if(root->left)
+//                calcTreeDepth(root->left, height, maxcount);
+//            if(root->right)
+//                calcTreeDepth(root->right, height, maxcount);
+//        }
+//    }
+//};
+
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        
+        if(root == NULL)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int maxcount = 0, i, n;
+        while(!q.empty())
+        {
+            ++maxcount;
+            n = q.size();
+            for(i = 0; i < n; ++i)
+            {
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
+        }
+        return maxcount;
     }
 };
