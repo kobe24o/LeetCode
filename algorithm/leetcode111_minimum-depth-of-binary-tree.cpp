896a7b964f7493d1a5963d94f63f35645ecbdbde
kobe_24o <mingmingchen1989@163.com>
2019-08-17 23:32:50 +0800

    Update leetcode111_minimum-depth-of-binary-tree.cpp

--- algorithm/leetcode111_minimum-depth-of-binary-tree.cpp
+++ algorithm/leetcode111_minimum-depth-of-binary-tree.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <climits>
-
+#include <queue>
 using namespace std;
 struct TreeNode {
     int val;
@@ -8,32 +8,59 @@ struct TreeNode {
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-class Solution {
+//class Solution {//DFS
+////public:
+////    int minDepth(TreeNode* root) {
+////        if(root == NULL)
+////            return 0;
+////        int leftHeight, rightHeight;
+////        if(root->left && root->right)
+////        {
+////            leftHeight = minDepth(root->left);
+////            rightHeight = minDepth(root->right);
+////        }
+////        else if(root->left && !root->right)
+////        {
+////            leftHeight = minDepth(root->left);
+////            rightHeight = INT_MAX;
+////        }
+////        else if(!root->left && root->right)
+////        {
+////            leftHeight = INT_MAX;
+////            rightHeight = minDepth(root->right);
+////        }
+////        else
+////        {
+////            leftHeight = rightHeight = 0;
+////        }
+////        return min(leftHeight, rightHeight)+1;
+////    }
+////};
+
+class Solution {//BFS
 public:
     int minDepth(TreeNode* root) {
         if(root == NULL)
             return 0;
-        int leftHeight, rightHeight;
-        if(root->left && root->right)
+        int height = 0, n;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
         {
-            leftHeight = minDepth(root->left);
-            rightHeight = minDepth(root->right);
+            height++;
+            n = q.size();
+            while(n--)
+            {
+                if(!q.front()->left && !q.front()->right)//第一个叶子节点退出
+                    return height;
+                if(q.front()->left)
+                    q.push(q.front()->left);
+                if(q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+            }
         }
-        else if(root->left && !root->right)
-        {
-            leftHeight = minDepth(root->left);
-            rightHeight = INT_MAX;
-        }
-        else if(!root->left && root->right)
-        {
-            leftHeight = INT_MAX;
-            rightHeight = minDepth(root->right);
-        }
-        else
-        {
-            leftHeight = rightHeight = 0;
-        }
-        return min(leftHeight, rightHeight)+1;
+        return height;
     }
 };
 int main()
