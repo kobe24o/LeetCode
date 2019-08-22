a4a50ca606b12ff458431666af346d090354a28c
kobe_24o <mingmingchen1989@163.com>
2019-08-22 23:10:45 +0800

    Update leetcode121_best-time-to-buy-and-sell-stock.cpp

--- algorithm/leetcode121_best-time-to-buy-and-sell-stock.cpp
+++ algorithm/leetcode121_best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,19 @@
+#include <vector>
+using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-
+        if(prices.empty())
+            return 0;
+        int minprice = prices[0], curprofit, maxprofit = 0;
+        for(int i = 1; i < prices.size(); ++i)
+        {
+            if(prices[i] < minprice)
+                minprice = prices[i];
+            curprofit = prices[i] - minprice;
+            if(curprofit > maxprofit)
+                maxprofit = curprofit;
+        }
+        return maxprofit;
     }
 };
