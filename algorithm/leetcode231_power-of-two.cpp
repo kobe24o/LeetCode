8aac575b4c2b195a7d79fd0fd6f3f2e7c88fa7a8
kobe_24o <mingmingchen1989@163.com>
2019-08-26 20:04:54 +0800

    Update leetcode231_power-of-two.cpp

--- algorithm/leetcode231_power-of-two.cpp
+++ algorithm/leetcode231_power-of-two.cpp
@@ -1,9 +1,22 @@
+#include<iostream>
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n <= 0)
             return false;
-        return (n&(n-1) == 0);
+        return (n&(n-1)) == 0;//错误写法 n&(n-1) == 0
     }
 };
+
 int main()
+{
+    Solution s;
+    std::cout << s.isPowerOfTwo(16) << std::endl;
+    int count = 0, num = 8;
+    while(num)
+    {
+        count++;
+        num = num&(num-1);
+    }
+    std::cout << count << std::endl;
+}
