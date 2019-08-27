2cb013eccbe185c1e842e5144a52802ea95fe866
kobe_24o <mingmingchen1989@163.com>
2019-08-27 23:22:13 +0800

    Update leetcode46_permutations.cpp

--- algorithm/leetcode46_permutations.cpp
+++ algorithm/leetcode46_permutations.cpp
@@ -5,10 +5,60 @@ public:
             return {};
         vector<int> row;
         vector<vector<int>> ans;
-        bt(nums, ans);
+        unordered_map<int, int> m;
+        bt(nums, 0, m, row, ans);
+        return ans;
     }
-    void bt(vector<int>& nums, vector<vector<int>>& ans)
+    void bt(vector<int>& nums, int count, unordered_map<int, int> m, vector<int> row, vector<vector<int>>& ans)
     {
-    	
+    	if(count == nums.size())
+        {
+            ans.push_back(row);   
+            return;
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(m.find(i) == m.end())
+            {
+                m[i] = 1;
+                row.push_back(nums[i]);
+                bt(nums,count+1,m,row,ans);
+                row.pop_back();
+                m.erase(i);
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        if(nums.empty())
+            return {};
+        vector<int> row;
+        vector<vector<int>> ans;
+        bool occurred[nums.size()];
+        memset(occurred, 0, sizeof(bool)*nums.size());
+        bt(nums, 0, occurred, row, ans);
+        return ans;
+    }
+    void bt(vector<int>& nums, int count, bool *occurred, vector<int> row, vector<vector<int>>& ans)
+    {
+        if(count == nums.size())
+        {
+            ans.push_back(row);   
+            return;
+        }
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(occurred[i] == false)
+            {
+                occurred[i] = true;
+                row.push_back(nums[i]);
+                bt(nums,count+1,occurred,row,ans);
+                row.pop_back();
+                occurred[i] = false;
+            }
+        }
     }
 };
