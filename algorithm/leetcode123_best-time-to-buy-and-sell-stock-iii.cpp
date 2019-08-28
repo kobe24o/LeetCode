8ad5f38d10510748b60252c597c9307fe60e7bb9
kobe_24o <mingmingchen1989@163.com>
2019-08-28 22:43:23 +0800

    Update leetcode123_best-time-to-buy-and-sell-stock-iii.cpp

--- algorithm/leetcode123_best-time-to-buy-and-sell-stock-iii.cpp
+++ algorithm/leetcode123_best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,47 +1,29 @@
 #include <vector>
 #include <iostream>
-
 using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if(prices.empty())
             return 0;
-        int buy = prices[0], sell, profit = 0, maxProfit = 0, secondMaxProfit = 0;
-        for (int i = 1; i < prices.size(); ++i)
+        int minprice = prices[0], curprofit, maxProfit = 0, secondMaxProfit = 0;
+        for(int i = 1; i < prices.size(); ++i)
         {
-        	if(prices[i] >= prices[i-1])//股票值钱了
-    		{
-    			if(i == prices.size()-1)
-                {
-                    sell = prices[i];
-                    profit = sell - buy;
-                    if(profit >= maxProfit)
-                    {
-                        secondMaxProfit = maxProfit;
-                        maxProfit = profit;
-                    }
-                    else if(profit > secondMaxProfit && profit < maxProfit)
-                    	secondMaxProfit = profit;
-                }
+            if(prices[i] < minprice)
+                minprice = prices[i];
+            curprofit = prices[i] - minprice;
+            if(curprofit >= maxProfit)
+            {
+                secondMaxProfit = maxProfit;
+                maxProfit = curprofit;
             }
-        	else//不值钱了
-        	{
-        		sell = prices[i-1];
-        		profit = sell - buy;
-        		buy = prices[i];
-        		if(profit >= maxProfit)
-                {
-                    secondMaxProfit = maxProfit;
-                    maxProfit = profit;
-                }
-                else if(profit > secondMaxProfit && profit < maxProfit)
-                	secondMaxProfit = profit;
-        	}
+            else if(curprofit > secondMaxProfit && curprofit < maxProfit)
+                secondMaxProfit = curprofit;
         }
         return maxProfit+secondMaxProfit;
     }
 };
+
 
 int main()
 {
