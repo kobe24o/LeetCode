685a1798d0fab481807b2225dd3ef3970e20c323
kobe_24o <mingmingchen1989@163.com>
2019-08-30 23:58:22 +0800

    LeetCode

--- algorithm/leetcode90_subsets-ii.cpp
+++ algorithm/leetcode90_subsets-ii.cpp
@@ -1,26 +1,58 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<int> sub;
         vector<vector<int>> ans;
+        ans.push_back({});
         bt(nums,sub,ans,0);
-       	return ans;
+        return ans;
     }
     void bt(vector<int>& nums,vector<int> sub, vector<vector<int>> &ans, int i)
     {
-    	if(i == nums.size())
-    	{
-    		ans.push_back(sub);
-    		return;
-    	}
-    	if(i > 0 && nums[i-1] == nums[i])
+        for(int idx = i; idx < nums.size(); ++idx)
         {
-            sub.push_back(nums[i]);
-            return;
+            if(idx > i && nums[idx] == nums[idx-1])
+                continue;
+            sub.push_back(nums[idx]);
+            ans.push_back(sub);
+            bt(nums,sub,ans,idx+1);
+            sub.pop_back();
         }
-    	bt(nums,sub,ans,i+1);
-    	sub.push_back(nums[i]);
-    	bt(nums,sub,ans,i+1);
     }
 };
+int main()
+{
+    vector<int> nums = {1,2,2};
+    Solution s;
+    s.subsetsWithDup(nums);
+}
+
+// class Solution {
+// public:
+//     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+//         vector<int> sub;
+//         vector<vector<int>> ans;
+//         ans.push_back({});
+//         sort(nums.begin(), nums.end());
+//         int i, j, left = 0, right;
+//         for(i = 0; i < nums.size(); ++i)
+//         {
+//             if(i != 0 && (nums[i-1] != nums[i]))
+//                 left = 0;
+//             right = ans.size();
+//             for(j = left; j < right; ++j)
+//             {
+//                 sub = ans[j];
+//                 sub.push_back(nums[i]);
+//                 ans.push_back(sub);
+//             }
+//             left = ans.size()-(right-left);
+//         }
+//         return ans;
+//     }
+// };
