2f120473e621d44c485c11128f1cddb9a55c0440
kobe_24o <mingmingchen1989@163.com>
2019-08-31 18:34:30 +0800

    lc

---
+++ algorithm/leetcode146_lru-cache.cpp
@@ -0,0 +1,21 @@
+class LRUCache {
+public:
+    LRUCache(int capacity) {
+        
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
+        
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
--- algorithm/leetcode206_reverse-linked-list.cpp
+++ algorithm/leetcode206_reverse-linked-list.cpp
@@ -6,19 +6,27 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode *prevNode = NULL, *nextNode;
-        while(head)
-        {
-            nextNode = head->next;
-            head->next = prevNode;
-            prevNode = head;
-            head = nextNode;
-        }
-        return prevNode;
-    }
+#include <iostream>
+//class Solution {
+//public:
+//    ListNode* reverseList(ListNode* head) {
+//        ListNode *prevNode = NULL, *nextNode;
+//        while(head)
+//        {
+//            nextNode = head->next;
+//            head->next = prevNode;
+//            prevNode = head;
+//            head = nextNode;
+//        }
+//        return prevNode;
+//    }
+//};
+
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 class Solution {
 public:
@@ -26,8 +34,15 @@ public:
     if (head == NULL || head->next == NULL)
         return head;
     ListNode *p = reverseList(head->next);
-    head->next->next = head;
+    head->next->next = head;//p->next = head;错误写法
     head->next = NULL;
     return p;
     }
 };
+int main()
+{
+    ListNode a(1), b(2), c(3), d(4), e(5);
+    a.next = &b, b.next = &c, c.next = &d, d.next = &e;
+    Solution s;
+    s.reverseList(&a);
+}
