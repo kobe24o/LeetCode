ac04ffe2dc7d87ba33c586d0389ddad32084c099
kobe_24o <mingmingchen1989@163.com>
2019-09-02 23:21:51 +0800

    lc

--- algorithm/leetcode35_search-insert-position.cpp
+++ algorithm/leetcode35_search-insert-position.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
+    	int i;
+        for(i = 0; i < nums.size(); ++i)
+        {
+        	if(target <= nums[i])
+    		{
+        		return i;
+        	}
+        }
+        return i;
+    }
+};
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(target <= nums[0])
+        	return 0;
+        if(target > nums[nums.size()-1])
+        	return nums.size();
+        int i = 0, j = nums.size()-1, mid;
+        while(i <= j)
+        {
+        	mid = i + (j-i)/2;
+        	if(nums[mid] == target)
+        		return mid;
+        	else if(nums[mid] > target)
+        		j = mid-1;
+        	else
+        		i = mid+1;
+        }
+        return j+1;
     }
 };
