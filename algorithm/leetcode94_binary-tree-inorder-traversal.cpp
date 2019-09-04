e38b3f0e28b86917bda3a0c27a3e04c26a5458c6
kobe_24o <mingmingchen1989@163.com>
2019-09-04 19:51:09 +0800

    Update leetcode94_binary-tree-inorder-traversal.cpp

--- algorithm/leetcode94_binary-tree-inorder-traversal.cpp
+++ algorithm/leetcode94_binary-tree-inorder-traversal.cpp
@@ -10,6 +10,37 @@
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-        
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
+    void inorder(TreeNode* root, vector<int> &ans)
+    {
+        if(root == NULL)
+            return;
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> stk;
+        while(root || !stk.empty())
+        {
+            while(root)
+            {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            ans.push_back(root->val);
+            stk.pop();
+            root = root->right;
+        }
+        return ans;
     }
 };
