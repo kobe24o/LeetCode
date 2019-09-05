4fe3ae6e363a4c3cc9d57704291a5247fe605a99
kobe_24o <mingmingchen1989@163.com>
2019-09-05 08:15:22 +0800

    Update leetcode118_pascals-triangle.cpp

--- algorithm/leetcode118_pascals-triangle.cpp
+++ algorithm/leetcode118_pascals-triangle.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-    	vector<vector<int>> ans;
-        vector<int> row;
+    	vector<vector<int>> ans(numRows);
+    	//要使用下标操作，提前写（numRows）
         if(numRows == 0)
         	return ans;
-        else if(numRows == 1)
-    	{
-    		row.push_back(1);
-    		ans.push_back(row);
-    		return ans;
-    	}
-        else if(numRows == 2)
-    	{
-    		row.push_back(1);
-    		ans.push_back(row);
-    		return ans;
-    	}
-        int i, j, sum;
-        for(i = 2; i < numRows; ++i)
+        int i, j;
+        for(i = 0; i < numRows; ++i)
         {
-        	row.clear();
-        	row.push_back(1);
-        	for(j = 1; j < ans[i-1].size(); ++j)
+        	for(j = 0; j <= i; ++j)
         	{
-        		sum = ans[i-1][j-1] + ans[i-1][j];
-        		row.push_back(sum);
+        		if(j == 0 || j == i)
+        			ans[i].push_back(1);
+        		else
+        			ans[i].push_back(ans[i-1][j-1] + ans[i-1][j]);
         	}
-        	row.push_back(1);
-        	ans.push_back(row);
         }
         return ans;
     }
