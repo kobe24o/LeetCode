fba9e06c6c07bc6536506fdb78dbe240c356cd17
kobe_24o <mingmingchen1989@163.com>
2019-09-07 17:31:32 +0800

    lc

--- algorithm/leetcode220_contains-duplicate-iii.cpp
+++ algorithm/leetcode220_contains-duplicate-iii.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        
+        set<long> s;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+        	auto it = s.lower_bound(long(nums[i])-long(t));
+        	if(it != s.end() && *it <= long(nums[i])+long(t))
+        		return true;
+        	else
+        	{
+        		s.insert(nums[i]);
+        		if(s.size() > k)
+        			s.erase(nums[i-k]);
+        	}
+        }
+        return false;
     }
 };
