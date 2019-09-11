abaf406062e8e21fc046d242bfe8ac9dbdde4b8e
kobe24o <mingmingchen1989@163.com>
2019-09-11 17:47:02 +0800

    Update leetcode852_peak-index-in-a-mountain-array.cpp

--- algorithm/leetcode852_peak-index-in-a-mountain-array.cpp
+++ algorithm/leetcode852_peak-index-in-a-mountain-array.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& A) {
-        
+        int left = 1, right = A.size()-2, mid;
+        while(left <= right)
+        {
+        	mid = left + ((right-left)>>1);
+        	if(A[mid]>A[mid-1] && A[mid]>A[mid+1])
+        		return mid;
+        	else if(A[mid] < A[mid+1])
+        		left = mid+1;
+        	else
+        		right = mid-1;
+        }
+        return -1;
     }
 };
