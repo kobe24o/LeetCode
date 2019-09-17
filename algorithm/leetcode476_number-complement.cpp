85ef24981709fa41edf1b726dccdacbf0801d9f2
kobe_24o <mingmingchen1989@163.com>
2019-09-17 20:26:45 +0800

    lc

--- algorithm/leetcode476_number-complement.cpp
+++ algorithm/leetcode476_number-complement.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int findComplement(int num) {
-        
+        int n = 1, num_copy = num;
+        while(num_copy/2)
+        {
+        	++n;
+        	num_copy /= 2;
+        }
+        while(n)
+        {
+        	num ^= (1<<n-1);
+        	n--;
+        }
+        return num;
     }
 };
+int main()
+{
+	Solution s;
+	s.findComplement(5);
+}
