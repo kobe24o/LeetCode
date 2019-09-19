e39ddc72d75c62b7792a00e410a3e1566a25b6fd
kobe24o <mingmingchen1989@163.com>
2019-09-19 18:29:25 +0800

    lc

--- algorithm/leetcode522_longest-uncommon-subsequence-ii.cpp
+++ algorithm/leetcode522_longest-uncommon-subsequence-ii.cpp
@@ -1,3 +1,9 @@
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+using namespace std;
+
 bool cmp(string &a, string &b) 
 {
 	if(a.size() == b.size())
@@ -5,20 +11,20 @@ bool cmp(string &a, string &b)
 		for(int i = 0; i < a.size(); ++i)
 		{
 			if(a[i] != b[i])
-				return a[i] < b[i];
+				return a[i] > b[i];
 		}
 	}
-	return a.size() < b.size();
+	return a.size() > b.size();
 }
 class Solution {
 public:
     int findLUSlength(vector<string>& strs) {
         sort(strs.begin(), strs.end(),cmp);
-        string preStr = strs.back();
+        string preStr = strs.front();
         bool same = false;
-        for(auto it = strs.rbegin()+1; it != strs.rend(); ++it) 
+        for(auto it = strs.begin()+1; it != strs.end(); ++it) 
         {
-        	if(*it != preStr && same == false)
+        	if(*it != preStr && same == false && !subSeq(strs, it))
         		return preStr.size();
         	else if(*it == preStr)
         	{
@@ -27,15 +33,46 @@ public:
         	}
             else if(*it != preStr && same == true)
             {
-                if(it != strs.rend())
+                if(it != strs.end()-1)
                 {
                 	same = false;
                 	preStr = *it;
                 }
                 else
-                	return strs.front().size();
+             
+                	if(!subSeq(strs,it))
+                		return it->size();
+            
             }
         }
-	return -1;
+		return -1;
+    }
+    bool subSeq(vector<string> &strs, vector<string>::iterator it)
+    {
+    	int len = it->size(), i;
+    	bool flag = false;
+    	for(auto idx = strs.begin(); idx != it; ++idx)
+    	{
+    		flag = true;
+    		for(i = 0; i < len; ++i)
+    		{
+    			if(idx->find((*it).substr(i,1)) == string::npos)
+    			{
+    				flag = false;
+    				break;
+    			}
+    			// else
+    			// 	flag = true;
+    		}
+    		if(flag == true)
+    			return flag;
+    	}
+    	return flag;
     }
 };
+ int main()
+ {
+ 	Solution s;
+ 	vector<string> strs = {"aaa","aaa","cb","abc"};
+ 	cout << s.findLUSlength(strs) << endl;
+ }
