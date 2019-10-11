95fcb9a6f131fbe07bc8cd34fe2a09148c7b991b
kobe_24o <mingmingchen1989@163.com>
2019-10-11 22:57:29 +0800

    Update leetcode117_populating-next-right-pointers-in-each-node-ii.cpp

--- algorithm/leetcode117_populating-next-right-pointers-in-each-node-ii.cpp
+++ algorithm/leetcode117_populating-next-right-pointers-in-each-node-ii.cpp
@@ -4,60 +4,60 @@ public:
         if(root == NULL)
         	return root;
         Node *head = root;
-        if(head->left && head->right)
+        if(root->left)
         {
-	        head->left->next = head->right;
-	    	while(head->next)
-	    	{
-	    		if(head->next->left)
-    			{	
-    				head->right->next = head->next->left;
-    				break;
-    			}
-	    		else if(!head->next->left && head->next->right)
-	    		{
-	    			head->right->next = head->next->right;
-	    			break;
-	    		}
-	    		head = head->next;
-	    	}
-	    }
-	    else if(head->left && !head->right)
-	    {
-	    	while(head->next)
-	    	{
-	    		if(head->next->left)
-	    		{
-	    			head->left->next = head->next->left;
-	    			break;
-	    		}
-	    		else if(!head->next->left && head->next->right)
-	    		{
-	    			head->left->next = head->next->right;
-	    			break;
-	    		}
-	    		head = head->next;
-	    	}
-	    }
-	    else if(!head->left && head->right)
-	    {
-	    	while(head->next)
-	    	{
-	    		if(head->next->left)
-	    		{
-	    			head->right->next = head->next->left;
-	    			break;
-	    		}
-	    		else if(!head->next->left && head->next->right)
-	    		{
-	    			head->right->next = head->next->right;
-	    			break;
-	    		}
-	    		head = head->next;
-	    	}
-	    }
-    	connect(root->left);
-    	connect(root->right);
+        	if(root->right)
+        		root->left->next = root->right;
+        	else
+        		root->left->next = findchild(root);
+        }
+        if(root->right)
+        	root->right->next = findchild(root);
+	    connect(root->right);
+    	connect(root->left); 	
+        return root;
+    }
+    Node* findchild(Node* root) 
+    {
+    	if(root->next == NULL)
+    		return NULL;
+    	while(root->next)
+    	{
+    		if(root->next->left)
+    			return root->next->left;
+    		if(root->next->right)
+    			return root->next->right;
+    		root = root->next;
+    	}
+    	return NULL;
+    }
+};
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == NULL || root->left == NULL)
+        	return root;
+        queue<Node*> q;
+        q.push(root);
+        Node *p;
+        int n;
+        while(!q.empty())
+        {
+        	n = q.size();
+        	while(n--)
+        	{
+        		p = q.front();
+        		q.pop();
+        		if(n)
+	        		p->next = q.front();
+        		if(p->left)
+        			q.push(p->left);
+        		if(p->right)
+        			q.push(p->right);
+	        	// p->next = n == 0 ? NULL : q.front();
+        	}	
+        }
         return root;
     }
 };
