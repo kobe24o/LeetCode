958de128e91337b7d377686f39aee2051f58aa9c
kobe_24o <mingmingchen1989@163.com>
2019-10-17 22:40:10 +0800

    Update leetcode779_k-th-symbol-in-grammar.cpp

--- algorithm/leetcode779_k-th-symbol-in-grammar.cpp
+++ algorithm/leetcode779_k-th-symbol-in-grammar.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int kthGrammar(int N, int K) {
-        
+        if (N == 1) 
+            return 0;
+        if (K <= 1 << N-2)
+            return kthGrammar(N-1, K);
+        return kthGrammar(N-1, K - (1 << N-2)) ^ 1;
     }
 };
