bf9d40a35bf4546db1182f8d5ea85cb23f2da5b2
kobe_24o <mingmingchen1989@163.com>
2019-10-18 00:12:21 +0800

    lc

--- algorithm/leetcode95_unique-binary-search-trees-ii.cpp
+++ algorithm/leetcode95_unique-binary-search-trees-ii.cpp
@@ -1,5 +1,11 @@
 #include<vector>
 using namespace std;
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 class Solution {
 public:
     vector<TreeNode*> generateTrees(int n) {
