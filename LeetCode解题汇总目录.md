2dca2b3d0d55dac6716964d8511877496a80b109
kobe_24o <mingmingchen1989@163.com>
2019-10-22 22:57:37 +0800

    lc

--- LeetCode解题汇总目录.md
+++ LeetCode解题汇总目录.md
@@ -152,6 +152,7 @@
 | [LeetCode 322. 零钱兑换（DP）](https://blog.csdn.net/qq_21201267/article/details/99894804) |
 | [LeetCode 328. 奇偶链表（双指针）](https://blog.csdn.net/qq_21201267/article/details/101649623) |
 | [LeetCode 334. 递增的三元子序列](https://blog.csdn.net/qq_21201267/article/details/101275950) |
+| [LeetCode 337. 打家劫舍 III（记忆化+递归）](https://blog.csdn.net/qq_21201267/article/details/102691125) |
 | [LeetCode 344. 反转字符串](https://blog.csdn.net/qq_21201267/article/details/100585634) |
 | [LeetCode 347. 前 K 个高频元素（哈希/优先队列）](https://blog.csdn.net/qq_21201267/article/details/102338389) |
 | [LeetCode 349. 两个数组的交集（哈希）](https://blog.csdn.net/qq_21201267/article/details/100945076) |
--- algorithm/leetcode337_house-robber-iii.cpp
+++ algorithm/leetcode337_house-robber-iii.cpp
@@ -10,19 +10,17 @@ public:
     		return 0;
     	if(m.find(make_pair(root,steal)) != m.end())
     		return m[make_pair(root,steal)];
-    	int noStealLeft = rob(root->left,0);
-    	int noStealRight = rob(root->right,0);
-    	m[make_pair(root->left,0)] = noStealLeft;
-    	m[make_pair(root->right,0)] = noStealRight;
     	if(steal)
-    		return root->val + noStealLeft + noStealLeft;
+		{
+			int val = root->val + rob(root->left,0) + rob(root->right,0);
+			m[make_pair(root,steal)] = val;
+			return val;
+		}
     	else//没有偷root
     	{
-    		int stealLeft = rob(root->left,1);
-    		int stealRight = rob(root->right,1);
-    		m[make_pair(root->left,1)] = stealLeft;
-    		m[make_pair(root->right,1)] = stealRight;
-    		return max(noStealLeft,stealLeft)+max(noStealLeft,stealRight);
+    		int val = max(rob(root->left,0),rob(root->left,1))+max(rob(root->right,0),rob(root->right,1));
+    		m[make_pair(root,steal)] = val;
+			return val;
     	}
     }
 };
