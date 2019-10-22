82e12f9b9752c11007010171768e153c3ec9bc3e
kobe_24o <mingmingchen1989@163.com>
2019-10-22 19:47:47 +0800

    Update leetcode198_house-robber.cpp

--- algorithm/leetcode198_house-robber.cpp
+++ algorithm/leetcode198_house-robber.cpp
@@ -18,3 +18,24 @@ public:
         return dp[n-1];
     }
 };
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size(), i;
+        if(n == 0)
+        	return 0;
+        else if(n == 1)
+        	return nums[0];
+        else if(n == 2)
+        	return max(nums[0],nums[1]);
+        int dp_i, dp_i_2 = nums[0], dp_i_1 = max(nums[0],nums[1]);
+        for(i = 2; i < n; ++i)
+        {
+        	dp_i = max(dp_i_2+nums[i], dp_i_1);
+        	dp_i_2 = dp_i_1;
+        	dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
+};
