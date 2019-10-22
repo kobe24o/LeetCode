dfa5fd070fd4aca6e0cb168811bfdc31282689d9
kobe24o <mingmingchen1989@163.com>
2019-10-22 14:08:22 +0800

    Update leetcode38_count-and-say.cpp

--- algorithm/leetcode38_count-and-say.cpp
+++ algorithm/leetcode38_count-and-say.cpp
@@ -1,6 +1,37 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        
+        string ans("1"), str;
+        int i, count = 0;
+        char prev;
+        while(--n)
+        {
+        	prev = ans[0];
+        	count = 1;
+        	for(i = 1; i < ans.size(); ++i)
+        	{
+        		if(ans[i] == prev)
+        		{
+        			count++;
+        		}
+        		else
+        		{
+        			str.push_back(count+'0');
+        			while(count--)
+        				str.push_back(prev);
+        			prev = ans[i];
+        			count = 1;
+        		}
+        		if(i == ans.size()-1)
+        		{
+        			str.push_back(count+'0');
+        			while(count--)
+        				str.push_back(prev);
+        		}
+        	}
+        	ans = str;
+        	str = "";
+        }
+        return ans;
     }
 };
