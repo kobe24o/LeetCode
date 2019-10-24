8fa74b58c1d833e4f3c7efc68d83e341985e0bc3
kobe_24o <mingmingchen1989@163.com>
2019-10-24 22:21:56 +0800

    Update leetcode240_search-a-2d-matrix-ii.cpp

--- algorithm/leetcode240_search-a-2d-matrix-ii.cpp
+++ algorithm/leetcode240_search-a-2d-matrix-ii.cpp
@@ -1,3 +1,5 @@
+#include<vector>
+using namespace std;
 class Solution {
 	int m,n;
 public:
@@ -15,7 +17,7 @@ public:
     {
     	if(ans)
     		return true;
-    	if(x1<0||x1>=m||x2<0||x2>=m||y1<0||y1>=n||y2<0||y2>=n)
+    	if(x1 > x2 || y1 > y2 ||x1<0||x1>=m||x2<0||x2>=m||y1<0||y1>=n||y2<0||y2>=n)
     		return false;
     	int mx = x1+((x2-x1)>>1);
     	int my = y1+((y2-y1)>>1);
@@ -36,42 +38,55 @@ public:
     }
 };
 
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.size()==0 || matrix[0].size() == 0)
-    		return false;
-        int r = matrix.size(), c = matrix[0].size();
-        int x = r-1, y = 0;
-        while(x>=0 && y<c)
-        {
-        	if(matrix[x][y] == target)
-        		return true;
-        	else if(matrix[x][y] < target)
-        		y++;
-        	else
-        		x--;
-        }
-        return false;
-    }
-};
+int main()
+{
+	vector<vector<int>> m = {
+							  {1,   4,  7, 11, 15},
+							  {2,   5,  8, 12, 19},
+							  {3,   6,  9, 16, 22},
+							  {10, 13, 14, 17, 24},
+							  {18, 21, 23, 26, 30}
+							};
+	Solution s;
+	s.searchMatrix(m,12);
+}
 
-class Solution {
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        if(matrix.size()==0 || matrix[0].size() == 0)
-    		return false;
-        int r = matrix.size(), c = matrix[0].size();
-        int x = 0, y = c-1;
-        while(x<r && y>=0)
-        {
-        	if(matrix[x][y] == target)
-        		return true;
-        	else if(matrix[x][y] < target)
-        		x++;
-        	else
-        		y--;
-        }
-        return false;
-    }
-};
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         if(matrix.size()==0 || matrix[0].size() == 0)
+//     		return false;
+//         int r = matrix.size(), c = matrix[0].size();
+//         int x = r-1, y = 0;
+//         while(x>=0 && y<c)
+//         {
+//         	if(matrix[x][y] == target)
+//         		return true;
+//         	else if(matrix[x][y] < target)
+//         		y++;
+//         	else
+//         		x--;
+//         }
+//         return false;
+//     }
+// };
+
+// class Solution {
+// public:
+//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+//         if(matrix.size()==0 || matrix[0].size() == 0)
+//     		return false;
+//         int r = matrix.size(), c = matrix[0].size();
+//         int x = 0, y = c-1;
+//         while(x<r && y>=0)
+//         {
+//         	if(matrix[x][y] == target)
+//         		return true;
+//         	else if(matrix[x][y] < target)
+//         		x++;
+//         	else
+//         		y--;
+//         }
+//         return false;
+//     }
+// };
