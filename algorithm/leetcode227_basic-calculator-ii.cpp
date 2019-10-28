4614a125063535fe5723077188bd5bd7ec6ab855
kobe24o <mingmingchen1989@163.com>
2019-10-28 10:39:11 +0800

    Create leetcode227_basic-calculator-ii.cpp

---
+++ algorithm/leetcode227_basic-calculator-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> stk;
+        char op = '+';
+        int prev = 0, num = 0, ans = 0;
+        for(int i = 0; i < s.length(); i++) 
+        {
+            if(s[i] == ' ')
+                continue;
+            if(isdigit(s[i]))
+            {
+                num = num * 10 + (s[i] - '0');
+            }
+            if(s[i] < '0' || i == s.size()-1)
+            {
+                if(op == '+')
+                    stk.push(num);
+                else if(op == '-')
+                    stk.push(-num);
+                else if(op == '*')
+                {
+                    prev = stk.top();
+                    stk.pop();
+                    stk.push(prev*num);
+                }
+                else
+                {
+                    prev = stk.top();
+                    stk.pop();
+                    stk.push(prev/num);
+                }
+                op = s[i];
+                num = 0;
+            }
+        }
+        while(!stk.empty());
+        {
+            ans += stk.top();
+            stk.pop();
+        }
+        return ans;
+    }
+};
