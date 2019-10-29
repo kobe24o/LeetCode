a8f4e1b02d57407476575929373ef5c3ede8376f
kobe24o <mingmingchen1989@163.com>
2019-10-29 16:59:05 +0800

    lc

--- algorithm/leetcode126_word-ladder-ii.cpp
+++ algorithm/leetcode126_word-ladder-ii.cpp
@@ -1,8 +1,15 @@
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <climits>
+
+using namespace std;
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        int len = wordList.size(), i, k, n, lv = 0, minLv = INT_MAX, j;
+        int len = wordList.size(), i, k, n, lv = 0, minLv = INT_MAX, j, ansLen;
         unordered_map<string,int> m;
+        bool visited[len] = {false};
         for(i = 0; i < len; ++i)
         {
             m[wordList[i]] = i;
@@ -11,7 +18,6 @@ public:
         }
         if(m.find(endWord) == m.end())
         	return {};
-        bool visited[len] = {false};
         queue<string> q;
         string str;
         vector<vector<string>> ans;
@@ -23,6 +29,7 @@ public:
         {
         	lv++;
         	n = q.size();
+            ansLen = ans.size();
         	while(n--)
         	{
 	        	for(i = 0; i < beginWord.size(); ++i)
@@ -35,27 +42,36 @@ public:
     	        			str[i] = 'a';
     	        		if(m.find(str) != m.end() && !visited[m[str]])
     	        		{
-                            for(auto it = ans.begin(); it != ans.end(); ++it)
+    	        		    // ansLen = ans.size();
+                            for(j = 0; j < ansLen; ++j)
                             {
-                                if(it->back() == q.front())
-                                    it->push_back(str);
-                                if(it->size() > minLv)
-                                    ans.erase(it);
+                                if(ans[j].back() == q.front())
+                                {
+                                    path = ans[j];
+                                    path.push_back(str);
+                                    ans.push_back(path);
+                                }
                             }
     	        			q.push(str);
     	        			visited[m[str]] = true;
     	        			if(str == endWord)
                             {
-                                lv++;
-                                if(lv < minLv)
-                                    minLv = lv; 
+                                if(lv+1 < minLv)
+                                    minLv = lv+1; 
                             }
     	        		}
                     }
 	        	}
 	        	q.pop();
 	        }
+            ans.erase(ans.begin(), ans.begin()+ansLen);
         }
         return ans;
     }
 };
+int main()
+{
+    Solution s;
+    vector<string> v = {"hot","dot","dog","lot","log","cog"};
+    s.findLadders("hit","cog",v);
+}
