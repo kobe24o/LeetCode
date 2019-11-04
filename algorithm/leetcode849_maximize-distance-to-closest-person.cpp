0f150133b57723e5fd11f16ae44fe3cf1097fba1
kobe_24o <mingmingchen1989@163.com>
2019-11-04 19:17:28 +0800

    Create leetcode849_maximize-distance-to-closest-person.cpp

---
+++ algorithm/leetcode849_maximize-distance-to-closest-person.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int N = seats.size(), i, prev = 0, maxDis = -1, mid;
+        for(i = 0; i < N; i++) 
+        {
+        	if(seats[i])//有人
+        	{
+        		mid = (prev+i)/2;
+        		if(seats[prev])
+        			maxDis = max(maxDis,min(mid-prev,i-prev));
+        		else//i是第一个人
+        			maxDis = i;
+        		prev = i;
+        	}
+        	else if(i == N-1 && !seats[i])
+        		maxDis = max(maxDis,i-prev);
+        }
+        return maxDis;
+    }
+};
