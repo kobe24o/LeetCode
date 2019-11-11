06c55e275de6d617a553338cab449d2e1fd48e71
kobe_24o <mingmingchen1989@163.com>
2019-11-11 21:32:36 +0800

    Update leetcode149_max-points-on-a-line.cpp

--- algorithm/leetcode149_max-points-on-a-line.cpp
+++ algorithm/leetcode149_max-points-on-a-line.cpp
@@ -1,47 +1,47 @@
-#include<iostream>
-#include<vector>
-using namespace std;
 class Solution {
 public:
-    int maxPoints(vector<vector<int>>& p) {
-    	if(p.size() <= 2)
-    		return p.size();
-        int i, j, k, count, maxCount = 2;
-        long dx, dy, dX, dY;
-        for(i = 0; i < p.size()-1; ++i)
+    int gcd(int a, int b) //求最大公约数
+    {
+        int g;
+        while(b != 0)
         {
-        	j = i+1;
-        	count = 1;
-        	dx = 0;
-			dy = 0;
-        	while(j < p.size() && dx==0 && dy==0)
-        	{//找到不为重合的点，计算dx，dy
-        		dx = p[j][0]-p[i][0];
-    			dy = p[j][1]-p[i][1];
-                j++;
-        		count++;
-        	}
-        	for(k = 0; k < p.size(); ++k)
-        	{
-        		if(k >= i && k <= j)
-        			continue;//重复的点不再考察
-        		dX = p[k][0]-p[i][0];
-        		dY = p[k][1]-p[i][1];
-        		if(dY*dx == dy*dX)//与选定的直线斜率相同
-        		{
-        			count++;
-        			if(count > maxCount)
-        				maxCount = count;
-        		}
-        	}
+            g = a%b;
+            a = b;
+            b = g;
+        }
+        return a;
+    }
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size(), maxCount = 2, same = 0, count = 0;
+        if (n <= 2) 
+            return n;
+        map<pair<int, int>, int> m;//最简斜率，<dx, dy>, 计数
+        int i, j, dx, dy, g;
+        for(i = 0; i < n; ++i)
+        {
+            m.clear();
+            same = 1;//i点自己
+            count = 0;
+            for(j = 0; j < n; ++j) 
+            {
+                if(j == i)
+                    continue;
+                dx = points[i][0] - points[j][0];
+                dy = points[i][1] - points[j][1];
+                if(dx == 0 && dy == 0) //与i相同的点
+                    ++same;
+                else 
+                {
+                    g = gcd(abs(dx), abs(dy));//求最大公约数，参数取正数
+                    m[{dx/g, dy/g}]++;//经过i点，斜率一样的点，计数+1
+                }
+            }
+            for(auto p : m)//遍历经过i点的所有直线
+                if(p.second > count)//经过i点的所有直线中某个斜率的最多
+                    count = p.second;
+            if (count+same > maxCount) //最多的点数 count+same 在一条直线上
+                maxCount = count + same;
         }
         return maxCount;
     }
 };
-
-int main()
-{
-	vector<vector<int>> v = {{1,1},{1,1},{1,1}};
-	Solution s;
-	cout << s.maxPoints(v);
-}
