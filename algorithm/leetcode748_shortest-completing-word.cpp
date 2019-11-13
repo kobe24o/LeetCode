58fdbf1d11e91c7ff9a25c612ea630f929ff03a9
kobe_24o <mingmingchen1989@163.com>
2019-11-13 19:14:39 +0800

    Create leetcode748_shortest-completing-word.cpp

---
+++ algorithm/leetcode748_shortest-completing-word.cpp
@@ -0,0 +1,48 @@
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
+class Solution {
+public:
+    string shortestCompletingWord(string licensePlate, vector<string>& words) {
+        unordered_map<char,int> m;
+        for(char c : licensePlate)
+        	if(isalpha(c))
+        		m[tolower(c)]++;
+        unordered_map<char,int> wc;
+        int minLen = INT_MAX;
+        bool flag;
+        string ans;
+        for(string word : words)
+        {
+        	wc.clear();
+        	flag = true;
+        	for(char c : word)
+        		if(isalpha(c))
+        			wc[c]++;
+        	for(auto lpChar : m)
+        	{
+        		if(wc.count(lpChar.first) < lpChar.second)
+        		{
+        			flag = false;
+        			break;
+        		}
+        	}
+        	if(flag)
+        	{
+        		if(word.size() < minLen)
+        		{
+        			minLen = word.size();
+        			ans = word;
+        		}
+        	}
+        }
+        return ans;
+    }
+};
+int main()
+{
+	vector<string> v = {"step", "steps", "stripe", "stepple"};
+	Solution s;
+	s.shortestCompletingWord("1s3 PSt",v);
+}
