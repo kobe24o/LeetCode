4590a9cf4bbafc151c6cf5abc35e5e0c6df162b7
kobe24o <mingmingchen1989@163.com>
2019-11-13 16:16:39 +0800

    Update leetcode443_string-compression.cpp

--- algorithm/leetcode443_string-compression.cpp
+++ algorithm/leetcode443_string-compression.cpp
@@ -9,22 +9,19 @@ public:
         	if(chars[i] == prev)//跟前面相等
         	{
         		count++;//计数+1
-        		if(i == n-1)
-        		{
-        			if(count != 1)
-	        		{
-	        			while(count)
-	        			{
-	        				num.push_back(count%10);
-	        				count /= 10;
-	        			}
-	        			for(k = num.size()-1; k >= 0; --k)
-	        				chars[j++] = num[k]+'0';
-	        			// num = "";
-	        		}
+        		if(i == n-1 && count != 1)//是最后一个字符吗？
+        		{	//只有一个字符的话，不需要写数字1
+        			while(count)//将计数的每位拆开
+        			{
+        				num.push_back(count%10);
+        				count /= 10;
+        			}
+        			//将每位，赋值到原数组
+        			for(k = num.size()-1; k >= 0; --k)
+        				chars[j++] = num[k]+'0';
         		}
         	}
-        	else
+        	else//跟前面不相等
         	{
         		if(count != 1)
         		{
@@ -35,14 +32,13 @@ public:
         			}
         			for(k = num.size()-1; k >= 0; --k)
         				chars[j++] = num[k]+'0';
-        			num = "";
+        			num = "";//处理好了，为下次做准备，清空
         		}
         		prev = chars[i];
-        		j++;//跳过第一个字符，不能覆盖
-        		chars[j++] = chars[i];
-        		count = 1;
+        		chars[j++] = chars[i];//上面数字填好了，该字母了
+        		count = 1;//计数重置为1个
         	}
         }
-        return j;
+        return j;//j的位置就是新数组的长度
     }
 };
