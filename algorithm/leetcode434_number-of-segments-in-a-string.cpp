9b2f8501d6df5a707af60fd458ed88c17edebc71
kobe_24o <mingmingchen1989@163.com>
2019-11-13 20:36:45 +0800

    Update leetcode434_number-of-segments-in-a-string.cpp

--- algorithm/leetcode434_number-of-segments-in-a-string.cpp
+++ algorithm/leetcode434_number-of-segments-in-a-string.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int countSegments(string s) {
-        
+        s.push_back(' ');//考虑边界可以简化很多
+        int cnt = 0, i;
+        for(i = 0; i < s.size() - 1; i++) 
+        {
+            if(s[i] != ' ' && s[i+1] == ' ')
+                cnt++;
+        }
+        return cnt;
     }
 };
