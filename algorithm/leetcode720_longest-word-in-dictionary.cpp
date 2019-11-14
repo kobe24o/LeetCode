6555429efb3d4914521bf701505c9ca97947bf43
kobe_24o <mingmingchen1989@163.com>
2019-11-15 00:07:30 +0800

    Update leetcode720_longest-word-in-dictionary.cpp

--- algorithm/leetcode720_longest-word-in-dictionary.cpp
+++ algorithm/leetcode720_longest-word-in-dictionary.cpp
@@ -1,4 +1,4 @@
-class Trie
+class Trie//Trie节点
 {
 public:
 	bool isWord;
@@ -12,16 +12,16 @@ public:
     string longestWord(vector<string>& words) {
     	Trie *root = new Trie();
     	Trie *cur;
-       	for(string str : words)
+       	for(string str : words)//遍历所有单词
        	{
        		cur = root;
-       		for(char ch : str)
+       		for(char ch : str)//遍历所有字符
        		{
        			if(cur->next[ch-'a'] == NULL)
        				cur->next[ch-'a'] = new Trie();
        			cur = cur->next[ch-'a'];
        		}
-       		cur->isWord = true;
+       		cur->isWord = true;//单词结束标志
        	}
        	string temp;
        	cur = root;
@@ -29,24 +29,24 @@ public:
        	for(i = 0; i < 26; ++i)
        	{
        		temp = "";
-       		if(cur->next[i] && cur->next[i]->isWord)	
+       		if(cur->next[i] && cur->next[i]->isWord)//对每个点dfs	
        			dfs(cur->next[i], temp, i);
        	}
        	return ans;
     }
 
-    void dfs(Trie* root, string temp, int i)
+    void dfs(Trie* root, string &temp, int i)
     {
     	if(!root)
     		return;
-    	if(root->isWord)
+    	if(root->isWord)//是结束标志，在字典中
     	{
-    		temp.push_back(i+'a');
+    		temp.push_back(i+'a');//加入该字符
     		if(temp.size() > ans.size())
-       			ans = temp;
+       			ans = temp;//更新更长的单词
     		for(int j = 0; j < 26; ++j)
-    			dfs(root->next[j],temp,j);
-    		temp.pop_back();
+    			dfs(root->next[j],temp,j);//dfs下一个字符
+    		temp.pop_back();//回溯
     	}
     }
 };
