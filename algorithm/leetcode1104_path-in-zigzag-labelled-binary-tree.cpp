289399f158a16b550f39941585fbf4e46af2cda9
kobe_24o <mingmingchen1989@163.com>
2019-11-16 18:12:45 +0800

    lc

--- algorithm/leetcode1104_path-in-zigzag-labelled-binary-tree.cpp
+++ algorithm/leetcode1104_path-in-zigzag-labelled-binary-tree.cpp
@@ -1,23 +1,16 @@
-#include <vector>
-#include<cmath>
-using namespace std;
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
         vector<int> ans;
-        int n = 1;
-        while(pow(2,n-1) < label)
+        int n = 0;
+        while(pow(2,n) <= label)
         	n++;
         while(n--)
         {
         	ans.push_back(label);
-        	label = 3*pow(2,n-1)-1 - (label>>1);
+        	label = 3*pow(2,n-1)-1 - (label>>1);//左右边界和-label/2
         }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-int main()
-{
-	Solution s;
-	s.pathInZigZagTree(14);
-}
---
+++ algorithm/leetcode958_check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        
+    }
+};
