1c71a0a624654db489e3f5cbab31c588d6abe0b0
kobe_24o <mingmingchen1989@163.com>
2019-11-17 22:20:34 +0800

    lc

--- algorithm/leetcode993_cousins-in-binary-tree.cpp
+++ algorithm/leetcode993_cousins-in-binary-tree.cpp
@@ -1,70 +1,71 @@
-class Solution {
+// class Solution {
+// public:
+//     bool isCousins(TreeNode* root, int x, int y) {
+//         queue<TreeNode*> q;
+//         TreeNode *tp;
+//         q.push(root);
+//         int n;
+//         bool xOccur = false, yOccur = false;
+//         while(!q.empty())
+//         {
+//         	n = q.size();
+//         	while(n--)//这个循环内是一层的节点
+//         	{
+// 	        	tp = q.front();
+// 	        	q.pop();
+// 	        	//如果都属于一个父节点，false
+// 	        	if((tp->left && tp->right) && ((tp->left->val == x && tp->right->val == y)
+// 	        		|| (tp->left->val == y && tp->right->val == x)))
+// 	        		return false;
+// 	        	if(tp->val == x)
+// 	        		xOccur = true;
+// 	        	if(tp->val == y)
+// 	        		yOccur = true;
+// 	        	if(tp->left)
+// 	        		q.push(tp->left);
+// 	        	if(tp->right)
+// 	        		q.push(tp->right);
+// 	        }
+// 	        //这一层结束了，检查x，y的出现状态
+// 	        if((xOccur^yOccur) == 1)//只有一个出现过了，说明不在一层
+// 	        	return false;
+// 	        else if(xOccur && yOccur)//都出现了
+// 	        	return true;
+//         }
+//         return false;
+//     }
+// };
+// 
+class Solution {    
+    TreeNode *pX = NULL, *pY = NULL;//x,y节点的父节点
+    int depX = 0, depY = 0;//x,y节点的深度
 public:
     bool isCousins(TreeNode* root, int x, int y) {
-        queue<TreeNode*> q;
-        TreeNode *tp;
-        q.push(root);
-        int n;
-        bool xOccur = false, yOccur = false;
-        while(!q.empty())
-        {
-        	n = q.size();
-        	while(n--)//这个循环内是一层的节点
-        	{
-	        	tp = q.front();
-	        	q.pop();
-	        	//如果都属于一个父节点，false
-	        	if((tp->left && tp->right) && ((tp->left->val == x && tp->right->val == y)
-	        		|| (tp->left->val == y && tp->right->val == x)))
-	        		return false;
-	        	if(tp->val == x)
-	        		xOccur = true;
-	        	if(tp->val == y)
-	        		yOccur = true;
-	        	if(tp->left)
-	        		q.push(tp->left);
-	        	if(tp->right)
-	        		q.push(tp->right);
-	        }
-	        //这一层结束了，检查x，y的出现状态
-	        if((xOccur^yOccur) == 1)//只有一个出现过了，说明不在一层
-	        	return false;
-	        else if(xOccur && yOccur)//都出现了
-	        	return true;
-        }
-        return false;
-    }
-};
-
-class Solution {	
-public:
-    bool isCousins(TreeNode* root, int x, int y) {
-    	if(root->val = x || root->val == y)
-    		return false;
-    	TreeNode *pX = NULL, *pY = NULL;//x,y节点的父节点
-		int depX = 0, depY = 0;//x,y节点的深度
-		bool found = false;
-        findXY(root,x,0,found,pX,depX);
-        found = false;
-        findXY(root,y,0,found,pY,depY);
+        if(root->val == x || root->val == y)
+            return false;
+        findXY(root,x,y,0);
         if((pX != pY) && (depX == depY))
-        	return true;
+            return true;
         return false;
     }
 
-    void findXY(TreeNode* root, int &v, int dep, bool &found, TreeNode* &parent, int &depXY)
+    void findXY(TreeNode* root, int &x, int &y, int dep)
     {
-    	if(root == NULL || found)
-    		return;
-    	if((root->left && root->left->val == v)
-    			|| (root->right && root->right->val == v))
-    	{
-    		parent = root;
-    		depXY = dep;
-    		found = true;
-    		return;
-    	}
-    	findXY(root->left,v,dep+1,found,parent,depXY);
-    	findXY(root->right,v,dep+1,found,parent,depXY);
+        if(root == NULL)
+            return;
+        if((root->left && (root->left->val == x))
+                || (root->right && (root->right->val == x)))
+        {
+            pX = root;
+            depX = dep;
+        }
+        if((root->left && (root->left->val == y))
+                || (root->right && (root->right->val == y)))
+        {
+            pY = root;
+            depY = dep;
+        }
+        findXY(root->left,x,y,dep+1);
+        findXY(root->right,x,y,dep+1);
     }
 };
