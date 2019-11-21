91333735410a46fdf918811e85ff843bfc02a041
unknown <chenmingming@nuctech.com>
2019-11-21 18:05:30 +0800

    Update leetcode925_long-pressed-name.cpp

--- algorithm/leetcode925_long-pressed-name.cpp
+++ algorithm/leetcode925_long-pressed-name.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
-        
+        int i = 0, j = 0;
+        if(name.size() > typed.size())
+        	return false;
+        for( ; i < name.size() && j < typed.size(); ++j)
+        {
+        	if(name[i] == typed[j])
+        		i++;
+        }
+        return (i == name.size());
     }
 };
