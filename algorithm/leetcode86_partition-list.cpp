306a422366eb215022186232513cc7aac00811d1
kobe_24o <mingmingchen1989@163.com>
2019-11-24 19:04:13 +0800

    Update leetcode86_partition-list.cpp

--- algorithm/leetcode86_partition-list.cpp
+++ algorithm/leetcode86_partition-list.cpp
@@ -12,28 +12,27 @@ public:
         }
         if(!head)//全部小于x
         	return emptyHead->next;
+        //不全部小于x，head处是大于等于x的
         ListNode *greatHead = head, *greatEnd = head, *smallEnd = prev;
         ListNode *cur = head->next;
-        greatEnd->next = NULL;
+        greatEnd->next = NULL;//之前没写，链表只有2个节点，出错
         while(cur)
         {
-        	while(cur && cur->val >= x)
+        	while(cur && cur->val >= x)//跳过都是大于等于的
         	{
         		greatEnd->next = cur;
         		greatEnd = cur;
-        		// p = cur;
         		cur = cur->next;
         	}
         	if(cur)//cur存在，其val小于x
         	{
-        		// greatEnd = p;
-        		greatEnd->next = NULL;
-        		smallEnd->next = cur;
+        		greatEnd->next = NULL;//断开大的尾部
+        		smallEnd->next = cur;//接上small段
         		smallEnd = cur;
         		cur = cur->next;
         	}
         }
-        smallEnd->next = greatHead;
+        smallEnd->next = greatHead;//最后把小于x的后面接上后面的
         return emptyHead->next;
     }
 };
