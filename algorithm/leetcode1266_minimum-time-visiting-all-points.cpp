b0956f254c12610246806b240a685aed716e6ef0
kobe24o <mingmingchen1989@163.com>
2019-11-27 17:16:36 +0800

    Update leetcode1266_minimum-time-visiting-all-points.cpp

--- algorithm/leetcode1266_minimum-time-visiting-all-points.cpp
+++ algorithm/leetcode1266_minimum-time-visiting-all-points.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        for()
+    	int i, dx, dy, prex = points[0][0], prey = points[0][1];
+        for(i = 1; i < points.size(); ++i)
+        {
+        	
+        }
     }
 };
