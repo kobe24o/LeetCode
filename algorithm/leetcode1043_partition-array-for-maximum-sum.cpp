cf13e22b5b59adb84601f3b91cada6bc1657bcda
kobe_24o <mingmingchen1989@163.com>
2019-11-27 23:50:39 +0800

    Update leetcode1043_partition-array-for-maximum-sum.cpp

--- algorithm/leetcode1043_partition-array-for-maximum-sum.cpp
+++ algorithm/leetcode1043_partition-array-for-maximum-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int maxSumAfterPartitioning(vector<int>& A, int K) {
-        int i, n = A.size(), Max = A[0];
-        int dp[n] = {0};
-        dp[0] = A[0];
-        for(i = 1; i < n; ++i)
+        int i, j, n = A.size(), Max;
+        int dp[n+1] = {0};
+        for(i = 1; i <= n; ++i)
         {
-        	Max = max(Max,A[i]);
-        	dp[i] = Max*(i+1);
+        	Max = A[i-1];
+        	for(j = 1; j <= K && i-j>=0; ++j)
+        	{
+        		Max = max(Max, A[i-j]);
+        		dp[i] = max(dp[i], dp[i-j]+j*Max);
+        	}
         }
+        return dp[n];
     }
 };
