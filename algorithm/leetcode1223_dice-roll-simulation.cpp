332922bac943043944d6d1479faa55209bb46afe
kobe_24o <mingmingchen1989@163.com>
2019-11-30 14:44:29 +0800

    Create leetcode1223_dice-roll-simulation.cpp

---
+++ algorithm/leetcode1223_dice-roll-simulation.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int dieSimulator(int n, vector<int>& rollMax) {
+        int dp[n][6];//每行代表第i个数，这个数是1-6？存的是可能的种类
+        memset(dp,0, sizeof dp);
+        
+    }
+};
