15ca5561d35549c05657e8e45453cf3447a58aec
kobe_24o <mingmingchen1989@163.com>
2019-12-01 22:33:44 +0800

    Update leetcode82_remove-duplicates-from-sorted-list-ii.cpp

--- algorithm/leetcode82_remove-duplicates-from-sorted-list-ii.cpp
+++ algorithm/leetcode82_remove-duplicates-from-sorted-list-ii.cpp
@@ -5,25 +5,29 @@ public:
         	return head;
         ListNode *emptyHead = new ListNode(0);
         emptyHead->next = head;
+        int count = 1;
         ListNode *end = emptyHead, *prev = head, *cur = head->next;
         while(cur != NULL)
         {
         	if(prev->val != cur->val)
         	{
-        		end->next = prev;
-        		end = prev;
+                if(count == 1)
+        		{
+                    end->next = prev;
+                    end = prev;
+                }
+                count = 1;
         	}
-        	while(cur && prev->val == cur->val)
-        	{
-        		prev = cur;
-        		cur = cur->next;
-        	}
-        	// end->next = cur;
-        	// end = cur;
-        	prev = prev->next;
-        	if(cur)
-        	cur = cur->next;
+        	else
+                count++;
+
+            prev = cur;
+            cur = cur->next;
         }
+        if(count == 1)
+            end->next = prev;
+        else
+            end->next = NULL;
         return emptyHead->next;
     }
 };
