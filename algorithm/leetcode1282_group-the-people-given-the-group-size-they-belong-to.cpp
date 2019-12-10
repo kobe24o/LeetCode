045d3e53dc1a379ffccdac21bf8e2fbc5003cc82
kobe_24o <mingmingchen1989@163.com>
2019-12-10 21:35:50 +0800

    Update leetcode1282_group-the-people-given-the-group-size-they-belong-to.cpp

--- algorithm/leetcode1282_group-the-people-given-the-group-size-they-belong-to.cpp
+++ algorithm/leetcode1282_group-the-people-given-the-group-size-they-belong-to.cpp
@@ -17,3 +17,21 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>> temp(500);
+        vector<vector<int>> ans;
+        for(int i = 0; i < groupSizes.size(); ++i)
+        {
+            temp[groupSizes[i]].push_back(i);
+            if(temp[groupSizes[i]].size() == groupSizes[i])
+            {
+                ans.push_back(temp[groupSizes[i]]);
+                temp[groupSizes[i]].clear();
+            }
+        }
+        return ans;
+    }
+};
