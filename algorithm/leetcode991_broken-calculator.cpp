5dff11f7f3867a12dc464f241c0d33dc0015c251
kobe24o <mingmingchen1989@163.com>
2019-12-11 15:02:12 +0800

    Update leetcode991_broken-calculator.cpp

--- algorithm/leetcode991_broken-calculator.cpp
+++ algorithm/leetcode991_broken-calculator.cpp
@@ -3,6 +3,12 @@ public:
     int brokenCalc(int X, int Y) {
         if(X >= Y)
             return X-Y;
-            
+        int n = 0;
+		while((X<<1) < Y)
+		{
+			X <<= 1;   
+			++n;
+		}
+		return n+1+Y-(X<<1);
     }
 };
