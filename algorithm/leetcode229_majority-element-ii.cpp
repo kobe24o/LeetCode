4ac690d0a4065b8a9f7b5b6adbb4cd1b9992057a
kobe_24o <mingmingchen1989@163.com>
2019-12-27 21:04:53 +0800

    lc

--- algorithm/leetcode229_majority-element-ii.cpp
+++ algorithm/leetcode229_majority-element-ii.cpp
@@ -1,6 +1,66 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
+        int n = nums.size(), cA = 0, cB = 0;
+        int numA, numB, i;
+        for(i = 0; i < n; i++) 
+        {
+        	if(cA == 0 || cB == 0)
+        	{
+        		if(cA == 0 && cB == 0)
+        		{
+        			numA = nums[i];
+        			cA++;
+        		}
+        		else if(cA != 0 && cB == 0)
+        		{
+        			if(numA == nums[i])
+        				cA++;
+        			else
+        			{
+        				numB = nums[i];
+        				cB++;
+        			}
+        		}
+        		else	//cA == 0 && cB != 0
+        		{
+        			if(numB == nums[i])
+        				cB++;
+        			else
+        			{
+        				numA = nums[i];
+        				cA++;
+        			}
+        		}
+        	}
+        	else	// 都不为零
+        	{
+        		if(nums[i] == numA)
+        			cA++;
+        		else if(nums[i] == numB)
+        			cB++;
+        		else
+        		{
+                    cB--;
+                    cA--;
+        		}
+        	}
+        }
+        if(cA == 0 && cB == 0)
+        	return {};
+        int nA=0, nB=0;
+        for(i = 0; i < n; i++)
+        {
+        	if(nums[i] == numA)
+        		nA++;
+        	else if(nums[i] == numB)
+        		nB++;
+        }
+        vector<int> ans;
+        if(nA >= n/3+1)
+        	ans.push_back(numA);
+        if(nB >= n/3+1)
+        	ans.push_back(numB);
+        return ans;
     }
-};
+};s
