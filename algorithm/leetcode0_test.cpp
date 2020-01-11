a4cb0aa73d49ef837ed915a5abd6969468cde2cb
kobe_24o <mingmingchen1989@163.com>
2020-01-11 16:51:50 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -8,30 +8,81 @@
 using namespace std;
 
 class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
 public:
-    vector<string> removeSubfolders(vector<string>& folder) {
-        sort(folder.begin(), folder.end());
-        int i;
-        vector<string> ans(1,folder[0]);
-        bool issub = false;
-        string str = folder[0]+"/";
-        for(i = 1; i < folder.size(); ++i)
+    int shortestBridge(vector<vector<int>>& A) {
+        queue<pair<int,int>> q;
+        queue<pair<int,int>> firstLand;
+        int i, j, n = A.size(), k, x, y, step=0, size;
+        vector<vector<bool>> visited(n,vector<bool> (n, false));
+        for(i = 0; i < n; ++i)
         {
-            while((i < folder.size()) && (folder[i].find(str) != folder[i].npos))
-                i++;
-            if(i < folder.size())
+            for(j = 0; j < n; ++j)
+                if(A[i][j])
+                {
+                    q.push(make_pair(i,j));
+                    firstLand.push(make_pair(i,j));
+                    visited[i][j] = true;
+                    break;
+                }
+        }
+        pair<int,int> tp;
+        while(!firstLand.empty())
+        {
+            tp = firstLand.front();
+            firstLand.pop();
+            for(k = 0; k < 4; ++k)
             {
-                ans.push_back(folder[i]);
-                str = ans.back()+"/";
+                x = tp.first + dir[k][0];
+                y = tp.second + dir[k][1];
+                if(x>=0 && x<n && y>=0 && y<n && A[x][y] && !visited[x][y])
+                {
+                    firstLand.push(make_pair(x,y));
+                    q.push(make_pair(x,y));
+                    visited[x][y] = true;
+                }
             }
+        }
+        bool found = false;
+        while(!q.empty())
+        {
+            size = q.size();
+            while(size--)
+            {
+                tp = q.front();
+                q.pop();
+                for(k = 0; k < 4; ++k)
+                {
+                    x = tp.first + dir[k][0];
+                    y = tp.second + dir[k][1];
+                    if(x>=0 && x<n && y>=0 && y<n && !visited[x][y])
+                    {
+                        if(A[x][y] == 0)
+                        {
+                            q.push(make_pair(x,y));
+                            visited[x][y] = true;
+                        }
+                        else
+                        {
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+                if(found)
+                    break;
+            }
+            step++;
+            if(found)
+                break;
 
         }
-        return ans;
+        return step-1;
     }
 };
 int main()
 {
+    vector<vector<int>> m = {{0,1,0,0,0},{0,1,0,1,1},{0,0,0,0,1},{0,0,0,0,0},{0,0,0,0,0}};
     Solution s;
-    vector<string> v{"/a","/a/b","/c/d","/c/d/e","/c/f"};
-    s.removeSubfolders(v);
+    s.shortestBridge(m);
 }
