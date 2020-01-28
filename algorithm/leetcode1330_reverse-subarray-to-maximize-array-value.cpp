f3608d19ddb9df7b6aa62c388ff1368be670adcd
kobe_24o <mingmingchen1989@163.com>
2020-01-28 23:13:37 +0800

    Update leetcode1330_reverse-subarray-to-maximize-array-value.cpp

--- algorithm/leetcode1330_reverse-subarray-to-maximize-array-value.cpp
+++ algorithm/leetcode1330_reverse-subarray-to-maximize-array-value.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int maxValueAfterReverse(vector<int>& nums) {
-        int i, j, n=nums.size(), val=0, maxval=INT_MIN, len;
+        int i, j, n = nums.size(), val = 0, maxval = 0;
         for(i = 0; i < n-1; i++)
             val += abs(nums[i]-nums[i+1]);
-        for(len =2; len < n; ++len)
+        if(n <= 2)
+            return val;
+        maxval = val;
+        for(i = 0; i < n; ++i)
         {
-            for(i = 0; i+len < n; ++i)
-            {
-                if(i == 0)
-                    maxval = max(maxval,val-abs(nums[i+len]-nums[i+len-1])+abs(nums[i+len]-nums[i]));
-                else if(i+len == n)
-                    maxval = max(maxval,val-abs(nums[i]-nums[i-1])+abs(nums[i+len]-nums[i]));
-                else
-                    maxval = max(maxval,val-abs(nums[i]-nums[i-1])-abs(nums[i+len]-nums[i+len-1])+abs(nums[i+len]-nums[i])+abs(nums[i+len-1]-nums[i-1]));
-            }
+            if(i != n-1)
+                maxval = max(maxval, val+abs(nums[0]-nums[i+1])-abs(nums[i]-nums[i+1]));//左边界0
+            if(i != 0)
+                maxval = max(maxval, val+abs(nums[i-1]-nums[n-1])-abs(nums[i]-nums[i-1]));//右边界n-1
         }
+        int max1 = INT_MIN, min1 = INT_MAX;
+        int max2 = INT_MIN, min2 = INT_MAX;
+        int max3 = INT_MIN, min3 = INT_MAX;
+        int max4 = INT_MIN, min4 = INT_MAX;
+        for(i = 1; i < n-1; ++i)
+        {
+            max1 = max(max1, nums[i-1]+nums[i]-abs(nums[i]-nums[i-1]));
+            max2 = max(max2, nums[i-1]-nums[i]-abs(nums[i]-nums[i-1]));
+            max3 = max(max3, -nums[i-1]+nums[i]-abs(nums[i]-nums[i-1]));
+            max4 = max(max4, -nums[i-1]-nums[i]-abs(nums[i]-nums[i-1]));
+            min1 = min(min1, nums[i-1]+nums[i]+abs(nums[i]-nums[i-1]));
+            min2 = min(min2, nums[i-1]-nums[i]+abs(nums[i]-nums[i-1]));
+            min3 = min(min3, -nums[i-1]+nums[i]+abs(nums[i]-nums[i-1]));
+            min4 = min(min4, -nums[i-1]-nums[i]+abs(nums[i]-nums[i-1]));
+        }
+        int maxdiff = max(INT_MIN, max(max1-min1, max(max2-min2, max(max3-min3, max4-min4))));
+        maxval = max(maxval, val+maxdiff);
         return maxval;
     }
 };
