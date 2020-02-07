86e3a9ff0511adf922708ac6cd7970e63fd34d4b
kobe_24o <mingmingchen1989@163.com>
2020-02-08 00:29:22 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -7,29 +7,90 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include<set>
-#include<vector>
-#include<iostream>
-using namespace std;
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& mat) {
+        if(mat.empty())
+            return 0;
+        int i, j, minL, maxR, maxarea = 0;
+        int r = mat.size(), c = mat[0].size();
+        vector<vector<int>> left(r,vector<int>(c,0));
+        vector<vector<int>> right(r,vector<int>(c,c));
+        vector<vector<int>> height(r,vector<int>(c,0));
+        for(i = 0; i < r; i++)
+        {
+            //填写left，相连的1,先到最高，然后最左侧的下标
+            minL = 0;
+            for(j = 1; j < c; j++)
+            {
+                if(i == 0)
+                {
+                    if(mat[i][j] == 1)
+                    {
+                        if(mat[i][j-1] == 0)
+                            minL = j;
+                        left[i][j] = minL;
+                    }
+                }
+                else
+                {
+                    if(mat[i][j] == 1)
+                    {
+                        if(mat[i][j-1] == 0)
+                            minL = j;
+                        left[i][j] = max(minL,left[i-1][j]);
+                    }
+                }
+            }
+
+            maxR = c;
+            for(j = c-2; j >= 0; j--)
+            {
+                if(i == 0)
+                {
+                    if(mat[i][j] == 1)
+                    {
+                        if(mat[i][j+1] == 0)
+                            maxR = j+1;
+                        right[i][j] = maxR;
+                    }
+                }
+                else
+                {
+                    if(mat[i][j] == 1)
+                    {
+                        if(mat[i][j+1] == 0)
+                            maxR = j+1;
+                        right[i][j] = min(maxR,right[i-1][j]);
+                    }
+                }
+            }
+
+            for(j = 0; j < c; j++)
+            {
+                if(i == 0)
+                {
+                    if(mat[i][j] == 1)
+                        height[i][j] = 1;
+                }
+                else
+                {
+                    if(mat[i][j] == 1)
+                        height[i][j] = 1+height[i-1][j];
+                }
+            }
+        }
+
+        for(i = 0; i < r; i++)
+        {
+            for(j = 0; j < c; j++)
+                maxarea = max(maxarea, (right[i][j]-left[i][j])*height[i][j]);
+        }
+        return maxarea;//返回最大面积
+    }
+};
 int main()
 {
-    int N, n, i=0, ans;
-    cin >> N;
-    n = N;
-    vector<int> arr(N);
-    while(n--)
-        cin >> arr[i++];
-    set<int> s;
-    for(i = 0; i < N; ++i)
-        s.insert(arr[i]);
-    i = 0;
-    for(auto& it : s)
-    {
-        i++;
-        ans = it;
-        if(i == 3)
-            break;
-    }
-    cout << ans;
-    return ans;
+    Solution s;
+    vector<vector<int>> v = {};
 }
--- algorithm/leetcode85_maximal-rectangle.cpp
+++ algorithm/leetcode85_maximal-rectangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& mat) {
-        if(matrix.empty())
+        if(mat.empty())
             return 0;
         int i, j, minL, maxR, maxarea = 0;
         int r = mat.size(), c = mat[0].size();
@@ -14,22 +14,22 @@ public:
             minL = 0;
             for(j = 1; j < c; j++)
             {
-                if(i == 0)
+                if(i == 0)//第一行
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                     {
-                        if(mat[i][j-1]==0)
-                            minL = j;
+                        if(mat[i][j-1] == '0')
+                            minL = j;//左边0，当前1，需要更新最左边的边界minL
                         left[i][j] = minL;
                     }
                 }
-                else
+                else//剩余行
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                     {
-                        if(mat[i][j-1]==0)
+                        if(mat[i][j-1] == '0')
                             minL = j;
-                        left[i][j] = max(minL,left[i-1][j]);
+                        left[i][j] = max(minL,left[i-1][j]);//跟上面的行，比较，取大
                     }
                 }
             }
@@ -37,44 +37,43 @@ public:
             maxR = c;
             for(j = c-2; j >= 0; j--)
             {
-                if(i == 0)
+                if(i == 0)//第一行
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                     {
-                        if(mat[i][j+1]==0)
-                            maxR = j;
+                        if(mat[i][j+1] == '0')
+                            maxR = j+1;//右边0，当前1，更新最右边的边界maxR
                         right[i][j] = maxR;
                     }
                 }
-                else
+                else//其余
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                     {
-                        if(mat[i][j+1]==0)
-                            maxR = j;
-                        right[i][j] = min(maxR,right[i-1][j]);
+                        if(mat[i][j+1] == '0')
+                            maxR = j+1;
+                        right[i][j] = min(maxR,right[i-1][j]);//还要更上面的比较，取小
                     }
                 }
             }
 
             for(j = 0; j < c; j++)
             {
-                if(i == 0)
+                if(i == 0)//第一行
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                         height[i][j] = 1;
                 }
-                else
+                else//剩余
                 {
-                    if(mat[i][j] == 1)
+                    if(mat[i][j] == '1')
                         height[i][j] = 1+height[i-1][j];
                 }
             }
-        }
 
-        for(i = 0; i < r; i++)
             for(j = 0; j < c; j++)
                 maxarea = max(maxarea, (right[i][j]-left[i][j])*height[i][j]);
+        }
         return maxarea;//返回最大面积
     }
 };
