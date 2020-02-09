ea4b4b5196dd2d18fcbd835a7c43f3719499d372
kobe_24o <mingmingchen1989@163.com>
2020-02-09 17:58:32 +0800

    Update leetcode0_test.cpp

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -64,16 +64,9 @@ public:
 };
 int main()
 {
-    Solution s;
-    ListNode *n1 = new ListNode(1);
-    ListNode *n2 = new ListNode(2);
-    ListNode *n3 = new ListNode(3);
-    ListNode *n4 = new ListNode(4);
-    ListNode *n5 = new ListNode(5);
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    cout <<  s.reverseKGroup(n1,2);
-    cout << " ";
+//    Solution s;
+    multimap<int,int> m;
+    m.insert(make_pair(0,1));
+//    m.insert(make_pair(0,1));
+    cout << m.size();
 }
