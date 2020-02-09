1f9c108b13157f2c9010fa7696640b3103324c04
kobe_24o <mingmingchen1989@163.com>
2020-02-09 16:47:07 +0800

    Update leetcode25_reverse-nodes-in-k-group.cpp

--- algorithm/leetcode25_reverse-nodes-in-k-group.cpp
+++ algorithm/leetcode25_reverse-nodes-in-k-group.cpp
@@ -80,46 +80,48 @@ public:
         ListNode *emptyHead = new ListNode(-1);
         emptyHead->next = head;
         ListNode *prev = emptyHead, *start = emptyHead->next, *end = emptyHead;
+        //start，end，对应k个节点的首尾，prev前一段的末尾
         ListNode *temp=NULL;
         bool flag = false;
         int count;
         while(end)
         {
             count = k;
-            start = prev->next;
-            while(end && count)
+            start = prev->next;//前一段的下一个为start
+            while(end && count)//end移动k次到末尾
             {
                 end = end->next;
                 count--;
             }
-            if(!end)//
-                flag = true;//这一段不足k个，提前结束了,allEnd是结尾
-            else
+            if(!end)//如果end为空，说明，不够k个
+                flag = true;//这一段不足k个，提前结束了
+            else//够k个，需要反转
             {
-                temp = end->next;
-                end->next = NULL;
+                temp = end->next;//temp为下次反转的段的开头
+                end->next = NULL;//断开与下段的链接
             }
-            if(!flag)
+            if(!flag)//没有提前结束，这段够k个，进行反转
             {
                 prev->next = reverseList(start);
+                //前一段的next接上新的头
                 start->next = temp;
-                prev = start;
-                end = start;
+                //原来的头start变成尾巴，接上下一段的头temp
+                prev = start;//更新prev为反转好的尾巴start
+                end = start;//反转k个后，原来的end不是结尾了，end更新为新的结尾start
             }
         }
         return emptyHead->next;
     }
 
     ListNode* reverseList(ListNode *head)
-    {
+    {   //反转链表，返回新的头结点
         ListNode *prev = NULL, *nt = head->next;
         while(head && head->next)
         {
             head->next = prev;
             prev = head;
             head = nt;
-            if(nt)
-                nt = nt->next;
+            nt = nt->next;
         }
         head->next = prev;
         return head;
