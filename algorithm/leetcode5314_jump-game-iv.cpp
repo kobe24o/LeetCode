a70ed286dffdd103ae99974b303c449b0a3ac738
kobe_24o <mingmingchen1989@163.com>
2020-02-09 17:19:59 +0800

    Update leetcode5314_jump-game-iv.cpp

--- algorithm/leetcode5314_jump-game-iv.cpp
+++ algorithm/leetcode5314_jump-game-iv.cpp
@@ -3,57 +3,64 @@ public:
     int minJumps(vector<int>& arr) {
         if(arr.size() == 1)
             return 0;
-        int i, tp, prev = arr[0];
+        int i, tp, prev = INT_MIN, step = 0, size;
         const int n = arr.size();
-        vector<int> dp(n, INT_MAX);
         queue<int> q;
         vector<bool> visited(n,false);
         q.push(0);
         visited[0] = true;
-        dp[0] = 0;
         multimap<int, int> m;
         for(i = 0; i < n; i++)
         {
             if(arr[i] == prev)
-                continue;
-            else
-            {
-                m.insert(make_pair(prev, i-1));
+                continue;//跳过一样的
+            else//只插入连续相同的一段的首尾，中间跳肯定不是最短距离
+            {  
+                if(i-1>=0)
+                    m.insert(make_pair(arr[i-1],i-1));
+                m.insert(make_pair(arr[i], i));
                 prev = arr[i];
             }
+            if(i == n-1)//插入最后一个位置，可能重复插入了，但是不影响
+                m.insert(make_pair(arr[i],i));
         }
-        m.insert(make_pair(arr[n-1],n-1));
-        while(!q.empty() && (dp[n-1] == INT_MAX))
+
+        while(!q.empty())
         {
-            tp = q.front();
-            q.pop();
-            if(tp+1 < n && !visited[tp+1])
+            size = q.size();
+            while(size--)
             {
-                dp[tp+1] = min(dp[tp+1], 1+dp[tp]);
-                q.push(tp+1);
-                visited[tp+1] = true;
-            }
-            if(tp-1>0 && !visited[tp-1])
-            {
-                dp[tp-1] = min(dp[tp-1], 1+dp[tp]);
-                q.push(tp-1);
-                visited[tp-1] = true;
-            }
-            if(dp[n-1] != INT_MAX)
-                return dp[n-1];
-            auto start = m.equal_range(arr[tp]).first, end = m.equal_range(arr[tp]).second;
-            for(auto it = start; it != end; ++it)
-            {
-                if(!visited[it->second])
+                tp = q.front();
+                q.pop();
+                if(tp == n-1)
+                    return step;
+
+                // 跳往i+1位置
+                if(tp+1 < n && !visited[tp+1])
                 {
-                    visited[it->second] = true;
-                    dp[it->second] = min(dp[it->second], 1+dp[tp]);
-                    q.push(it->second);
+                    q.push(tp+1);
+                    visited[tp+1] = true;
                 }
-                if(dp[n-1] != INT_MAX)
-                    return dp[n-1];
+                // 跳往i-1位置
+                if(tp-1>0 && !visited[tp-1])
+                {
+                    q.push(tp-1);
+                    visited[tp-1] = true;
+                }
+                
+                // 跳往值相同的位置
+                auto start = m.equal_range(arr[tp]).first, end = m.equal_range(arr[tp]).second;
+                for(auto it = start; it != end; ++it)
+                {
+                    if(!visited[it->second])
+                    {
+                        visited[it->second] = true;
+                        q.push(it->second);
+                    }
+                }
             }
+            step++;
         }
-        return dp[n-1];
+        return step;
     }
 };
