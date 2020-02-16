925806e2563ec64d0dd8c18b0d7ead7f79b120ed
kobe_24o <mingmingchen1989@163.com>
2020-02-17 01:07:47 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -8,45 +8,32 @@
 using namespace std;
 class Solution {
 public:
-    int maxEvents(vector<vector<int>>& events) {
-        sort(events.begin(), events.end(),[](vector<int>& a, vector<int>& b)
-        		{
-        			if(a[0] == b[0])
-        				return a[1] < b[1];
-        			return a[0] < b[0];
-        		});
-        int i, j, count = 0, attendTime = 0;
-        for(i = 0; i < events.size(); ++i)
+    bool isPossible(vector<int>& target) {
+        long sum = 0, s, i, num, tp, scale;
+        priority_queue<long> q;//默认大顶堆
+        for(i = 0; i < target.size(); ++i)
         {
-        	if(attendTime < events[i][0])
-        	{
-        		attendTime = events[i][0];
-        		count++;
-        		attendTime++;
-        	}
-        	else if(attendTime <= events[i][1])
-        	{
-        		for(j = i+1; j < events.size() && events[i][1] <= events[j][1]; ++j)
-        			;
-        		if(j < events.size() && attendTime >= events[j][0])
-        		{
-        			count++;
-        			events.erase(events.begin()+j);
-        			attendTime++;
-        			i--;
-        			continue;
-        		}
-                count++;
-        		attendTime++;
-        	}
+            sum += target[i];//总和
+            q.push(target[i]);
         }
-        return count;
+        while(q.top() != 1)
+        {
+            tp = q.top();
+            q.pop();
+            s = sum-tp;//剩余的和
+            scale = (tp-q.top()-1)/s;
+            num = tp-scale*s;//栈顶-s，应该为1或者比1大的数
+            if(num < 1)//小于1则false
+                return false;
+            q.push(num);
+            sum -= scale*s;//和减少了s
+        }
+        return true;
     }
 };
 int main()
 {
     Solution s;
-    vector<vector<int>> v = {{1,1},{1,2},{3,3},{1,5},{1,5}};
-    sort(v.rbegin(),v.rend());
-    cout << s.maxEvents(v);
+    vector<int> v = {10000000,1};
+    cout << s.isPossible(v);
 }
--- algorithm/leetcode5343_construct-target-array-with-multiple-sums.cpp
+++ algorithm/leetcode5343_construct-target-array-with-multiple-sums.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        int i, num;
-        long sum = 0, s;
-        priority_queue<int> q;//默认大顶堆
+        long sum = 0, s, i, num, tp, scale;
+        priority_queue<long> q;//默认大顶堆
         for(i = 0; i < target.size(); ++i)
         {
-        	sum += target[i];
+        	sum += target[i];//总和
         	q.push(target[i]);
         }
         while(q.top() != 1)
         {
-        	s = sum-q.top();
-        	num = q.top()-s;
-        	if(num < 1)
-        		return false;
+        	tp = q.top();
         	q.pop();
+        	s = sum-tp;//剩余的和
+        	scale = (tp-q.top()-1)/(s+1);
+        	num = tp-scale*s;//栈顶-s，应该为1或者比1大的数
+        	if(num < 1)//小于1则false
+        		return false;
         	q.push(num);
-        	sum -= s;
+        	sum -= s;//和减少了s
         }
         return true;
     }
