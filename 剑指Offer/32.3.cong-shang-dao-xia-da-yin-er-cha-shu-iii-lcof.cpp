f23dc8da205ea9733b7eb9636862f4798c0f79ab
kobe_24o <mingmingchen1989@163.com>
2020-02-17 14:00:38 +0800

    Update 32.3.cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof.cpp

--- 剑指Offer/32.3.cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof.cpp
+++ 剑指Offer/32.3.cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof.cpp
@@ -1,6 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        if(!root)
+        	return {};
+        queue<TreeNode*> q;
+        TreeNode *tp;
+        q.push(root);
+        stack<TreeNode*> s;
+        int size;
+        vector<int> lv;
+        vector<vector<int>> ans;
+        bool flag = false;
+        while(!q.empty())
+        {
+        	size = q.size();
+        	flag = !flag;
+        	while(size--)
+        	{
+        		tp = q.front();
+        		q.pop();
+        		lv.push_back(tp->val);
+        		if(flag)
+        		{
+        			if(tp->left)  s.push(tp->left);
+        			if(tp->right) s.push(tp->right);
+        		}
+        		else
+        		{
+        			if(tp->right) s.push(tp->right);
+        			if(tp->left)  s.push(tp->left);	
+        		}
+        	}
+        	
+    		while(!s.empty())
+    		{
+    			q.push(s.top());
+    			s.pop();
+    		}
+        	ans.push_back(lv);
+        	lv.clear();
+        }
+        return ans;
     }
 };
