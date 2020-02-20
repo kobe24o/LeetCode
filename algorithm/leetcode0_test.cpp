e6a6cd060a9cac523cb267f9953dfd0a52883b17
kobe_24o <mingmingchen1989@163.com>
2020-02-20 14:42:15 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -8,45 +8,36 @@
 using namespace std;
 class Solution {
 public:
-	int n;
-	vector<string> ans;
-	string str;
-	int num = 0;
-public:
-    vector<string> permutation(string s) {
-        sort(s.begin(), s.end());
-        n = s.size();
-    	vector<bool> visited(n,false);
-        bt(s,0,visited);
+    string reverseWords(string s) {
+        s.push_back(' ');//处理边界方便
+        int i, n = s.size();
+        string ans;
+        stack<string> stk;
+        for(i = 0; i < n; ++i)
+        {
+            if(s[i] != ' ')
+                ans += s[i];
+            else// (s[i]==' ')
+            {
+                if(ans.size())
+                {
+                    stk.push(ans);
+                    ans.clear();
+                }
+            }
+        }
+        while(!stk.empty())
+        {
+            ans += stk.top()+' ';
+            stk.pop();
+        }
+        ans.pop_back();
         return ans;
-    }
-
-    void bt(string& s, int count, vector<bool>& visited)
-    {
-        num++;
-    	if(count == n)
-    	{
-    		ans.push_back(str);
-    		return;
-    	}
-    	for(int i = 0; i < n; ++i)
-    	{
-    		if(!visited[i])
-    		{
-    			if(i != 0 && s[i-1] == s[i] && visited[i-1])
-    				continue;
-    			visited[i] = true;
-    			str.push_back(s[i]);
-    			bt(s,count+1,visited);
-    			str.pop_back();
-    			visited[i] = false;
-    		}
-    	}
     }
 };
 int main()
 {
     Solution s;
-    s.permutation("iaaa");
-    cout << s.num;
+    s.reverseWords("the sky is blue");
+
 }
--- 剑指Offer/58.1.fan-zhuan-dan-ci-shun-xu-lcof.cpp
+++ 剑指Offer/58.1.fan-zhuan-dan-ci-shun-xu-lcof.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        
+        s.push_back(' ');//处理边界方便
+        int i, n = s.size();
+        string ans;
+        stack<string> stk;
+        for(i = 0; i < n; ++i)
+        {
+        	if(s[i] != ' ')
+        		ans += s[i];
+        	else// (s[i]==' ')
+        	{
+        		if(ans.size())
+        		{
+        			stk.push(ans);
+        			ans.clear();
+        		}
+        	}
+        }
+        while(!stk.empty())
+        {
+        	ans += stk.top()+" ";
+        	stk.pop();
+        }
+        ans.pop_back();
+        return ans;
     }
 };
