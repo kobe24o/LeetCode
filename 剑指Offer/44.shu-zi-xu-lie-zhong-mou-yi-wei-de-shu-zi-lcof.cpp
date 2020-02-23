f535a25c770dd9ea3bd24030706f63178cd58edc
kobe_24o <mingmingchen1989@163.com>
2020-02-23 08:14:26 +0800

    Update 44.shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof.cpp

--- 剑指Offer/44.shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof.cpp
+++ 剑指Offer/44.shu-zi-xu-lie-zhong-mou-yi-wei-de-shu-zi-lcof.cpp
@@ -3,15 +3,13 @@ public:
     int findNthDigit(int n) {
     	if(n < 10)
     		return n;
-    	int count = 10, i = 2, bits = 10;
+    	int count = 10, i = 2, num = 9;
     	while(count+9*pow(10,i-1)*i <= n)
     	{
     		count += 9*pow(10,i-1)*i;
+    		num += 9*pow(10,i-1);
     		i++;
     	}
-    	while(count+i <= n)
-    	{
-
-    	}
+    	
     }
 };
