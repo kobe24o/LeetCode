4baff862546aa0c5b9f1bee3883061be990ccf47
kobe_24o <mingmingchen1989@163.com>
2020-02-23 22:20:13 +0800

    lc

--- algorithm/leetcode5172_largest-multiple-of-three.cpp
+++ algorithm/leetcode5172_largest-multiple-of-three.cpp
@@ -1,102 +1,75 @@
 class Solution {
 public:
     string largestMultipleOfThree(vector<int>& digits) {
-    	int count[10] = {0}, i, n;
-    	for(i = 0; i < digits.size(); ++i)
-    		count[digits[i]]++;
-    	string ans;
-    	while(count[9]--)	ans += '9';
-    	while(count[6]--)	ans += '6';
-    	while(count[3]--)	ans += '3';
-    	while(count[0]--)	ans += '0';
-        for(i = 0; i < 10; ++i)
-    	{
-    		if(count[i] >= 3)
-    		{
-    			n = (count[i]/3-1)*3;
-                count[i] -= n;
-    			while(n--)
-    				ans += char(i+'0');
-    		}
-    	}
-    	while(count[8])
-    	{
-    		while(count[8] && count[7])
-    		{
-    			ans += "87";
-    			count[8]--;
-    			count[7]--;
-    		}
-    		while(count[8] && count[4])
-    		{
-    			ans += "84";
-    			count[8]--;
-    			count[4]--;
-    		}
-    		while(count[8] && count[1])
-    		{
-    			ans += "81";
-    			count[8]--;
-    			count[1]--;
-    		}
-            break;
-    	}
-    	while(count[7])
-    	{
-    		while(count[7] && count[5])
-    		{
-    			ans += "75";
-    			count[7]--;
-    			count[5]--;
-    		}
-    		while(count[7] && count[2])
-    		{
-    			ans += "72";
-    			count[7]--;
-    			count[2]--;
-    		}
-            break;
-    	}
-    	while(count[5])
-    	{
-    		while(count[5] && count[4])
-    		{
-    			ans += "54";
-    			count[5]--;
-    			count[4]--;
-    		}
-    		while(count[5] && count[1])
-    		{
-    			ans += "51";
-    			count[5]--;
-    			count[1]--;
-    		}
-            break;
-    	}
-    	while(count[4])
-    	{
-    		while(count[4] && count[2])
-    		{
-    			ans += "42";
-    			count[4]--;
-    			count[2]--;
-    		}
-            break;
-    	}
-    	while(count[2])
-    	{
-    		while(count[2] && count[1])
-    		{
-    			ans += "21";
-    			count[2]--;
-    			count[1]--;
-    		}
-            break;
-    	}
-    	
-    	sort(ans.begin(), ans.end(),[](char& a, char& b){return a > b;});
-    	if(ans != "" && ans[0]=='0')
-    		return "0";
-    	return ans;
+        int count[10] = {0}, i, sum = 0, time;
+        string ans; 
+        for(i = 0; i < digits.size(); ++i)
+        {
+            count[digits[i]]++;
+            sum += digits[i];
+            ans += digits[i]+'0';
+        }
+        sort(ans.begin(), ans.end(),[](char& a, char& b){return a > b;});
+        if(sum%3 == 1)
+        {
+            i = ans.size()-1;
+            if(count[1]!=0||count[4]!=0||count[7]!=0)
+            {
+                for( ; i>=0; --i)
+                {
+                    if(ans[i]=='1'||ans[i]=='4'||ans[i]=='7')
+                    {
+                        ans.erase(ans.begin()+i);
+                        break;
+                    }
+                }
+            }
+            else
+            {   
+                time = 2;
+                for( ; i>=0; --i)
+                {
+                    if(ans[i]=='2'||ans[i]=='5'||ans[i]=='8')
+                    {
+                        ans.erase(ans.begin()+i);
+                        time--;
+                        if(time == 0)
+                            break;
+                    }
+                }
+            }
+        }
+        else if(sum%3 == 2)
+        {
+            i = ans.size()-1;
+            if(count[2]!=0||count[5]!=0||count[8]!=0)
+            {
+                for( ; i>=0; --i)
+                {
+                    if(ans[i]=='2'||ans[i]=='5'||ans[i]=='8')
+                    {
+                        ans.erase(ans.begin()+i);
+                        break;
+                    }
+                }
+            }
+            else
+            {   
+                time = 2;
+                for( ; i>=0; --i)
+                {
+                    if(ans[i]=='1'||ans[i]=='4'||ans[i]=='7')
+                    {
+                        ans.erase(ans.begin()+i);
+                        time--;
+                        if(time == 0)
+                            break;
+                    }
+                }
+            }
+        }
+        if(ans != "" && ans[0]=='0')
+            return "0";
+        return ans;
     }
 };
