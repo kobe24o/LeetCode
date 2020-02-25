c8e1352d1a434ad77fcfd27789e8646b90c34f14
kobe_24o <mingmingchen1989@163.com>
2020-02-25 12:13:50 +0800

    Update 01.03.string-to-url-lcci.cpp

--- 程序员面试金典/01.03.string-to-url-lcci.cpp
+++ 程序员面试金典/01.03.string-to-url-lcci.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     string replaceSpaces(string S, int length) {
-    	string ans;
-    	for(int i = 0; i < length; ++i)
+        int i, j, count = 0;
+    	for(i = 0; i < length; ++i)
     	{
     		if(S[i] == ' ')
-    			ans += "%20";
-    		else
-    			ans += S[i];
+        		count++;
     	}
+        int len = length-count+3*count;
+        string ans(len,' ');
+        for(i = 0, j = 0; i < length; ++i,++j)
+        {
+            if(S[i] != ' ')
+                ans[j] = S[i];
+            else
+            {
+                ans[j++] = '%';
+                ans[j++] = '2';
+                ans[j]   = '0';
+            }
+        }
     	return ans;
     }
 };
