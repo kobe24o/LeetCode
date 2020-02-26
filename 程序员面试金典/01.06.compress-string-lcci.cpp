047ae0012210c1550c00bbc710e492dcdc87a6d6
kobe24o <mingmingchen1989@163.com>
2020-02-26 11:28:01 +0800

    lc

--- 程序员面试金典/01.06.compress-string-lcci.cpp
+++ 程序员面试金典/01.06.compress-string-lcci.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     string compressString(string S) {
-
+    	if(S.size()<2)
+    		return S;
+    	char prev = S[0];
+    	int i, count = 1;
+    	string ans;
+    	for(i = 1; i < S.size(); ++i)
+    	{
+    		if(S[i] == prev)
+    			count++;
+    		else
+    		{
+    			ans += prev+to_string(count);
+    			count = 1;
+    			prev = S[i];
+    		}
+    		if(i == S.size()-1)
+    			ans += prev+to_string(count);
+			if(ans.size() >= S.size())
+				return S;
+    	}
+    	return ans;
     }
 };
