d1817f7a25655d3ad97fc6098708ada75dfcb76c
kobe_24o <mingmingchen1989@163.com>
2020-03-04 11:18:36 +0800

    lc

--- 程序员面试金典/17.14.smallest-k-lcci.cpp
+++ 程序员面试金典/17.14.smallest-k-lcci.cpp
@@ -13,3 +13,53 @@ public:
     	return arr;
     }
 };
+
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+    	sort(arr.begin(),arr.end());
+        return vector<int>(arr.begin(),arr.begin()+k);
+    }
+};
+
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        if(arr.empty()||(k==0))
+            return {};
+    	findkth(arr,k,0,arr.size()-1);
+    	return vector<int> (arr.begin(), arr.begin()+k);
+    }
+
+    int findkth(vector<int>& arr, int& k, int l, int r)
+    {
+    	selectMid(arr,l,r);
+    	int p = arr[l];
+    	int i = l, j = r;
+    	while(i < j)
+    	{
+    		while(i < j && arr[j] > p)
+    			j--;
+    		swap(arr[i],arr[j]);
+    		while(i < j && arr[i] <= p)
+    			i++;
+    		swap(arr[i],arr[j]);
+    	}
+    	if(i == k)
+    		return i;
+    	else if(i < k)
+    		return findkth(arr,k,i+1,r);
+    	return findkth(arr,k,l,i-1);
+    }
+
+    void selectMid(vector<int>& arr, int& l, int& r)
+    {
+    	int mid = l+((r-l)>>1);
+    	if(arr[mid] > arr[r])
+    		swap(arr[mid],arr[r]);
+    	if(arr[l] > arr[r])
+    		swap(arr[mid],arr[r]);
+    	if(arr[mid] > arr[l])
+    		swap(arr[mid],arr[l]);
+    }
+};
