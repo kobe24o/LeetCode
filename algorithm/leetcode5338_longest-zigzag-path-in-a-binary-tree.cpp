463129f58d5aed217db7164eb1b27e36f0353aff
kobe_24o <mingmingchen1989@163.com>
2020-03-08 01:07:46 +0800

    Update leetcode5338_longest-zigzag-path-in-a-binary-tree.cpp

--- algorithm/leetcode5338_longest-zigzag-path-in-a-binary-tree.cpp
+++ algorithm/leetcode5338_longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,6 +1,30 @@
 class Solution {
+	int ans = 0;
 public:
     int longestZigZag(TreeNode* root) {
+    	if(!root)
+    		return 0;
+    	dfs(root->left,0,0);
+    	dfs(root->right,0,1);
+    	return ans;
+    }
 
+    void dfs(TreeNode* root, int count, bool dir)
+    {
+    	if(!root) 
+    	{
+    		ans = max(ans,count);
+    		return;
+    	}
+    	if(dir)//前一个是右节点
+    	{
+    		dfs(root->left,count+1,0);
+    		dfs(root->right,0,1);
+    	}
+    	else//前一个是左节点
+    	{
+    		dfs(root->left,0,0);
+    		dfs(root->right,count+1,1);
+    	}
     }
 };
