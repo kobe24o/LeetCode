6c856c0fb894217c8d3f7a294c3e704d5bc19813
kobe_24o <mingmingchen1989@163.com>
2020-03-14 18:41:12 +0800

    Update leetcode0_test.cpp

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -16,62 +16,52 @@ struct ListNode {
 using namespace std;
 class Solution {
 public:
-    /**
-     * @param atk1: the power of heros
-     * @param atk2: the power of monsters
-     * @return: how many monsters can you kill at most?
-     */
-    int getAns(vector<int> &atk1, vector<int> &atk2) {
-        sort(atk1.begin(),atk1.end());
-        sort(atk2.begin(),atk2.end());
-        int i = 0, j = 0, count = 0;
-        for(i = 0; i < atk1.size(); ++i)
+    int lengthOfLIS(vector<int>& nums) {
+        if(nums.size() == 0)
+            return 0;
+        int i, l, r, n = nums.size(), maxlen = 0, idx;
+        vector<int> arr(nums);
+        for(i = 0; i < n; ++i)
         {
-            cout << atk1[i] << " ";
-            j = bs(atk2,atk1[i],0,atk2.size()-1);
-            if(j == -1){
-                cout << atk2.back() << "fail " << endl;
-                atk2.pop_back();
-            }
-            else
+            l = 0, r = maxlen;
+            idx = bs(nums,arr,l,maxlen,nums[i],maxlen);
+            if(idx == maxlen)
             {
-                count++;
-                cout << *(atk2.begin()+j) << " win" << endl;
-                atk2.erase(atk2.begin()+j);
+                arr[idx+1] = nums[i];
+                maxlen++;
             }
         }
-        return count;
+        return maxlen;
     }
 
-    int bs(vector<int>& a, int &target, int l, int r)
+    int bs(vector<int>& nums, vector<int> &arr, int l, int r, int& target, int& maxlen)
     {
         int mid;
         while(l <= r)
         {
-            mid = l+((r-l)>>1);
-            if(a[mid] > target)
-                r = mid-1;
+            mid = l + ((r-l)>>1);
+            if(arr[mid] < target)
+                l = mid+1;
             else
             {
-                if((mid==a.size()-1) || a[mid+1] > target)
-                    return mid;
+                if(mid == maxlen || arr[mid+1] > target)
+                    break;
                 else
-                    l = mid+1;
+                    r = mid-1;
             }
         }
         return -1;
     }
 };
 
-
 int main() {
     vector<vector<int>> v = {{1,3,2},{4,6,5},{7,9,8},{13,15,14},{10,12,11}};
-    vector<int> v1 = {17,17,37,20,35,21,33,16,3,45};
+    vector<int> v1 = {10,9,2,5,3,7,101,18};
     vector<int> v3 = {21,44,5,21,33,38,23,5,25,43};
     string str = "eceeeefasdghjklqwertyuio";
     vector<vector<int>> v2 = {{2,1},{2,3}};//{4,2},{5,2},{6,5},{7,1},{8,3},{9,1},{10,1}};
     Solution s;
-    s.getAns(v1,v3);
+    s.lengthOfLIS(v1);
 
     ListNode *h1 = new ListNode(3);
     ListNode *h2 = new ListNode(5);
