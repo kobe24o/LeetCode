a6f3abc8d052820c86888bc07c90ce21fac0c8c7
kobe_24o <mingmingchen1989@163.com>
2020-03-19 15:03:16 +0800

    Create 03.05.sort-of-stacks-lcci.cpp

---
+++ 程序员面试金典/03.05.sort-of-stacks-lcci.cpp
@@ -0,0 +1,22 @@
+class SortedStack {
+public:
+    SortedStack() {
+        
+    }
+    
+    void push(int val) {
+
+    }
+    
+    void pop() {
+
+    }
+    
+    int peek() {
+
+    }
+    
+    bool isEmpty() {
+
+    }
+};
