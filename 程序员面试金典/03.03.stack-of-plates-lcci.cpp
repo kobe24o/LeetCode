145508b3919dea80ed02ad94dfcd6033e0244208
kobe_24o <mingmingchen1989@163.com>
2020-03-19 12:23:39 +0800

    Create 03.03.stack-of-plates-lcci.cpp

---
+++ 程序员面试金典/03.03.stack-of-plates-lcci.cpp
@@ -0,0 +1,19 @@
+class StackOfPlates {
+	int n;
+public:
+    StackOfPlates(int cap) {
+    	n = cap;
+    }
+    
+    void push(int val) {
+
+    }
+    
+    int pop() {
+
+    }
+    
+    int popAt(int index) {
+
+    }
+};
