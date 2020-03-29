878b41c57bbc65afa75747040be79cb47acf9208
kobe_24o <mingmingchen1989@163.com>
2020-03-29 18:24:12 +0800

    lc

--- algorithm/leetcode5370_design-underground-system.cpp
+++ algorithm/leetcode5370_design-underground-system.cpp
@@ -26,3 +26,29 @@ public:
         return station_time_n[s][0]/double(station_time_n[s][1]);
     }
 };
+
+class UndergroundSystem {
+    unordered_map<string, unordered_map<string,pair<int,int>>> Station_time_n;// 起点：终点；2个int（总时间，人数）
+    unordered_map<int, pair<string,int>> id_startStation_t;// 乘客id，（起点，出发时刻）
+    string startStation;
+    int time;
+public:
+    UndergroundSystem() { }
+    
+    void checkIn(int id, string stationName, int t) {
+        id_startStation_t[id].first = stationName;//  乘客id，（起点，出发时刻）
+        id_startStation_t[id].second = t;
+    }
+    
+    void checkOut(int id, string stationName, int t) {
+        startStation = id_startStation_t[id].first;
+        time = t-id_startStation_t[id].second;
+        Station_time_n[startStation][stationName].first += time;
+        Station_time_n[startStation][stationName].second += 1;
+        id_startStation_t.erase(id);
+    }
+    
+    double getAverageTime(string startStation, string endStation) {
+        return Station_time_n[startStation][endStation].first/double(Station_time_n[startStation][endStation].second);
+    }
+};
--- 程序员面试金典/16.19.pond-sizes-lcci.cpp
+++ 程序员面试金典/16.19.pond-sizes-lcci.cpp
@@ -1,4 +1,6 @@
 class Solution {
+	int m, n;
+	vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0},{1,1},{-1,-1},{1,-1},{-1,1}};
 public:
     vector<int> pondSizes(vector<vector<int>>& land) {
     	
