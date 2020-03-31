58a6eaddbc1d7fac2b2b2aa36854d1b579b3ed5d
kobe_24o <mingmingchen1989@163.com>
2020-03-31 21:05:32 +0800

    lc

--- 程序员面试金典/10.02.group-anagrams-lcci.cpp
+++ 程序员面试金典/10.02.group-anagrams-lcci.cpp
@@ -15,3 +15,36 @@ public:
         return ans;
     }
 };
+
+class Solution {
+	int count[26] = {0};
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> m;
+        vector<vector<string>> ans;
+        string origin;
+        for(auto& s : strs)
+        {
+        	origin = s;
+        	mysort(s);
+        	m[s].push_back(origin);
+        }
+        for(auto& mi : m)
+        	ans.push_back(mi.second);
+        return ans;
+    }
+    void mysort(string& s)
+    {
+    	for(int i = 0; i < s.size(); ++i)
+    		count[s[i]-'a']++;
+    	s = "";
+    	for(int i = 0; i < 26; ++i)
+    	{
+    		while(count[i])
+    		{
+                s += i+'a';
+                count[i]--;
+            }
+    	}
+    }
+};
