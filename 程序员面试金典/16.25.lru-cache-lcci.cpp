b6abcd7db0a210a51f3ed11df9d39e85175d1528
kobe_24o <mingmingchen1989@163.com>
2020-04-02 11:21:16 +0800

    lc

--- 程序员面试金典/16.25.lru-cache-lcci.cpp
+++ 程序员面试金典/16.25.lru-cache-lcci.cpp
@@ -1,52 +1,124 @@
-class ListNode
+class listNode
 {
 public:
-	int val;
-	ListNode* next;
-	ListNode* prev;
-	ListNode(int x) : val(x), next(NULL), prev(NULL){}
+    int key, value;
+    listNode* next;
+    listNode* prev;
+    listNode(int k, int v) : key(k), value(v), next(NULL), prev(NULL){}
 };
-
+class List
+{
+public:
+    listNode *head, *tail;
+    int len;
+    List():len(0)
+    {
+        head = new listNode(0,0);
+        tail = new listNode(0,0);
+        head->next = tail;
+        tail->next = head;
+    }
+    void addAtHead(listNode* node)
+    {
+        node->next = head->next;
+        node->prev = head;
+        head->next->prev = node;
+        head->next = node;
+        len++;
+    }
+    void delNode(listNode* node)
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        len--;
+    }
+    listNode* dellast()
+    {
+        if(size()==0)
+            return NULL;
+        listNode* last = tail->prev;
+        delNode(last);
+        return last;
+    }
+    int size()
+    {
+        return len;
+    }
+};
 class LRUCache {
-	int cap;
-	int size = 0;
-	unordered_map<int,ListNode*> key_node;
-	unordered_map<ListNode*,int> node_key;
-	ListNode* emptyHead;
-	ListNode* tail;
+    int cap;
+    unordered_map<int,listNode*> key_node;
+    List cache;
 public:
     LRUCache(int capacity) {
-    	cap = capacity;
-    	emptyHead = new ListNode(0);
-    	tail = emptyHead;
+        cap = capacity;
     }
     
     int get(int key) {
-    	if(!key_node.count(key))
-    		return -1;
-    	
+        if(!key_node.count(key))
+            return -1;
+        int val = key_node[key]->value;
+        put(key,val);
+        return val;
     }
     
     void put(int key, int value) {
-    	if(size < cap)
-    	{
-    		ListNode *node = new ListNode(value);
-    		key_node[key] = node;
-    		node_key[node] = key;
-    		size++;
-    		if(emptyHead->next)
-    			emptyHead->next->prev = node;
-    		node->next = emptyHead->next;
-    		if(!node->next)
-    			tail = node;
-    	}
-    	else
-    	{
-    		tail = tail->prev;
-    		delete tail->next;
-    		tail->next = NULL;
-    		size--;
-    		put(key,value);
-    	}
+        listNode *node = new listNode(key, value);
+        if(key_node.count(key))
+        {
+            cache.delNode(key_node[key]);
+            cache.addAtHead(node);
+            key_node[key] = node;
+        }
+        else
+        {
+            if(cap == cache.size())
+            {
+                listNode *last = cache.dellast();
+                key_node.erase(last->key);
+            }
+            cache.addAtHead(node);
+            key_node[key] = node;
+        }
+    }
+};
+
+class LRUCache {
+    list<int> cache;
+    int cap;
+    unordered_map<int,int> kv;
+    unordered_map<int,list<int>::iterator> kPos;
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+    }
+    
+    int get(int key) {
+        if(!kv.count(key))
+            return -1;
+        put(key,kv[key]);
+        return kv[key];
+    }
+    
+    void put(int key, int value) {
+        if(kv.count(key))
+        {
+            cache.erase(kPos[key]);
+            cache.push_front(key);
+            kPos[key] = cache.begin();
+            kv[key] = value;
+        }
+        else
+        {
+            if(cap == cache.size())
+            {
+                int lastkey = cache.back();
+                cache.pop_back();
+                kv.erase(lastkey);
+            }
+            kv[key] = value;
+            cache.push_front(key);
+            kPos[key] = cache.begin();
+        }
     }
 };
