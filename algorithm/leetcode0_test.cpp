7023f7f34fc27fe1e8bcf06854c13cd66a630fab
kobe_24o <mingmingchen1989@163.com>
2020-04-03 20:58:33 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -19,88 +19,75 @@ struct TreeNode {
 };
 using namespace std;
 
-class Solution {	//桶排序
+class Solution {
+    unordered_map<string,string> father;//并查集
+    unordered_map<string,int> m;//名称，频次
 public:
-    vector<int> sortArray(vector<int>& arr) {
-        if(arr.size() <= 1)
-            return arr;
-        int i, j = 0, min, max;
-        min = max = arr[0];
-        for(i = 1; i < arr.size(); ++i)
+    vector<string> trulyMostPopular(vector<string>& names, vector<string>& synonyms) {
+        string name, name1, name2, p;
+        int i, count;
+        for(string& n : names)
         {
-            min = arr[i] < min ? arr[i] : min;
-            max = arr[i] > max ? arr[i] : max;
+            i = n.find("(");
+            name = n.substr(0,i);
+            count = 0;
+            while(++i < n.size()-1)
+                count = 10*count+n[i]-'0';
+            m[name] = count;//获取每个名字的次数
+            father[name] = name;//并查集初始化
         }
-        if(min == max)
-            return arr;
-        int div = 5;//桶个数
-        int space = (max-min)/div+1;
-        vector<int> temp(arr.size());
-        vector<int> bucketsize(div,0);
-        vector<int> bucketPos(div,0);
-        for(i = 0; i < arr.size(); ++i)
-            bucketsize[(arr[i]-min)/space]++;
-        bucketPos[0] = bucketsize[0];
-        for(i = 1; i < arr.size(); ++i)
-            bucketPos[i] += bucketPos[i-1] + bucketsize[i];//桶结束位置的下一个
-        for(i = 0; i < arr.size(); ++i)
-            temp[--bucketPos[(arr[i]-min)/space]] = arr[i];
-        for(i = 0; i < div; ++i)
+        for(auto& n : synonyms)
+        {   //这里可能有上面不存在的name，再添加一遍
+            i = n.find(",");
+            name1 = n.substr(1,i-1);
+            name2 = n.substr(i+1,n.size()-i-2);
+            father[name1] = name1;//并查集初始化
+            father[name2] = name2;//并查集初始化
+        }
+        for(auto& n : synonyms)
         {
-            if(bucketsize[i] > 1)
+            i = n.find(",");
+            name1 = n.substr(1,i-1);
+            name2 = n.substr(i+1,n.size()-i-2);
+            merge(name1,name2);//全部进行合并和路径压缩
+        }
+        unordered_map<string,vector<string>> fatherSet;
+        for(auto f : father)
+        {
+            name = f.first;
+            p = uniFind(name);
+            fatherSet[p].push_back(name);
+        }
+        vector<string> ans;
+        for(auto& f : fatherSet)
+        {
+            count = 0;
+            for(auto& v : f.second)
             {
-                qsort(temp,bucketPos[i],bucketPos[i]+bucketsize[i]-1);
+                count += m[v];
             }
+            ans.push_back(f.first+"("+to_string(count)+")");
         }
-        for(i = 0; i < arr.size(); ++i)
-            arr[i] = temp[i];
-        return arr;
+        return ans;
     }
 
-    void qsort(vector<int>& arr, int l, int r)
+    string uniFind(string x)
     {
-        if(l >= r)
-            return;
-        int Pl = l, Pr = l;
-        partition(arr,l,r,Pl,Pr);
-        qsort(arr,l,Pl-1);
-        qsort(arr,Pr+1,r);
+        if(x == father[x])
+            return x;
+        return father[x] = uniFind(father[x]);//等式为路径压缩操作
     }
 
-    void partition(vector<int>& arr, int l, int r, int& Pl, int& Pr)
+    void merge(string x, string y)
     {
-        selectMid(arr,l,r);
-        int P = arr[l];
-        int i = l, j = r;
-        while(i < j)
+        string fatherx = uniFind(x);
+        string fathery = uniFind(y);
+        if(fatherx != fathery)
         {
-            while(i < j && P < arr[j])//没有等于号，哨兵都在左侧
-                j--;
-            swap(arr[i], arr[j]);
-            while(i < j && arr[i] <= P)
-                i++;
-            swap(arr[i], arr[j]);
+            if(fatherx < fathery)
+                swap(fatherx, fathery);//x的字典序大
+            father[fatherx] = fathery;//字典序小的y做代表
         }
-        Pl = Pr = i;
-        for(i = i-1; i >= l; --i)
-        {
-            if(arr[i] == P)
-            {
-                Pl--;
-                swap(arr[i], arr[Pl]);
-            }
-        }
-    }
-
-    void selectMid(vector<int>& arr, int l, int r)
-    {
-        int mid = l+((r-l)>>1);
-        if(arr[mid] > arr[r])
-            swap(arr[mid],arr[r]);
-        if(arr[l] > arr[r])
-            swap(arr[l], arr[r]);
-        if(arr[mid] > arr[l])
-            swap(arr[mid], arr[l]);
     }
 };
 
@@ -112,17 +99,27 @@ public:
 //则当前节点所构成的最大距离即左深度+右深度-2*当前节点深度。
 
 //["...1","..S.","..*.",".0.T"]   9
-
+void printv(vector<string>& v)
+{
+    for(auto& vi : v)
+        cout << vi << " ";
+    cout << endl;
+}
 int main() {
     vector<vector<char>> v4 = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
     vector<int> v1 = {1,2,3,3,4,3,4,5,6};
     vector<int> v3 = {21,44,5,21,33,38,23,5,25,43};
     string str = "eceeeefasdghjklqwertyuio";
     vector<vector<int>> v2 = {{2,4,3},{6,5,2}};
-    vector<string> st  = { };
+    vector<string> st  = {"Pwsuo(71)","Prf(48)","Rgbu(49)","Zvzm(31)","Xxcl(25)","Bbcpth(42)","Padz(70)","Jmqqsj(19)","Uwy(26)","Jylbla(65)","Xioal(11)","Npbu(62)","Jpftyg(96)","Tal(46)","Hnc(100)","Yldu(85)","Alqw(45)","Wbcxi(34)","Kxjw(36)","Clplqf(8)","Fayxe(66)","Slfwyo(48)","Xbesji(70)","Pmbz(22)","Oip(2)","Fzoe(63)","Qync(79)","Utc(11)","Sqwejn(19)","Ngi(8)","Gsiiyo(60)","Bcs(73)","Icsvku(1)","Yzwm(92)","Vaakt(21)","Uvt(70)","Axaqkm(100)","Gyhh(84)","Gaoo(98)","Ghlj(35)","Umt(13)","Nfimij(52)","Zmeop(77)","Vje(29)","Rqa(47)","Upn(89)","Zhc(44)","Slh(66)","Orpqim(69)","Vxs(85)","Gql(19)","Sfjdjc(62)","Ccqunq(93)","Oyo(32)","Bvnkk(52)","Pxzfjg(45)","Kaaht(28)","Arrugl(57)","Vqnjg(50)","Dbufek(63)","Fshi(62)","Lvaaz(63)","Phlto(41)","Lnow(70)","Mqgga(31)","Adlue(82)","Zqiqe(27)","Mgs(46)","Zboes(56)","Dma(70)","Jnij(57)","Ghk(14)","Mrqlne(39)","Ljkzhs(35)","Rmlbnj(42)","Qszsny(93)","Aasipa(26)","Wzt(41)","Xuzubb(90)","Maeb(56)","Mlo(18)","Rttg(4)","Kmrev(31)","Kqjl(39)","Iggrg(47)","Mork(88)","Lwyfn(50)","Lcp(42)","Zpm(5)","Qlvglt(36)","Liyd(48)","Jxv(67)","Xaq(70)","Tkbn(81)","Rgd(85)","Ttj(28)","Ndc(62)","Bjfkzo(54)","Lqrmqh(50)","Vhdmab(41)"};
+    vector<string> st1 = {"(Uvt,Rqa)","(Qync,Kqjl)","(Fayxe,Upn)","(Maeb,Xaq)","(Pmbz,Vje)","(Hnc,Dma)","(Pwsuo,Gyhh)","(Gyhh,Aasipa)","(Fzoe,Lcp)","(Mgs,Vhdmab)","(Qync,Rgd)","(Gql,Liyd)","(Gyhh,Tkbn)","(Arrugl,Adlue)","(Wbcxi,Slfwyo)","(Yzwm,Vqnjg)","(Lnow,Vhdmab)","(Lvaaz,Rttg)","(Nfimij,Iggrg)","(Vje,Lqrmqh)","(Jylbla,Ljkzhs)","(Jnij,Mlo)","(Adlue,Zqiqe)","(Qync,Rttg)","(Gsiiyo,Vxs)","(Xxcl,Fzoe)","(Dbufek,Xaq)","(Ccqunq,Qszsny)","(Zmeop,Mork)","(Qync,Ngi)","(Zboes,Rmlbnj)","(Yldu,Jxv)","(Padz,Gsiiyo)","(Oip,Utc)","(Tal,Pxzfjg)","(Adlue,Zpm)","(Bbcpth,Mork)","(Qync,Lvaaz)","(Pmbz,Qync)","(Alqw,Ngi)","(Bcs,Maeb)","(Rgbu,Zmeop)"};
     Solution s;
-    s.sortArray(v1);
-
+    vector<string>ans = s.trulyMostPopular(st,st1);
+    sort(ans.begin(), ans.end());
+    vector<string> v5 = {"Prf(48)","Zvzm(31)","Jmqqsj(19)","Uwy(26)","Jylbla(100)","Xioal(11)","Npbu(62)","Jpftyg(96)","Pxzfjg(91)","Dma(170)","Jxv(152)","Kxjw(36)","Clplqf(8)","Slfwyo(82)","Xbesji(70)","Fzoe(130)","Alqw(424)","Oip(13)","Sqwejn(19)","Icsvku(1)","Vqnjg(142)","Vaakt(21)","Rqa(117)","Axaqkm(100)","Aasipa(262)","Gaoo(98)","Ghlj(35)","Umt(13)","Iggrg(99)","Fayxe(155)","Zhc(44)","Slh(66)","Orpqim(69)","Gsiiyo(215)","Sfjdjc(62)","Ccqunq(186)","Oyo(32)","Bvnkk(52)","Kaaht(28)","Fshi(62)","Phlto(41)","Mqgga(31)","Adlue(171)","Lnow(157)","Rmlbnj(98)","Jnij(75)","Ghk(14)","Mrqlne(39)","Wzt(41)","Xuzubb(90)","Kmrev(31)","Bbcpth(256)","Lwyfn(50)","Qlvglt(36)","Gql(67)","Bcs(262)","Ttj(28)","Ndc(62)","Bjfkzo(54)"};
+    sort(v5.begin(), v5.end());
+    printv(ans);
+    printv(v5);
     string s1 = "1";
     cout << s1[1] << "s[1]" << endl;
     TreeNode *t1 = new TreeNode(1);
--- 程序员面试金典/17.07.baby-names-lcci.cpp
+++ 程序员面试金典/17.07.baby-names-lcci.cpp
@@ -1,48 +1,69 @@
 class Solution {
+    unordered_map<string,string> father;//并查集
+    unordered_map<string,int> m;//名称，频次
 public:
     vector<string> trulyMostPopular(vector<string>& names, vector<string>& synonyms) {
-    	string name, name1;
-    	int i, count;
-    	unordered_map<string,string> tran;
-    	unordered_map<string,int> m;
-    	for(auto& n : synonyms)
-    	{
-    		i = n.find(",");
-    		name = n.substr(1,i-1);
-    		name1 = n.substr(i+1,n.size()-i-2);
-    		if(name < name1)
-    			swap(name, name1);
-    		tran[name] = name1;
-    	}
-    	for(string& n : names)
-    	{
-    		i = n.find("(");
-    		name = n.substr(0,i);
-    		count = 0;
-    		while(++i < n.size()-1)
-    		{
-    			count = 10*count+n[i]-'0';
-    		}
-    		m[name] = count;
-    	}
-    	for(auto& mi : m)
-    	{
-    		name = mi.first;
-    		count = 0;
-    		while(tran.count(name))
-    		{
-    			count += m[name];
-    			m[name] = 0;
-    			name = tran[name];
-    		}
-    		m[name] += count;
-    	}
-    	vector<string> ans;
-    	for(auto& mi : m)
-    	{
-    		if(mi.second)
-    			ans.push_back(mi.first+"("+to_string(mi.second)+")");
-    	}
-    	return ans;
+        string name, name1, name2, p;
+        int i, count;
+        for(string& n : names)
+        {
+            i = n.find("(");
+            name = n.substr(0,i);
+            count = 0;
+            while(++i < n.size()-1)
+                count = 10*count+n[i]-'0';
+            m[name] = count;//获取每个名字的次数
+            father[name] = name;//并查集初始化
+        }
+        for(auto& n : synonyms)
+        {   //这里可能有上面不存在的name，再添加一遍
+            i = n.find(",");
+            name1 = n.substr(1,i-1);
+            name2 = n.substr(i+1,n.size()-i-2);
+            father[name1] = name1;//并查集初始化
+            father[name2] = name2;//并查集初始化
+        }
+        for(auto& n : synonyms)
+        {
+            i = n.find(",");
+            name1 = n.substr(1,i-1);
+            name2 = n.substr(i+1,n.size()-i-2);
+            merge(name1,name2);//全部进行合并和路径压缩
+        }
+        unordered_map<string,vector<string>> fatherSet;
+        for(auto f : father)//压缩后的f(只有两层)，所有边的顶层（字典序最小的）
+        {
+            name = f.first;
+            p = uniFind(name);
+            fatherSet[p].push_back(name);
+        }
+        vector<string> ans;
+        for(auto& f : fatherSet)
+        {	//所有的等效名字
+            count = 0;
+            for(auto& v : f.second)//它底下包含的name
+                count += m[v];//加上他的计数
+            ans.push_back(f.first+"("+to_string(count)+")");
+        }
+        return ans;
+    }
+
+    string uniFind(string x)
+    {
+        if(x == father[x])
+            return x;
+        return father[x] = uniFind(father[x]);//等式为路径压缩操作
+    }
+
+    void merge(string x, string y)
+    {
+        string fatherx = uniFind(x);
+        string fathery = uniFind(y);
+        if(fatherx != fathery)
+        {
+            if(fatherx < fathery)
+                swap(fatherx, fathery);//x的字典序大
+            father[fatherx] = fathery;//字典序小的y做代表
+        }
     }
 };
