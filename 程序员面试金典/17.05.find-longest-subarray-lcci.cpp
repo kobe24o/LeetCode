2475d2ffec79750b460b3d7b20a6de8db142d84e
kobe_24o <mingmingchen1989@163.com>
2020-04-05 21:57:28 +0800

    Update 17.05.find-longest-subarray-lcci.cpp

--- 程序员面试金典/17.05.find-longest-subarray-lcci.cpp
+++ 程序员面试金典/17.05.find-longest-subarray-lcci.cpp
@@ -3,44 +3,26 @@ public:
     vector<string> findLongestSubarray(vector<string>& array) {
         if(array.size() < 2)
             return {};
-        unordered_map<int, vector<int>> m;
-        int sum = 0, i;
-        for(i = 0; i < array.size(); ++i)
+        unordered_map<int, int> m;
+        int start = 0, end = 0, sum = 0;
+        for(int i = 0; i < array.size(); ++i)
         {
-        	if(isdigit(array[i][0]))
-        		sum++;
-        	else
-        		sum--;
-        	if(m[sum].size() < 2)
-        		m[sum].push_back(i);
-        	else
-        		m[sum][1] = i;
+            if(isdigit(array[i][0]))
+                sum++;
+            else
+                sum--;
+            if(sum == 0)//等于0，表示[0,i]满足题意
+                start = 0, end = i; 
+            else if(!m.count(sum))//其他数字，记录起点
+                m[sum] = i; 
+            else if(i-m[sum] > end-start+1)//map包含sum，长度更大
+            {
+                start = m[sum]+1;//更新区间端点
+                end = i;
+            }
         }
-        if(sum == 0)
-        	return array;
-        int maxlen = 0;
-        vector<int> key;
-        for(auto& mi : m)
-        {
-        	if(mi.second[1]-mi.second[0] > maxlen)
-        	{
-        		maxlen = mi.second[1]-mi.second[0]
-        		key.clear();
-        		key.push_back(mi.first);
-        	}
-        	else if(mi.second[1]-mi.second[0] == maxlen)
-        		key.push_back(mi.first);
-        }
-        int minidx = INT_MAX;
-        int ansKey;
-        for(int k : key)
-        {
-        	if(m[k][0] < minidx)
-        	{
-        		minidx = m[k][0];
-        		ansKey = k;
-        	}
-        }
-        return vector<string> (array.begin()+m[ansKey][0], array.begin()+m[ansKey][1]);
+        if(start == end)
+            return{};
+        return vector<string>(array.begin()+start, array.begin()+end+1);
     }
 };
