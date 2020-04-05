a4702f2047f5b004e9d4addda8bd387c08a183cb
kobe_24o <mingmingchen1989@163.com>
2020-04-05 15:15:16 +0800

    Update leetcode5367.minimum-subsequence-in-non-increasing-order.cpp

--- algorithm/leetcode5367.minimum-subsequence-in-non-increasing-order.cpp
+++ algorithm/leetcode5367.minimum-subsequence-in-non-increasing-order.cpp
@@ -18,3 +18,35 @@ public:
         return vector<int>(nums.begin(),nums.begin()+i+1);
     }
 };
+
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        int count[101] = {0};
+        int i, sum = 0, s = 0;
+        for(i = 0; i < nums.size(); ++i)
+        {
+            sum += nums[i];
+            count[nums[i]]++;
+        }
+        vector<int> ans;
+        for(i = 100; i >= 0; --i)
+        {
+            while(count[i]--)
+            {
+                if(s+i > sum-i)
+                {
+                    ans.push_back(i);
+                    return ans;
+                }
+                else
+                {
+                    s += i;
+                    sum -= i;
+                    ans.push_back(i);
+                }
+            }
+        }
+        return ans;
+    }
+};
