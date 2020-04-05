3e36e2e3244ea3a1cab5599140a5d0e8e044efdf
kobe_24o <mingmingchen1989@163.com>
2020-04-05 23:59:11 +0800

    Update leetcode460.lfu-cache.cpp

--- algorithm/leetcode460.lfu-cache.cpp
+++ algorithm/leetcode460.lfu-cache.cpp
@@ -1,7 +1,14 @@
+class node{
+public:
+	int k, v, f;
+	node(int key, int val, int freq):k(key),v(val),f(freq){}
+};
 class LFUCache {
+	unordered_map<int, list<node>::iterator> key_table;
+    unordered_map<int, list<node>> freq_table;
 public:
     LFUCache(int capacity) {
-    	
+
     }
     
     int get(int key) {
