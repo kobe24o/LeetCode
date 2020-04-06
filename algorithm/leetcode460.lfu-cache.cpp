66638a3e797454fb153bc7aa4ad7b5e887568e54
kobe_24o <mingmingchen1989@163.com>
2020-04-06 17:05:42 +0800

    Update leetcode460.lfu-cache.cpp

--- algorithm/leetcode460.lfu-cache.cpp
+++ algorithm/leetcode460.lfu-cache.cpp
@@ -8,7 +8,7 @@ class LFUCache {
 	unordered_map<int, list<node>::iterator> kPos;//key 对应的节点迭代器位置
     unordered_map<int, list<node>> freq_list;//不同的频数下挂着一条双链表，尾部是最少使用的
     int cap;
-    int minfreq;
+    int minfreq;//最小的频数
     int size;
 public:
     LFUCache(int capacity) {
@@ -18,43 +18,43 @@ public:
     }
     
     int get(int key) {
-    	if(!kPos.count(key))
+    	if(kPos.find(key)==kPos.end())
     		return -1;
-    	auto it = kPos[key];
+    	auto it = kPos[key];//找到对应的迭代器
     	int f = it->f;
     	int v = it->v;
     	if(f == minfreq && freq_list[f].size() == 1)
-    		minfreq++;
-    	freq_list[f].erase(it);
-    	freq_list[++f].push_front(node(key,v,f));
-    	kPos[key] = freq_list[f].begin();
+    		minfreq++;//最小的频数的节点只有1个，被移走了，最小频数+1
+    	freq_list[f].erase(it);//删除
+    	freq_list[++f].push_front(node(key,v,f));//新频数链表加入新节点
+    	kPos[key] = freq_list[f].begin();//记录迭代器位置
     	return v;
     }
     
     void put(int key, int value) {
-    	if(kPos.count(key))
-    	{
+    	if(kPos.find(key)!=kPos.end())
+    	{	//存在key
     		auto it = kPos[key];
 			int f = it->f;
 			if(f == minfreq && freq_list[f].size()==1)
 				minfreq++;
 			freq_list[f].erase(it);
-			freq_list[++f].push_front(node(key,value,f))
-			kPos[key] = m[f].begin();
+			freq_list[++f].push_front(node(key,value,f));
+			kPos[key] = freq_list[f].begin();
     	}
-    	else if(size < cap)
+    	else if(size < cap)//不存在key，但还可插入
     	{
-			minfreq = 1;
-			m[minfreq].push_front(node(key,value,1));
-			kPos[key] = m[1].begin();
+			minfreq = 1;//新插入的只有1次
+			freq_list[minfreq].push_front(node(key,value,1));
+			kPos[key] = freq_list[1].begin();
 			size++;
     	}
-    	else
+    	else if(cap != 0 && size == cap)//不存在key，且满了，且容量不为0
     	{
     		auto Node = freq_list[minfreq].back();
     		int k = Node.k;
-    		freq_list[minfreq].pop_back();
-    		kPos.rease(k);
+    		freq_list[minfreq].pop_back();//频数最小的链表末尾的删除
+    		kPos.erase(k);//删除末尾key对应的迭代器
     		size--;
     		put(key, value);
     	}
