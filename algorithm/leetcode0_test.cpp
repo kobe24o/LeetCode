eef1212cdbcdad62ee86dc83245a42529ed04d1b
kobe_24o <mingmingchen1989@163.com>
2020-04-07 17:39:17 +0800

    lc

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -19,55 +19,60 @@ struct TreeNode {
 };
 using namespace std;
 
+struct cmp1{
+    bool operator()(pair<int,int> a, pair<int,int> b) const
+    {
+        if(a.first==b.first&&a.second==b.second)
+            return false;
+        if(a.first==b.first)
+            return a.second < b.second;
+        return a.first < b.first;
+    }
+};
+struct cmp2{
+    bool operator()(pair<int,int> a, pair<int,int> b) const
+    {
+        if(a.first==b.first&&a.second==b.second)
+            return false;
+        if(a.second==b.second)
+            return a.first < b.first;
+        return a.second < b.second;
+    }
+};
 class Solution {
 public:
-    vector<string> findLongestSubarray(vector<string>& array) {
-        if(array.size() < 2)
-            return {};
-        map<int, pair<int,int>> m;
-        int sum = 0;
-        for(int i = 0; i < array.size(); ++i)
+    int bestSeqAtIndex(vector<int>& height, vector<int>& weight) {
+        int i, j, n = height.size(),maxP = 1;
+        vector<pair<int,int>> p(n);
+        for(i = 0; i < n; ++i)
+            p[i] = {height[i], weight[i]};
+        set<pair<int,int>,cmp1> dp1;
+        set<pair<int,int>,cmp2> dp2;
+        dp1.insert(p[0]);
+        dp1.insert({65,90});
+        for(i = 0; i < n; ++i)
         {
-            if(isdigit(array[i][0]))
-                sum++;
+            auto it = dp1.upper_bound(p[i]);
+            if(it == dp1.end())
+                dp1.insert(p[i]);
             else
-                sum--;
-            if(!m.count(sum))
             {
-                m[sum].first = i;
-                m[sum].second = -1;
+                dp1.erase(it);
+                dp1.insert(p[i]);
             }
-            else if(m[sum].first != -1)
-                m[sum].second = i;
         }
-        if(sum == 0)
-            return array;
-        int maxlen = 0;
-        vector<int> key;
-        for(auto& mi : m)
+        for(i = 0; i < n; ++i)
         {
-            if(mi.second.second == -1)
-                continue;
-            if(mi.second.second-mi.second.first > maxlen)
+            auto it = dp2.upper_bound(p[i]);
+            if(it == dp2.end())
+                dp2.insert(p[i]);
+            else
             {
-                maxlen = mi.second.second-mi.second.first;
-                key.clear();
-                key.push_back(mi.first);
-            }
-            else if(mi.second.second-mi.second.first == maxlen)
-                key.push_back(mi.first);
-        }
-        string minidx = "zzzzzzzzzzzz";
-        int ansKey;
-        for(int k : key)
-        {
-            if(array[m[k].first] < minidx)
-            {
-                minidx = array[m[k].first];
-                ansKey = k;
+                dp2.erase(it);
+                dp2.insert(p[i]);
             }
         }
-        return vector<string> (array.begin()+m[ansKey].first, array.begin()+m[ansKey].second);
+        return max(dp1.size(),dp2.size());
     }
 };
 
@@ -87,14 +92,14 @@ void printv(vector<int>& v)
 }
 int main() {
     vector<vector<char>> v4 = {{'5','3','.','.','7','.','.','.','.'},{'6','.','.','1','9','5','.','.','.'},{'.','9','8','.','.','.','.','6','.'},{'8','.','.','.','6','.','.','.','3'},{'4','.','.','8','.','3','.','.','1'},{'7','.','.','.','2','.','.','.','6'},{'.','6','.','.','.','.','2','8','.'},{'.','.','.','4','1','9','.','.','5'},{'.','.','.','.','8','.','.','7','9'}};
-    vector<int> v1 = {1,2,3,3,4,3,4,5,6};
-    vector<int> v3 = {21,44,5,21,33,38,23,5,25,43};
+    vector<int> v1 = {65,70,56,75,60,68};
+    vector<int> v3 = {100,150,90,190,95,110};
     string str = "eceeeefasdghjklqwertyuio";
     vector<vector<int>> v2 = {{2,4,3},{6,5,2}};
     vector<string> st  = {"42","10","O","t","y","p","g","B","96","H","5","v","P","52","25","96","b","L","Y","z","d","52","3","v","71","J","A","0","v","51","E","k","H","96","21","W","59","I","V","s","59","w","X","33","29","H","32","51","f","i","58","56","66","90","F","10","93","53","85","28","78","d","67","81","T","K","S","l","L","Z","j","5","R","b","44","R","h","B","30","63","z","75","60","m","61","a","5","S","Z","D","2","A","W","k","84","44","96","96","y","M"};
     vector<string> st1 = {"(Uvt,Rqa)","(Qync,Kqjl)","(Fayxe,Upn)","(Maeb,Xaq)","(Pmbz,Vje)","(Hnc,Dma)","(Pwsuo,Gyhh)","(Gyhh,Aasipa)","(Fzoe,Lcp)","(Mgs,Vhdmab)","(Qync,Rgd)","(Gql,Liyd)","(Gyhh,Tkbn)","(Arrugl,Adlue)","(Wbcxi,Slfwyo)","(Yzwm,Vqnjg)","(Lnow,Vhdmab)","(Lvaaz,Rttg)","(Nfimij,Iggrg)","(Vje,Lqrmqh)","(Jylbla,Ljkzhs)","(Jnij,Mlo)","(Adlue,Zqiqe)","(Qync,Rttg)","(Gsiiyo,Vxs)","(Xxcl,Fzoe)","(Dbufek,Xaq)","(Ccqunq,Qszsny)","(Zmeop,Mork)","(Qync,Ngi)","(Zboes,Rmlbnj)","(Yldu,Jxv)","(Padz,Gsiiyo)","(Oip,Utc)","(Tal,Pxzfjg)","(Adlue,Zpm)","(Bbcpth,Mork)","(Qync,Lvaaz)","(Pmbz,Qync)","(Alqw,Ngi)","(Bcs,Maeb)","(Rgbu,Zmeop)"};
     Solution s;
-
+    s.bestSeqAtIndex(v1,v3);
     printv(v1);
     v1.resize(3);
     printv(v1);
