0c90c54dc1d790eb8df686a7d94327ef13d51aee
kobe_24o <mingmingchen1989@163.com>
2020-04-11 20:24:30 +0800

    lc

--- 程序员面试金典/17.17.multi-search-lcci.cpp
+++ 程序员面试金典/17.17.multi-search-lcci.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution {	//TLE
 public:
     vector<vector<int>> multiSearch(string big, vector<string>& smalls) {
     	unordered_map<char,vector<int>> m;
@@ -31,10 +31,52 @@ public:
     	return ans;
     }
 };
-
+//------------Trie
+class trie
+{
+public:
+	bool isEnd = false;
+	trie* next[26] = {NULL};
+	void insert(string& s)
+	{
+		trie *cur = this;
+		for(int i = 0; i < s.size(); ++i)
+		{
+			if(!cur->next[s[i]-'a'])
+				cur->next[s[i]-'a'] = new trie();
+			cur = cur->next[s[i]-'a'];
+		}
+		cur->isEnd = true;
+	}
+};
 class Solution {
 public:
     vector<vector<int>> multiSearch(string big, vector<string>& smalls) {
-    	
+    	trie* t = new trie();
+    	unordered_map<string,int> m;
+    	int i, j, n = smalls.size();
+    	bool found;
+    	vector<vector<int>> ans(n);
+    	for(i = 0; i < n; ++i)
+    		m[smalls[i]] = i;
+    	for(i = 0; i < n; ++i)
+    		t->insert(smalls[i]);
+    	string s;
+    	trie* cur;
+    	for(i = 0; i < big.size(); ++i)
+    	{
+    		s = "";
+    		cur = t;
+    		for(j = i; j < big.size(); ++j)
+    		{
+    			if(!cur->next[big[j]-'a'])
+    				break;
+    			s += big[j];
+    			cur = cur->next[big[j]-'a'];
+    			if(cur->isEnd)
+    				ans[m[s]].push_back(i);
+    		}
+    	}
+    	return ans;
     }
 };
