cc102c6d761c8f017f082c4b4e90738b9c757eb0
kobe_24o <mingmingchen1989@163.com>
2020-04-15 19:15:30 +0800

    Update leetcode0_test.cpp

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -21,17 +21,15 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findSquare(vector<vector<int>>& mat) {
-        if(mat.size()==0 || mat[0].size() == 0)
-            return {};
-        int m = mat.size(), n = mat[0].size(), i, j, k;
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), i, j;
         vector<vector<int>> sumof0Up(m, vector<int>(n,0));//向上连续0个数
         vector<vector<int>> sumof0Left(m, vector<int>(n,0));//向左连续0个数
         for(i = 0; i < m; i++)
         {
             for(j = 0; j < n; j++)
             {
-                if(mat[i][j] == 0)
+                if(grid[i][j] == 0)
                 {
                     if(i==0 && j==0)
                         sumof0Left[i][j] = 1, sumof0Up[i][j] = 1;
@@ -53,45 +51,28 @@ public:
                 }
             }
         }
-        vector<int> ans(3,-1);
-        int edge, x, y;
+        int maxEdge = 0, edge, x, y;
         for(i = m-1; i >= 0; i--)
         {
             for(j = n-1; j >= 0; --j)
             {
                 edge = min(sumof0Up[i][j], sumof0Left[i][j]);
+                //初始边长
                 while(edge > 0)
                 {
-                    if(ans[2] > edge)//肯定小，不必检查了
+                    if(maxEdge > edge)//肯定小，不必检查了
                         break;
-                    x = i-edge+1;
-                    y = j-edge+1;
-                    if(sumof0Up[x][j]>=edge && sumof0Left[i][y]>=edge)
-                    {
-                        if(edge > ans[2])
-                        {
-                            ans[2] = edge;
-                            ans[0] = x;
-                            ans[1] = y;
-                        }
-                        else if(edge == ans[2] && x <= ans[0])
-                        {
-                            if(x < ans[0])
-                            {
-                                ans[0] = x;
-                                ans[1] = y;
-                            }
-                            if(x == ans[0] && y < ans[1])
-                                ans[1] = y;
-                        }
+                    x = i-edge+1;//上方边的x
+                    y = j-edge+1;//左侧边的y
+                    if(sumof0Up[i][y]>=edge && sumof0Left[x][j]>=edge)
+                    {	//左侧边  上侧边长都大于等 edge
+                        maxEdge = max(maxEdge, edge);
                     }
-                    edge--;
+                    edge--;//遍历所有可能
                 }
             }
         }
-        if(ans[0]==-1)
-            return {};
-        return ans;
+        return maxEdge*maxEdge;
     }
 };
 
@@ -111,7 +92,7 @@ void printv(vector<int>& v)
 }
 int main() {
 //    vector<vector<int>> v6 = {{-13260,8589},{1350,8721},{-37222,-19547},{-54293,-29302},{-10489,-13241},{-19382,574},{5561,1033},{-22508,-13241},{-1542,20695},{9277,2820},{-32081,16145},{-50902,23701},{-8636,19504},{-17042,-28765},{-27132,-24156},{-48323,-4607},{30279,29922}};
-    vector<vector<int>> v6 ={{1,0,1},{0,0,1},{0,0,1}};
+    vector<vector<int>> v6 ={{1,1,1},{1,0,1},{1,1,1}};
     vector<int> v1 = {68,130,64};
     vector<int> v2 = {-230,194,7};
     vector<int> v3 = {-1,0};
@@ -121,7 +102,7 @@ int main() {
     vector<string> st  = {"a","aa"};
     vector<string> st1 = {};
     Solution s;
-    v4 = s.findSquare(v6);
+    s.largest1BorderedSquare(v6);
     printv(v4);
 
     string s1 = "1";
