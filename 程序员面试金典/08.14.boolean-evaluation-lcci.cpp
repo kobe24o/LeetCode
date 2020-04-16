198e3e6c41193268a05c4459d96090425db775ab
kobe_24o <mingmingchen1989@163.com>
2020-04-16 13:47:41 +0800

    Update 08.14.boolean-evaluation-lcci.cpp

--- 程序员面试金典/08.14.boolean-evaluation-lcci.cpp
+++ 程序员面试金典/08.14.boolean-evaluation-lcci.cpp
@@ -1,42 +1,45 @@
 class Solution {
-	int sum = 0;
-	int n;
 public:
     int countEval(string s, int result) {
-    	n = s.size()/2; //运算次数
-    	dfs(s,0,result);
-    	return sum;
-    }
-
-    void dfs(string& s, int count, int result)
-    {
-    	if(count==n)
-    	{
-    		if(s[0]-'0' == result)
-    			sum++;
-    		return;
-    	}
-    	string origin, b;
-    	for(int i = 0; i < s.size()/2; ++i)
-    	{
-    		origin = s.substr(2*i,3);
-    		b = operation(origin);
-    		s.replace(2*i,3,b);
-    		dfs(s,count+1,result);
-    		s.replace(2*i,1,origin);
-    	}
-    }
-
-    string operation(string str)
-    {
-    	int a = str[0]-'0', b = str[2]-'0';
-    	int ans;
-    	if(str[1] == '&')
-    		ans = a&b;
-    	else if(str[1] == '|')
-    		ans = a|b;
-    	else
-    		ans = a^b;
-    	return ans==1 ? "1" : "0";
+        if(s=="")
+            return 0;
+        int i, j, n = s.size(), len;
+        vector<vector<int>> dp0(n,vector<int>(n,0));
+        vector<vector<int>> dp1(n,vector<int>(n,0));
+        //dp？[i][j] 表示 区间[i,j]内运算值为 ？ 的方案数
+        for(i = 0; i < n; i+=2)
+        {
+            if(s[i]=='1')
+                dp1[i][i] = 1;
+            else
+                dp0[i][i] = 1;
+        }
+        for(len = 2; len <= n-1; len += 2)
+        {
+            for(i = 0; i < n-len; i += 2)
+            {
+                for(j = i; j <= i+len-2; j+=2)
+                {
+                    if(s[j+1]=='&')
+                    {
+                        dp1[i][i+len] += dp1[i][j]*dp1[j+2][i+len];
+                        dp0[i][i+len] += dp0[i][j]*dp0[j+2][i+len]+dp1[i][j]*dp0[j+2][i+len]+dp0[i][j]*dp1[j+2][i+len];
+                    }
+                    else if(s[j+1]=='|')
+                    {
+                        dp1[i][i+len] += dp1[i][j]*dp1[j+2][i+len]+dp1[i][j]*dp0[j+2][i+len]+dp0[i][j]*dp1[j+2][i+len];
+                        dp0[i][i+len] += dp0[i][j]*dp0[j+2][i+len];
+                    }
+                    else//^
+                    {
+                        dp1[i][i+len] += dp1[i][j]*dp0[j+2][i+len]+dp0[i][j]*dp1[j+2][i+len];
+                        dp0[i][i+len] += dp0[i][j]*dp0[j+2][i+len]+dp1[i][j]*dp1[j+2][i+len];
+                    }
+                }
+            }
+        }
+        if(result)
+            return dp1[0][n-1];
+        return dp0[0][n-1];
     }
 };
