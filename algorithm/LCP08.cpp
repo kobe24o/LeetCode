b119ffdfa8d0a30c34f631f1ba87f8ecc80ad7dc
kobe_24o <mingmingchen1989@163.com>
2020-04-18 22:07:41 +0800

    lc

--- algorithm/LCP08.cpp
+++ algorithm/LCP08.cpp
@@ -1,60 +1,58 @@
-struct cmp1
+struct cmp1//自定义map排序规则
 {
-	bool operator()(const vector<int>& a, const vector<int>& b)const
-	{
-		if(a[0]==b[0])
-		{
-			if(a[1]==b[1])
-				return a[2] < b[2];
-			return a[1] < b[1];
-		}
-		return a[0] < b[0];
-	}
+    bool operator()(const vector<int>& a, const vector<int>& b)const
+    {
+        if(a[0]==b[0])
+        {
+            if(a[1]==b[1])
+                return a[2] < b[2];
+            return a[1] < b[1];
+        }
+        return a[0] < b[0];
+    }
 };
 
 class Solution {
 public:
     vector<int> getTriggerTime(vector<vector<int>>& inc, vector<vector<int>>& req) {
-    	int t = 0, n = req.size(), a = 0, b = 0, c =0;
-    	vector<int> ans(n,-1);
-    	map<vector<int>, int> m;
-    	set<vector<int>,cmp1> set1;
-    	for(int i = 0; i < n; ++i)
-    	{
-    		m[req[i]]=i;
-    		set1.insert(req[i]);
-    	}
+        int t = 0, n = req.size(), a = 0, b = 0, c =0;
+        vector<int> ans(n,-1);
+        multimap<vector<int>, int, cmp1> m;
+        for(int i = 0; i < n; ++i)
+            m.insert(make_pair(req[i],i));
 
-    	vector<int> ok;
-    	vector<int> tp;
-    	auto end1 = set1.upper_bound({a,b,c});
-		for(auto it = set1.begin(); it != end1; ++it)
-		{
-			tp = *it;
-			if(a>=tp[0] && b>=tp[1] && c>=tp[2])
-			{
-    			ans[m[tp]] = t;
-    			set1.erase(tp);
-    		}
-		}
-    	for(int i = 0; i < inc.size(); ++i)
-    	{
-    		t = i+1;
-    		a += inc[i][0];
-    		b += inc[i][1];
-    		c += inc[i][2];
-    		ok.clear();
-    		auto end = set1.upper_bound({a,b,c});
-    		for(auto it = set1.begin(); it != end; ++it)
-    		{
-    			tp = *it;
-    			if(a>=tp[0] && b>=tp[1] && c>=tp[2])
-    			{
-	    			ans[m[tp]] = t;
-	    			set1.erase(tp);
-	    		}
-    		}
-    	}
-    	return ans;
+        vector<int> tp;
+        auto end1 = m.upper_bound({a,b,c});
+        for(auto it = m.begin(); it != end1; )
+        {
+            tp = it->first;
+            if(a>=tp[0] && b>=tp[1] && c>=tp[2])
+            { 
+                ans[it->second] = t;
+                m.erase(it++);//这样做迭代器不会失效
+            }
+            else
+                it++;
+        }
+        for(int i = 0; i < inc.size(); ++i)
+        {
+            t = i+1;
+            a += inc[i][0];
+            b += inc[i][1];
+            c += inc[i][2];
+            auto end = m.upper_bound({a,b,c});
+            for(auto it = m.begin(); it != end; )
+            {
+                tp = it->first;
+                if(a>=tp[0] && b>=tp[1] && c>=tp[2])
+                {
+                    ans[it->second] = t;
+                    m.erase(it++);//这样做迭代器不会失效
+                }
+                else
+                    it++;
+            }
+        }
+        return ans;
     }
 };
--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -20,44 +20,71 @@ struct TreeNode {
 using namespace std;
 
 
-class Solution {
-public:
-    int minJump(vector<int>& jump) {
-        int i, j, n = jump.size(), t = 0, size,tp;
-        vector<bool> vis(n,false);
-        vis[0] = true;
-        set<int> set;
-        for(i = 0; i < n; ++i)
-            set.insert(i);
-        queue<int> q;
-        q.push(0);
-        set.erase(0);
-        while(!q.empty())
+struct cmp1
+{
+    bool operator()(const vector<int>& a, const vector<int>& b)const
+    {
+        if(a[0]==b[0])
         {
-            size = q.size();
-            t++;
-            while(size--)
-            {
-                tp = q.front();
-                q.pop();
-                if(tp+jump[i] >= n)
-                    return t;
-                if(set.count(tp+jump[tp]))
-                {
-                    q.push(tp+jump[tp]);
-                    set.erase(tp+jump[tp]);
-                }
-                auto end = set.upper_bound(tp);
-                for(auto it = set.begin(); it != end; ++it)
-                {
-                    q.push(*it);
-                    set.erase(*it);
-                }
-            }
+            if(a[1]==b[1])
+                return a[2] < b[2];
+            return a[1] < b[1];
         }
-        return t;
+        return a[0] < b[0];
     }
 };
+
+class Solution {
+public:
+    vector<int> getTriggerTime(vector<vector<int>>& inc, vector<vector<int>>& req) {
+        int t = 0, n = req.size(), a = 0, b = 0, c =0;
+        vector<int> ans(n,-1);
+        multimap<vector<int>, int, cmp1> m;
+        // multiset<vector<int>,cmp1> set1;
+        for(int i = 0; i < n; ++i)
+        {
+            m.insert(make_pair(req[i],i));
+            // set1.insert(req[i]);
+        }
+
+        vector<int> tp;
+        auto end1 = m.upper_bound({a,b,c});
+        for(auto it = m.begin(); it != end1; )
+        {
+            tp = it->first;
+            if(a>=tp[0] && b>=tp[1] && c>=tp[2])
+            {
+
+                ans[it->second] = t;
+                m.erase(it++);
+            }
+            else
+                ++it;
+        }
+        for(int i = 0; i < inc.size(); ++i)
+        {
+            t = i+1;
+            a += inc[i][0];
+            b += inc[i][1];
+            c += inc[i][2];
+            auto end = m.upper_bound({a,b,c});
+            for(auto it = m.begin(); it != end; )
+            {
+                tp = it->first;
+                if(a>=tp[0] && b>=tp[1] && c>=tp[2])
+                {
+
+                    ans[it->second] = t;
+                    m.erase(it++);
+                }
+                else
+                    ++it;
+            }
+        }
+        return ans;
+    }
+};
+
 //["01","10","0","1","1001010"] 9
 //["01","0","0101010"]  6
 //["011000","0111010","01101010"] 9
@@ -74,17 +101,17 @@ void printv(vector<int>& v)
 }
 int main() {
 //    vector<vector<int>> v6 = {{-13260,8589},{1350,8721},{-37222,-19547},{-54293,-29302},{-10489,-13241},{-19382,574},{5561,1033},{-22508,-13241},{-1542,20695},{9277,2820},{-32081,16145},{-50902,23701},{-8636,19504},{-17042,-28765},{-27132,-24156},{-48323,-4607},{30279,29922}};
-    vector<vector<int>> v6 ={{1,1,1},{1,0,1},{1,1,1}};
+    vector<vector<int>> v6 ={{2,8,4},{2,5,0},{10,9,8}};
+    vector<vector<int>> v5 ={{2,11,3},{9,17,12},{15,10,7},{9,17,12},{8,1,14}};
     vector<int> v1 = {2,5,1,1,1,1};
     vector<int> v2 = {-230,194,7};
     vector<int> v3 = {-1,0};
     vector<int> v4 = {3,2};
     string str = "eceeeefasdghjklqwertyuio";
-    vector<vector<int>> v5 = {{2,4,3},{6,5,2}};
     vector<string> st  = {"hjhbr", "dixpgflm", "jjzgr", "gb", "ruzih", "zvthz", "rcadj", "agched", "jwvouurr", "hpmyrbq", "rdzfv", "pdffy", "ihsvg", "dihvb", "fhdwixmy", "cpvhj", "x", "aotsh", "qgahgz", "upoij"};
     vector<string> st1 = {};
     Solution s;
-    cout << s.minJump(v1) << endl;
+    v4 = s.getTriggerTime(v6,v5);
     printv(v4);
 
     string s1 = "1";
