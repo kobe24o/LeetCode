ed3ba0e83a0d925cae29c547c6bd0df80daf95c5
kobe_24o <mingmingchen1989@163.com>
2020-04-19 12:28:31 +0800

    Update leetcode0_test.cpp

--- algorithm/leetcode0_test.cpp
+++ algorithm/leetcode0_test.cpp
@@ -35,36 +35,41 @@ struct cmp1
 };
 
 class Solution {
-    char ch[3] = {'a','b','c'};
-    vector<string> ans;
-    string s;
-    int N, K;
 public:
-    string getHappyString(int n, int k) {
-        N = n;
-        K = k;
-        dfs(s);
-        if(k > ans.size())
-            return "";
-        return ans[k];
-    }
-
-    void dfs(string s)
-    {
-        if(ans.size()>=K)
-            return;
-        if(s.size()==N)
+    vector<int> getTriggerTime(vector<vector<int>>& inc, vector<vector<int>>& req) {
+        int t = 0, n = req.size(), a = 0, b = 0, c =0;
+        vector<int> ans(n,-1);
+        multimap<vector<int>, int, cmp1> m;
+        for(int i = 0; i < n; ++i)
         {
-            ans.push_back(s);
-            return;
+            m.insert(make_pair(req[i],i));
         }
-        for(int i = 0; i < 3; ++i)
+
+        vector<int> tp;
+
+        for(int i = -1; i < intinc.size(); ++i)
         {
-            if(s.empty() || s.back()!=ch[i])
+            t = i+1;
+            if(i >=0)
             {
-                dfs(s+ch[i]);
+                a += inc[i][0];
+                b += inc[i][1];
+                c += inc[i][2];
+            }
+            auto end = m.upper_bound({a,b,c});
+            for(auto it = m.begin(); it != end; )
+            {
+                tp = it->first;
+                if(a>=tp[0] && b>=tp[1] && c>=tp[2])
+                {
+                    ans[it->second] = t;
+                    m.erase(it++);//这样做迭代器不会失效
+                }
+                else
+                    it++;
             }
         }
+        return ans;
     }
 };
 //["01","10","0","1","1001010"] 9
@@ -84,7 +89,7 @@ void printv(vector<int>& v)
 int main() {
 //    vector<vector<int>> v6 = {{-13260,8589},{1350,8721},{-37222,-19547},{-54293,-29302},{-10489,-13241},{-19382,574},{5561,1033},{-22508,-13241},{-1542,20695},{9277,2820},{-32081,16145},{-50902,23701},{-8636,19504},{-17042,-28765},{-27132,-24156},{-48323,-4607},{30279,29922}};
     vector<vector<int>> v6 ={{2,8,4},{2,5,0},{10,9,8}};
-    vector<vector<int>> v5 ={{2,11,3},{9,17,12},{15,10,7},{9,17,12},{8,1,14}};
+    vector<vector<int>> v5 ={{2,11,3},{15,10,7},{9,17,12},{8,1,14}};
     vector<int> v1 = {2,5,1,1,1,1};
     vector<int> v2 = {-230,194,7};
     vector<int> v3 = {-1,0};
@@ -93,7 +98,7 @@ int main() {
     vector<string> st  = {"hjhbr", "dixpgflm", "jjzgr", "gb", "ruzih", "zvthz", "rcadj", "agched", "jwvouurr", "hpmyrbq", "rdzfv", "pdffy", "ihsvg", "dihvb", "fhdwixmy", "cpvhj", "x", "aotsh", "qgahgz", "upoij"};
     vector<string> st1 = {};
     Solution s;
-    cout << s.getHappyString(3,9) << endl;
+    s.getTriggerTime(v6,v5);
     printv(v4);
 
     string s1 = "1";
