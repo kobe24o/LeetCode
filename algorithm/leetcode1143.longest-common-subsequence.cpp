cd83c7ef1639315bc3407c28667a2ed95bbbae7b
kobe_24o <mingmingchen1989@163.com>
2020-04-27 14:09:01 +0800

    Create leetcode1143.longest-common-subsequence.cpp

---
+++ algorithm/leetcode1143.longest-common-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+    	int m = text1.size(), n = text2.size(), i, j;
+    	vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    	for(i = 1; i <= m; ++i)
+    	{
+    		for(j = 1; j <= n; ++j)
+    		{
+    			if(text1[i-1] == text2[j-1])
+    				dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
+    			else
+    				dp[i][j] = max(dp[i][j], max(dp[i-1][j], max(dp[i][j-1],dp[i-1][j-1])));
+    		}
+    	}
+    	return dp[m][n];
+    }
+};
