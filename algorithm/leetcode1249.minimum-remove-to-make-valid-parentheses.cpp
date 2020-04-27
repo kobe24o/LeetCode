93df150f5ce8d0e386c371165c729f3c26ac83de
kobe_24o <mingmingchen1989@163.com>
2020-04-27 16:45:13 +0800

    lc

--- algorithm/leetcode1249.minimum-remove-to-make-valid-parentheses.cpp
+++ algorithm/leetcode1249.minimum-remove-to-make-valid-parentheses.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-    	
+        string ans;
+        stack<int> s;
+        unordered_set<int> del;
+        for(int i = 0; i < s.size(); ++i)
+        {
+        	if(s[i] == '(')
+        		s.push(i);
+        	else if(s[i] == ')')
+        	{
+        		if(!s.empty())
+        			s.pop_back();
+        		else
+        			del.insert(i);
+        	}
+        }
+        while(!s.empty())
+        {
+        	del.insert(s.top());
+        	s.pop();
+        }
+        for(int i = 0; i < s.size(); ++i)
+        {
+        	if(s.count(i))
+        		continue;
+        	ans += s[i];
+        }
+        return ans;
     }
 };
