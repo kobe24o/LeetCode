19730fd4ca8ba1a6f6bf78c228e52d50c7bfbe03
kobe_24o <mingmingchen1989@163.com>
2020-04-28 20:22:31 +0800

    Update leetcode873.length-of-longest-fibonacci-subsequence.cpp

--- algorithm/leetcode873.length-of-longest-fibonacci-subsequence.cpp
+++ algorithm/leetcode873.length-of-longest-fibonacci-subsequence.cpp
@@ -26,3 +26,35 @@ public:
     	return maxlen;
     }
 };
+
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& A) {
+    	unordered_map<int,int> m;//val, idx
+        int i, j, prevAi, idx, maxlen = 0, n = A.size();
+    	for(i = 0; i < n; ++i)
+    		m[A[i]] = i;
+    	vector<vector<int>> dp(n,vector<int>(n,0));
+    	//dp[i][j] 表示以 A[i],A[j]结尾的序列长度
+    	for(i = 0; i < n; ++i)
+    		for(j = i+1; j < n; ++j)
+    			dp[i][j] = 2;
+    	for(i = 0; i < A.size(); ++i)
+    	{
+    		for(j = i+1; j < A.size(); ++j)
+    		{
+    			prevAi = A[j]-A[i];//A[i] 前一位数
+                if(m.count(prevAi))
+                {
+                	idx = m[prevAi];
+                	if(idx < i)
+                	{
+                        dp[i][j] = max(dp[i][j],dp[idx][i]+1);
+                        maxlen = max(maxlen,dp[i][j]);
+                    }
+                }
+    		}
+    	}
+    	return maxlen;
+    }
+};
