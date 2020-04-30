d63fa5b9abc92b649896cbeeae6ac11714d3f52f
kobe_24o <mingmingchen1989@163.com>
2020-04-30 12:26:30 +0800

    Update leetcode309.best-time-to-buy-and-sell-stock-with-cooldown.cpp

--- algorithm/leetcode309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ algorithm/leetcode309.best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-    	if(prices.size() <= 1)
-    		return 0;
-    	int i, n = prices.size();
+        if(prices.size() <= 1)
+            return 0;
+        int i, n = prices.size();
         vector<vector<int>> dp(n,vector<int>(2,0));
         dp[0][0] = 0;
         dp[0][1] = -prices[0];
         dp[1][0] = max(dp[0][0], dp[0][1]+prices[1]);
-        dp[1][1] = max(dp[0][1], dp[0][0]-prices[i]);
+        dp[1][1] = max(dp[0][1], dp[0][0]-prices[1]);
         for(i = 2; i < n; ++i)
         {
             dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i]);
-            			// 休息，       前一天有股票，卖出
+                        // 休息，       前一天有股票，卖出
             dp[i][1] = max(dp[i-1][1], dp[i-2][0]-prices[i]);
                         // 休息，       前2天没股票，买入，要隔一天
         }
