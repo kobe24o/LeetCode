21e69dbcb0633a572432ff9f64f22c9a03c09df8
kobe_24o <mingmingchen1989@163.com>
2020-05-03 00:45:51 +0800

    Update leetcode5385.max-difference-you-can-get-from-changing-an-integer.cpp

--- algorithm/leetcode5385.max-difference-you-can-get-from-changing-an-integer.cpp
+++ algorithm/leetcode5385.max-difference-you-can-get-from-changing-an-integer.cpp
@@ -2,60 +2,57 @@ class Solution {
 public:
     int maxDiff(int num) {
         vector<int> big;
-        int n = num, i = 0, first, idx;
-        while(n)
+        int n, i = 0, first;
+        while(num)
         {
-            big.insert(big.begin(),n%10);
-            n /= 10;
+            big.insert(big.begin(),num%10);
+            num /= 10;
         }
         vector<int> small(big);
-        while(i < big.size())
+        n = big.size();
+        while(i < n)
         {
             if(big[i]==9)
                 i++;
             else
                 break;
         }
-        if(i != big.size())
+        if(i != n)
         {
             first = big[i];
-            for( ; i < big.size(); ++i)
+            for( ; i < n; ++i)
                 if(big[i]==first)
-                    big[i] = 9;
+                    big[i] = 9;//换成9
         }
-        if(small[0]==1)
+        if(small[0]==1)//首位是1
         {
             i = 1;
-            while(i < big.size()) {
-            if(small[i]<2)
-                i++;
-            else
-                break;
+            while(i <n) 
+            {
+                if(small[i]<2)
+                    i++;
+                else
+                    break;
             }
-            if(i < big.size())
+            if(i < n)//大于等 2 的 i
             {
                 first = small[i];
-                for( ; i < small.size(); ++i)
+                for( ; i < n; ++i)
                     if(small[i]==first)
-                        small[i] = 0;
+                        small[i] = 0;//都变成0
             }
         }
-        else
+        else//首位 >= 2
         {
             set<int> s;
-            i = 0;
-            int idx;
-            for(; i < big.size(); ++i)
-            {
+            for(i = 0; i < n; ++i)
                 s.insert(small[i]);
-            }
-            if(s.size()==1)
+            if(s.size()==1)//全部一样
             {
-
                 for(i = 0 ; i < small.size(); ++i)
-                        small[i] = 1;
+                    small[i] = 1;//都变成1
             }
-            else
+            else//不都一样把首位变成1
             {
                 first = small[0];
                 for(i = 0 ; i < small.size(); ++i)
@@ -63,13 +60,70 @@ public:
                         small[i] = 1;
             }
         }
-        
         int a =0, b = 0;
         for(int i = 0; i < big.size(); ++i)
             a = a*10+big[i];
         for(int i = 0; i < big.size(); ++i)
             b = b*10+small[i];
-        
+        return a-b;
+    }
+};
+
+class Solution {
+public:
+    int maxDiff(int num) {
+        vector<int> big;
+        int n, i = 0, first;
+        while(num)
+        {
+            big.insert(big.begin(),num%10);
+            num /= 10;
+        }
+        vector<int> small(big);
+        n = big.size();
+        while(i < n)
+        {
+            if(big[i]==9)
+                i++;
+            else
+                break;
+        }
+        if(i != n)
+        {
+            first = big[i];
+            for( ; i < n; ++i)
+                if(big[i]==first)
+                    big[i] = 9;//换成9
+        }
+        i = 0;
+        while(i < n) 
+        {
+            if(small[i]<2)
+                i++;
+            else
+                break;
+        }
+        if(i < n)
+        {
+            first = small[i];
+            if(first == small[0])//等于高位
+            {
+                for(i = 0; i < n; ++i)
+                    if(small[i]==first)
+                        small[i] = 1;//都变成1
+            }
+            else//不等于高位
+            {
+                for(i = 0; i < n; ++i)
+                    if(small[i]==first)
+                        small[i] = 0;//都变成0
+            }
+        }
+        int a =0, b = 0;
+        for(int i = 0; i < big.size(); ++i)
+            a = a*10+big[i];
+        for(int i = 0; i < big.size(); ++i)
+            b = b*10+small[i];
         return a-b;
     }
 };
