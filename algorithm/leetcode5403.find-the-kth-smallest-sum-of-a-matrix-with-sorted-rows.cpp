9182a8f8f030079756e385da4eb786c20a9208ed
kobe_24o <mingmingchen1989@163.com>
2020-05-03 19:13:01 +0800

    Update leetcode5403.find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.cpp

---
+++ algorithm/leetcode5403.find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& mat, int k) {
+    	vector<int> ans(mat[0]);
+    	int i, j, ki;
+    	for(i = 1; i < mat.size(); ++i)
+    	{
+    		multiset<int> s;
+    		for()
+    	}
+    }
+};
