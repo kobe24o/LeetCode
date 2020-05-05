70e14caf5ea773403f0c50ba0dff2cdc9dcd54f0
kobe_24o <mingmingchen1989@163.com>
2020-05-05 23:06:50 +0800

    Update leetcode1140.stone-game-ii.cpp

--- algorithm/leetcode1140.stone-game-ii.cpp
+++ algorithm/leetcode1140.stone-game-ii.cpp
@@ -7,16 +7,23 @@ public:
     		presum[i] = presum[i-1]+piles[i];
     	if(piles.size() <= 2)
     		return piles[n-1];
-    	vector<<vector<int>>> dp(n,vector<int>(n+1,0));
+    	vector<<vector<int>>> dp(n+1,vector<int>(n+1,0));
     	//dp[i][j] 表示 剩余i堆石头，M为j时能得到的最多石头
-    	for(i = 0; i < n; ++i)
-    		for(j = 1; j <= n; ++j)
-    			dp[i][]
-    	for(i = 0; i < n; ++i)
+    	for(i = 1; i <= n; ++i)
     	{
-    		for(j = 1; j <= i+1; ++j)
+    		for(j = i; j <= n; ++j)
     		{
-    			
+    			dp[i][j] = presum[n-1]-presum[n-j-1];//可以全部拿走
+    		}
+    	}
+    	for(i = 1; i <= n; ++i)
+    	{
+    		for(j = 1; j <= i; ++j)
+    		{
+    			for(k = 1; k <= min(2*j, i); ++k)
+    			{
+    				dp[i][j] = max(dp[i][j], presum[n-1]-presum[n-k-1])
+    			}
     		}
     	}
     	return 
