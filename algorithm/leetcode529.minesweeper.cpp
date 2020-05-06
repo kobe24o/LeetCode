61b3ec235e91eea04b2ac97f8707d0a5b81c08d6
kobe_24o <mingmingchen1989@163.com>
2020-05-06 22:53:05 +0800

    Update leetcode529.minesweeper.cpp

--- algorithm/leetcode529.minesweeper.cpp
+++ algorithm/leetcode529.minesweeper.cpp
@@ -48,3 +48,51 @@ public:
     	return board;
     }
 };
+
+class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1},{1,1},{-1,-1},{1,-1},{-1,1}};
+    int m,n;
+public:
+    vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click) {
+    	if(board[click[0]][click[1]] == 'M')
+    	{
+    		board[click[0]][click[1]] = 'X';
+    		return board;
+    	}
+    	m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m,vector<bool>(n,false));
+        visited[click[0]][click[1]] = true;
+    	dfs(board,click[0],click[1],visited);
+        return board;
+    }
+
+    void dfs(vector<vector<char>>& board, int i, int j, vector<vector<bool>>& visited)
+    {
+        int x, y, k, count = 0;
+        for(k = 0; k < 8; ++k)
+        {
+            x = i + dir[k][0];
+            y = j + dir[k][1];
+            if(x>=0 && x<m && y>=0 && y<n && board[x][y]=='M')
+                count++;//8个方向有几颗地雷
+        }
+        if(count == 0)//地雷为0，需要周围的都加入队列，去检查是否继续翻开
+        {
+            board[i][j] = 'B';//中间标记为B
+            for(k = 0; k < 8; ++k)
+            {
+                x = i + dir[k][0];
+                y = j + dir[k][1];
+                if(x>=0 && x<m && y>=0 && y<n && !visited[x][y] && board[x][y]=='E')
+                {
+                    visited[x][y] = true;
+                    dfs(board,x,y,visited);
+                }
+            }
+        }
+        else
+        {	//不为零，标记为数字
+            board[i][j] = char('0'+count);
+        }
+    }
+};
