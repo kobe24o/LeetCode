23571de02ff3fa5ce41bf9083f715cbad9273ee2
kobe_24o <mingmingchen1989@163.com>
2020-05-10 13:02:36 +0800

    Update leetcode923.3sum-with-multiplicity.cpp

--- algorithm/leetcode923.3sum-with-multiplicity.cpp
+++ algorithm/leetcode923.3sum-with-multiplicity.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& A, int target) {
-    	int i, j, k, t, n = A.size(), mod = 1e9+7;
+    	int i, j, k, t, n = A.size(), mod = 1e9+7, count = 0, sum, l, r, n1, n2;
     	sort(A.begin(), A.end());
-    	i = 0, j = n-1;
-    	while(i < j)
+    	for(i = 0; i < n-2; ++i)
     	{
-    		t = target-(A[i]+A[j]);
-    		for(k = i+1; )
+    		j = i+1, k = n-1;
+    		while(j < k)
+    		{
+    			sum = A[i]+A[j]+A[k];
+    			if(sum==target)
+    			{
+    				l = A[j], r = A[k];
+    				n1 = n2 = 0;
+    				while(j < k && A[j]==l)
+    					n1++,j++;
+    				while(j <= k && A[k]==r)
+    					n2++,k--;
+                    if(l != r)
+    				    count += n1*n2;
+                    else
+                        count += (n1+n2)*(n1+n2-1)/2;
+    				count %= mod;
+    			}
+    			else if(sum < target)
+    				j++;
+    			else
+    				k--;
+    		}
     	}
+    	return count;
     }
 };
