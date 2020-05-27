93e40bf81b7c6392970422bd5895abc303770438
kobe_24o <mingmingchen1989@163.com>
2020-05-27 11:05:31 +0800

    Update leetcode390.elimination-game.cpp

--- algorithm/leetcode390.elimination-game.cpp
+++ algorithm/leetcode390.elimination-game.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-    	if(n == 1)	return 1;
-    	if(n == 2 || n == 3 || n == 4 || n == 5) return 2;
-    	if(n == 6 || n == 7) return 4;
-    	if(n%4 == 0 || (n-1)%4 == 0) return 6;
-    	else 
-    		return 8;
+    	if(n == 1)
+    		return 1;
+    	return 2*lastRemaining(n/2) + 2*(n/2)+2;
     }
 };
