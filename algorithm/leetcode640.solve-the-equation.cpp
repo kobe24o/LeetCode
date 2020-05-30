3df75749f0156bfa238b922a3d79dc6617b065eb
kobe_24o <mingmingchen1989@163.com>
2020-05-30 09:14:43 +0800

    lc

--- algorithm/leetcode640.solve-the-equation.cpp
+++ algorithm/leetcode640.solve-the-equation.cpp
@@ -70,3 +70,56 @@ public:
     	return "x="+to_string(ans);
     }
 };
+
+class Solution {
+public:
+    string solveEquation(string equation) {
+        int i, lnum = 0, rnum = 0, lcoe = 0, rcoe = 0, n=0;
+        int pos = equation.find('=');
+        vector<int> coeff = cal_coeff(equation.substr(0,pos));
+        lcoe = coeff[0];
+        lnum = coeff[1];
+        coeff = cal_coeff(equation.substr(pos+1));
+        rcoe = coeff[0];
+        rnum = coeff[1];
+        if(lcoe == rcoe && lnum == rnum)
+            return "Infinite solutions";
+        if(lcoe == rcoe && lnum != rnum)
+            return "No solution";
+        int ans = (lnum-rnum)/(rcoe-lcoe);
+        return "x="+to_string(ans);
+    }
+
+    vector<int> cal_coeff(string equation)
+    {
+        char ch;
+        bool positive = true;
+        int i, n = 0, coe = 0, num = 0;
+        for(i = 0; i < equation.size(); ++i)
+        {
+            ch = equation[i];
+            if(ch == 'x')
+            {
+                coe += n==0 ? ((i>0&&equation[i-1]=='0')? 0 : (positive ? 1 : -1)) : (positive ? n : -n);
+                n=0;
+            }
+            else if(ch == '-')
+            {
+                num += (positive ? n : -n);
+                positive = false;
+                n=0;
+            }
+            else if(ch == '+')
+            {
+                num += (positive ? n : -n);
+                positive = true;
+                n=0;
+            }
+            else
+                n = 10*n+ch-'0';
+        }
+        if(equation[i-1] != 'x')
+            num += (positive ? n : -n);
+        return {coe, num};
+    }
+};
