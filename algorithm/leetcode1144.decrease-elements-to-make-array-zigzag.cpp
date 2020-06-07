c04a70e4de4281fe173c53ac4da7731e3d3289b3
kobe_24o <mingmingchen1989@163.com>
2020-06-07 12:18:01 +0800

    Update leetcode1144.decrease-elements-to-make-array-zigzag.cpp

--- algorithm/leetcode1144.decrease-elements-to-make-array-zigzag.cpp
+++ algorithm/leetcode1144.decrease-elements-to-make-array-zigzag.cpp
@@ -2,34 +2,33 @@ class Solution {
 public:
     int movesToMakeZigzag(vector<int>& nums) {
     	int i = 1, n = nums.size(), op1 = 0, op2 = 0;
-    	int cur1;
+    	int cur;
     	for(i = 0; i < n; i++) 
     	{
+    		cur = nums[i];
     		//目标: 都小于两侧
     		if(i&1)//奇数
     		{
-    			cur1 = nums[i];
-	    		if(cur1 >= nums[i-1])
+	    		if(cur >= nums[i-1])
 	    		{
-	    			op1 += cur1-nums[i-1]+1;
-	    			cur1 = nums[i-1]-1;
+	    			op1 += cur-nums[i-1]+1;
+	    			cur = nums[i-1]-1;
 	    		}
-	    		if(i+1 < n && cur1 >= nums[i+1])
+	    		if(i+1 < n && cur >= nums[i+1])
 	    		{
-	    			op1 += cur1-nums[i+1]+1;
+	    			op1 += cur-nums[i+1]+1;
 	    		}
     		}
     		else
     		{
-    			cur1 = nums[i];
-	    		if(n > 0 && cur1 >= nums[i-1])
+	    		if(i > 0 && cur >= nums[i-1])
 	    		{
-	    			op2 += cur1-nums[i-1]+1;
-	    			cur1 = nums[i-1]-1;
+	    			op2 += cur-nums[i-1]+1;
+	    			cur = nums[i-1]-1;
 	    		}
-	    		if(i+1 < n && cur1 >= nums[i+1])
+	    		if(i+1 < n && cur >= nums[i+1])
 	    		{
-	    			op2 += cur1-nums[i+1]+1;
+	    			op2 += cur-nums[i+1]+1;
 	    		}
     		}
     	}
