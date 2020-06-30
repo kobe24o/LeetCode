b83f5947d1554d95659191813eff5ca05ef67730
kobe_24o <mingmingchen1989@163.com>
2020-07-01 00:13:16 +0800

    Update leetcode718.maximum-length-of-repeated-subarray.py

--- algorithm/Python3_version/leetcode718.maximum-length-of-repeated-subarray.py
+++ algorithm/Python3_version/leetcode718.maximum-length-of-repeated-subarray.py
@@ -5,7 +5,7 @@ class Solution:
         maxlen = 0
         dp = [[0]*(n+1) for _ in range(m+1)]
         for i in range(1,m+1):
-            for j in range(i, n+1):
+            for j in range(1, n+1):
                 if A[i-1] == B[j-1]:
                     dp[i][j] = dp[i-1][j-1]+1
                 maxlen = max(maxlen, dp[i][j])
