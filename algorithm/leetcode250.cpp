08d0e030122b42d9270ae5b98487e0c29970edd9
kobe_24o <mingmingchen1989@163.com>
2020-07-04 19:27:47 +0800

    Update leetcode250.cpp

--- algorithm/leetcode250.cpp
+++ algorithm/leetcode250.cpp
@@ -1,6 +1,23 @@
 class Solution {
+	int count = 0;
 public:
     int countUnivalSubtrees(TreeNode* root) {
-
+    	dfs(root);
+    	return count;
+    }
+    bool dfs(TreeNode* root)
+    {
+    	if(!root) return true;
+    	bool l = dfs(root->left);
+    	bool r = dfs(root->right);
+    	if((!root->left && !root->right) 
+    		||(root->left && !root->right && root->val == root->left->val)
+    		||(!root->left && root->right && root->val == root->right->val)
+    		||(root->left && root->right && root->val == root->left->val && root->val == root->right->val))
+		{
+			count++;
+			return true;
+		}    		
+		return false;
     }
 };
