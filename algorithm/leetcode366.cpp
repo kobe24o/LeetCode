0cef8a50981f04f3d50a343a20ddaf6969fec3aa
kobe_24o <mingmingchen1989@163.com>
2020-07-06 13:54:10 +0800

    Update leetcode366.cpp

--- algorithm/leetcode366.cpp
+++ algorithm/leetcode366.cpp
@@ -1,9 +1,43 @@
 class Solution {
     vector<vector<int>> ans;
+    queue<TreeNode*> q;
+    unordered_map<TreeNode*, int> map;//父节点底下挂着几个子节点
 public:
     vector<vector<int>> findLeaves(TreeNode* root) {
-        if(!root)
-        findLeaves(root->left);
-        findLeaves(root->right);
+        reverse(root);
+        while(!q.empty())
+        {
+        	int size = q.size(), i = 0;
+        	vector<int> lv(size);
+        	while(size--)
+        	{
+        		TreeNode *cur = q.front();
+        		q.pop();
+        		map[cur->left]--;
+        		lv[i++] = cur->val;
+        		if(cur->left && map[cur->left]==0)
+        			q.push(cur->left);
+        	}
+        	ans.push_back(lv);
+        }
+        return ans;
+    }
+    TreeNode* reverse(TreeNode* root)
+    {
+    	if(!root) return NULL;
+    	auto l = root->left;
+    	auto r = root->right;
+    	if(!l && !r)
+    		q.push(root);
+    	map[root] += (l?1:0) + (r?1:0);//记得加括号
+    	root->left = NULL;
+    	root->right = NULL;
+    	auto lc = reverse(l);
+    	auto rc = reverse(r);
+    	if(lc)
+    		lc->left = root;
+    	if(rc)
+    		rc->left = root;
+    	return root;
     }
 };
