077f3bcbb1cc8f057006f288d6a4d57d315e553e
kobe_24o <mingmingchen1989@163.com>
2020-07-09 09:08:00 +0800

    Update leetcode490.cpp

--- algorithm/leetcode490.cpp
+++ algorithm/leetcode490.cpp
@@ -35,3 +35,44 @@ public:
     	return false;
     }
 };
+
+class Solution {
+    int m, n;
+    vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
+    bool found = false;
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        m = maze.size(), n = maze[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n,false));
+        visited[start[0]][start[1]] = true;
+        dfs(maze,start,destination,visited);
+        return found;
+    }
+    void dfs(vector<vector<int>>& maze, vector<int> start, vector<int>& destination, vector<vector<bool>> &visited)
+    {
+        if(found) return;
+        int i = start[0], j = start[1], x, y, k;
+        if(i==destination[0] && j==destination[1])
+        {
+            found = true;
+            return;
+        }
+        for(k = 0; k < 4; ++k)
+        {
+            x = i;
+            y = j;
+            while(x+dir[k][0]>=0 && x+dir[k][0]<m && y+dir[k][1]>=0 && y+dir[k][1]<n
+                    && maze[x+dir[k][0]][y+dir[k][1]]==0)
+            {
+                x += dir[k][0];
+                y += dir[k][1];
+                // visited[x][y] = true;//不能加这一句，一会下面进不了队列
+            }
+            if(!visited[x][y])
+            {
+                visited[x][y] = true;
+                dfs(maze,{x,y},destination,visited);
+            }
+        }
+    }
+};
