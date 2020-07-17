6e54762a8601121950e8a4ef9a4135c44fce8c62
kobe_24o <mingmingchen1989@163.com>
2020-07-17 22:50:21 +0800

    Update leetcode694.cpp

--- algorithm/leetcode694.cpp
+++ algorithm/leetcode694.cpp
@@ -39,3 +39,43 @@ public:
         return s.size();
     }
 };
+
+class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
+    int m, n;
+    set<vector<vector<int>>> s;
+public:
+    int numDistinctIslands(vector<vector<int>>& grid) {
+        if(grid.empty() || grid[0].empty()) return 0;
+        m = grid.size(), n = grid[0].size();
+        for(int i = 0, j; i < m; ++i)
+        {
+            for(j = 0; j < n; ++j)
+            {
+                if(grid[i][j] == 0)
+                    continue;
+                vector<vector<int>> path;
+                grid[i][j] = 0;//访问过
+                DFS(grid,i,j,i,j,path);
+                s.insert(path);
+            }
+        }
+        return s.size();
+    }
+
+    void DFS(vector<vector<int>>& grid, int x0, int y0, int x, int y, vector<vector<int>>& path)
+    {
+        path.push_back({x-x0, y-y0});//路径记录相对坐标
+        int k, nx, ny;
+        for(k = 0; k < 4; ++k)
+        {
+            nx = x + dir[k][0];
+            ny = y + dir[k][1];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny])
+            {
+                grid[nx][ny] = 0;//访问过
+                DFS(grid, x0, y0, nx, ny, path);
+            }
+        }
+    }
+};
