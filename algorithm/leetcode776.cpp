0d544ecc6f87cb8b413a0c3d5879e6a780d115d2
kobe24o <mingmingchen1989@163.com>
2020-07-22 17:36:42 +0800

    Update leetcode776.cpp

--- algorithm/leetcode776.cpp
+++ algorithm/leetcode776.cpp
@@ -1,11 +1,34 @@
 class Solution {
     vector<TreeNode*> ans;
+    bool found = false;
 public:
     vector<TreeNode*> splitBST(TreeNode* root, int V) {
         if(!root) return {NULL, NULL};
         if(root->val <= V)
             return {root, root->right};
         ans.push_back(root);
-        
+        split(root, NULL, 0, V);
+        return ans;
+    }
+    void split(TreeNode* root, TreeNode* fa,int side, int V)
+    {
+        if(!root || found)
+            return;
+        if(root->val <= V && (!root->right || root->right->val > V))
+        {
+            found = true;
+            if(side == 0)
+                fa->left = root->right;
+            else
+                fa->right = root->right;
+            if(ans[0] != root)
+                ans.push_back(root);
+            else
+                ans.push_back(NULL);
+            root->right = NULL;
+            return;
+        }
+        split(root->left, root, 0, V);
+        split(root->right, root, 1, V);
     }
 };
