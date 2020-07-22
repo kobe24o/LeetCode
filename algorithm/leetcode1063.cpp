230a7069aec83b940916c62c39c9abeb0e3042a3
kobe_24o <mingmingchen1989@163.com>
2020-07-22 20:24:02 +0800

    Update leetcode1063.cpp

--- algorithm/leetcode1063.cpp
+++ algorithm/leetcode1063.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     int validSubarrays(vector<int>& nums) {
         stack<int> s;
-        s.push(0);
+        nums.push_back(-1); # 保证最后全部清空栈
         int count = 0;
         for(int i = 0; i < nums.size(); ++i)
         {
-        	while(!s.empty() && nums[i] >= nums[s.top()])
+        	while(!s.empty() && nums[i] < nums[s.top()])
         	{
+                count += i-s.top();
         		s.pop();
         	}
-        	count += i-s.top();
         	s.push(i);
         }
         return count;
