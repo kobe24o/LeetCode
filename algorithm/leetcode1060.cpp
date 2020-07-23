bc81c73c7ed25a73086c0f10c371f2cbbe23d466
kobe24o <mingmingchen1989@163.com>
2020-07-23 16:25:09 +0800

    Update leetcode1060.cpp

--- algorithm/leetcode1060.cpp
+++ algorithm/leetcode1060.cpp
@@ -15,3 +15,27 @@ public:
     	return nums[n-1]+k;
     }
 };
+
+class Solution {
+public:
+    int missingElement(vector<int>& nums, int k) {
+        int l = 0, r = nums.size()-1, mid, miss;
+        while(l <= r)
+        {
+            mid = l+((r-l)>>1);
+            miss = countmissing(nums, mid);
+            if(miss < k)
+                l = mid+1;
+            else if(miss > k)
+                r = mid-1;
+            else
+                return nums[mid]-1;
+
+        }
+        return nums[r]+k-countmissing(nums,r);
+    }
+    int countmissing(vector<int>& nums, int i)
+    {
+        return nums[i]-nums[0]-i;
+    }
+};
