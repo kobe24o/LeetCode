473178818e8a86f372eea5324dd55e6b4335ede1
kobe_24o <mingmingchen1989@163.com>
2020-07-25 10:03:29 +0800

    Update leetcode410.cpp

--- algorithm/leetcode410.cpp
+++ algorithm/leetcode410.cpp
@@ -32,3 +32,20 @@ public:
     	return true;
     }
 };
+
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int n = nums.size(), i, j, k;
+        vector<long long> sum(n+1, 0);
+        for(i = 1; i <= n; ++i)
+            sum[i] = sum[i-1] + nums[i-1];
+        vector<vector<long long>> dp(n+1, vector<long long>(m+1,1e15));
+        dp[0][0] = 0;
+        for(i = 1; i <= n; ++i)
+            for(j = 1; j <= min(i,m); ++j)
+                for(k = 0; k < i; ++k)
+                    dp[i][j] = min(dp[i][j], max(dp[k][j-1], sum[i]-sum[k]));
+        return dp[n][m];
+    }
+};
