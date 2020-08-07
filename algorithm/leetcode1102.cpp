00dff4ec03432c32fb1d02c87ca4a4356451a539
kobe_24o <mingmingchen1989@163.com>
2020-08-07 19:45:12 +0800

    Update leetcode1102.cpp

--- algorithm/leetcode1102.cpp
+++ algorithm/leetcode1102.cpp
@@ -1,42 +1,50 @@
+struct point
+{
+	int x;
+	int y;
+	int val;
+	point(int x0, int y0, int v)
+	{
+		x = x0;
+		y = y0;
+		val = v;
+	}
+};
+struct cmp
+{
+    bool operator()(point& a, point& b) 
+    {
+        return a.val < b.val;
+    }
+};
 class Solution {
-	int m, n;
-	int ans = INT_MAX;
 	vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
 public:
     int maximumMinimumPath(vector<vector<int>>& A) {
-        m = A.size(), n = A[0].size();
+        int m = A.size(), n = A[0].size(), i, j, x, y, k, ans = A[0][0];
         vector<vector<bool>> visited(m, vector<bool>(n,false));
         visited[0][0] = true;
-        dfs(0,0,A, A[0][0], visited);
+        priority_queue<point,vector<point>,cmp> q;
+        q.push(point(0, 0, A[0][0]));
+        while(!q.empty())
+        {
+        	ans = min(ans, q.top().val);
+        	i = q.top().x;
+        	j = q.top().y;
+        	q.pop();
+        	if(i==m-1 && j==n-1)
+        		return ans;
+        	for(k = 0; k < 4; ++k)
+        	{
+        		x = i + dir[k][0];
+        		y = j + dir[k][1];
+        		if(x>=0 && x<m && y>=0 && y<n && !visited[x][y])
+        		{
+        			q.push(point(x, y, A[x][y]));
+        			visited[x][y] = true;
+        		}
+        	}
+        }
         return ans;
-    }
-    void dfs(int x, int y, vector<vector<int>>& A, int minpoint, vector<vector<bool>>& visited)
-    {
-    	if(ans != INT_MAX)
-    		return;
-    	minpoint = min(minpoint, A[x][y]);
-    	if(x==m-1 && y==n-1)
-    	{
-    		ans = minpoint;
-    		return;
-    	}
-    	int i,j,k, nx, ny, maxval = 0;
-    	vector<vector<int>> around;
-    	for(k = 0; k < 4; ++k)
-    	{
-    		i = x + dir[k][0];
-    		j = y + dir[k][1];
-    		if(i>=0 && i<m && j>=0 && j<n && !visited[i][j])
-    			around.push_back({i,j});
-    	}
-    	sort(around.begin(), around.end(),[&](auto a, auto b){
-    		return A[a[0]][a[1]] > A[b[0]][b[1]];
-    	});
-    	for(auto& pos : around)
-    	{
-    		visited[pos[0]][pos[1]] = true;
-    		dfs(pos[0],pos[1],A,min(minpoint,A[pos[0]][pos[1]]),visited);
-    	}
-    	
     }
 };
