877f5a3bd35705fa47785a09f039b6296f03b7df
kobe_24o <mingmingchen1989@163.com>
2020-08-08 20:02:01 +0800

    Create leetcode353.cpp

---
+++ algorithm/leetcode353.cpp
@@ -0,0 +1,19 @@
+class SnakeGame {
+public:
+    /** Initialize your data structure here.
+        @param width - screen width
+        @param height - screen height 
+        @param food - A list of food positions
+        E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
+    SnakeGame(int width, int height, vector<vector<int>>& food) {
+        
+    }
+    
+    /** Moves the snake.
+        @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
+        @return The game's score after the move. Return -1 if game over. 
+        Game over when snake crosses the screen boundary or bites its body. */
+    int move(string direction) {
+        
+    }
+};
