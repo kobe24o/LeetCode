c3676e25bd1c49a09b9d5bc6a28b3454c60ff9e6
kobe_24o <mingmingchen1989@163.com>
2020-08-08 21:37:11 +0800

    lc

--- LeetCode解题汇总目录.md
+++ LeetCode解题汇总目录.md
@@ -390,6 +390,7 @@
 | [LeetCode 349. 两个数组的交集（哈希）](https://blog.csdn.net/qq_21201267/article/details/100945076) |                                                              |
 | [LeetCode 350. 两个数组的交集 II（哈希）](https://blog.csdn.net/qq_21201267/article/details/100995174) |                                                              |
 | [LeetCode 351. 安卓系统手势解锁（回溯）](https://michael.blog.csdn.net/article/details/107738912) |                                                              |
+| [LeetCode 353. 贪吃蛇（deque+set）](https://michael.blog.csdn.net/article/details/107886222) |                                                              |
 | [LeetCode 354. 俄罗斯套娃信封问题（最长上升子序 DP/二分查找）](https://michael.blog.csdn.net/article/details/105370146) |                                                              |
 | [LeetCode 355. 设计推特（哈希map+set）](https://michael.blog.csdn.net/article/details/105480197) |                                                              |
 | [LeetCode 356. 直线镜像](https://michael.blog.csdn.net/article/details/107528027) |                                                              |
--- algorithm/leetcode353.cpp
+++ algorithm/leetcode353.cpp
@@ -1,4 +1,10 @@
 class SnakeGame {
+    vector<vector<int>> food;
+    int m, n, i = 0, score = 0;
+    int x = 0, y = 0;
+    unordered_map<string, vector<int>> dir;
+    deque<pair<int,int>> body;
+    set<pair<int,int>> body_set;
 public:
     /** Initialize your data structure here.
         @param width - screen width
@@ -6,7 +12,15 @@ public:
         @param food - A list of food positions
         E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
     SnakeGame(int width, int height, vector<vector<int>>& food) {
-        
+        this->food = food;
+        n = width;
+        m = height;
+        dir["R"] = {0,1};
+        dir["D"] = {1,0};
+        dir["U"] = {-1,0};
+        dir["L"] = {0,-1};
+        body.push_back({0,0});
+        body_set.insert({0,0});
     }
     
     /** Moves the snake.
@@ -14,6 +28,27 @@ public:
         @return The game's score after the move. Return -1 if game over. 
         Game over when snake crosses the screen boundary or bites its body. */
     int move(string direction) {
-        
+        x += dir[direction][0];
+        y += dir[direction][1];
+        if(!(x>=0 && x<m && y>=0 && y<n)) 
+            return -1;//出界
+        if(i < food.size() && x == food[i][0] && y == food[i][1])
+        {   //吃到食物
+            body.push_front({x, y});
+            body_set.insert({x, y});
+            i++;
+            score++;
+        }
+        else//没吃到
+        {
+            pair<int,int> tail = body.back();
+            body_set.erase(tail);
+            body.pop_back();
+            body.push_front({x, y});
+            if(body_set.count({x,y}))
+                return -1;
+            body_set.insert({x,y});
+        }
+        return score;
     }
 };
