7461e12ab5f5d51f2a4c9a72b81bf57b07cdb857
kobe_24o <mingmingchen1989@163.com>
2020-08-12 21:32:31 +0800

    LC

--- algorithm/leetcode759.cpp
+++ algorithm/leetcode759.cpp
@@ -17,6 +17,26 @@ public:
 class Solution {
 public:
     vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
-        
+        int l = -1;
+        vector<Interval> v;
+        for(auto& s: schedule) 
+        	for(auto& i : s)
+        	{
+        		v.push_back(i);
+        		l = min(l, i.end);
+        	}
+        sort(v.begin(), v.end(), [&](auto& a, auto& b){
+        	if(a.start == b.start)
+        		return a.end < b.end;
+        	return a.start < b.start;
+        });
+        vector<Interval> ans;
+        for(int i = 0; i < v.size(); ++i)
+        {
+        	if(l < v[i].start && l != -1)
+        		ans.push_back(Interval(l, v[i].start));
+        	l = max(l, v[i].end);
+        }
+        return ans;
     }
 };
