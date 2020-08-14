6ca74fa3fbabc591428510dd104e76f20b7707ae
kobe24o <mingmingchen1989@163.com>
2020-08-14 09:59:37 +0800

    Update leetcode473.cpp

--- algorithm/leetcode473.cpp
+++ algorithm/leetcode473.cpp
@@ -1,8 +1,37 @@
 class Solution {
+	bool found = false;
+	int part, sum, count = 0;
 public:
     bool makesquare(vector<int>& nums) {
-    	int sum = accumulate(nums.begin(), nums.end(), 0);
+    	sum = accumulate(nums.begin(), nums.end(), 0);
     	if(sum%4) return false;
-    	
+    	part = sum/4;
+    	vector<bool> visited(nums.size(), false);
+    	bt(nums, visited, 0);
+    	return found;
+    }
+    void bt(vector<int>& nums, vector<bool>& visited, int s)
+    {
+    	if(s > part || found) return;
+    	if(s == part)
+    	{
+    		count++;
+    		s = 0;
+    		if(count==4)
+    		{
+    			found = true;
+    			return;
+    		}
+    	}
+    	for(int i = 0; i < nums.size(); ++i)
+    	{
+    		if(visited[i])
+    			continue;
+    		visited[i] = true;
+    		s += nums[i];
+    		bt(nums, visited, s);
+    		s -= nums[i];
+    		visited[i] = false;
+    	}
     }
 };
