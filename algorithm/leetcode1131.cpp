28c9abc227661781d27da8a64cb8f8c70b1dbf58
kobe24o <mingmingchen1989@163.com>
2020-08-17 18:00:15 +0800

    Update leetcode1131.cpp

--- algorithm/leetcode1131.cpp
+++ algorithm/leetcode1131.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
     	int min1 = INT_MAX, min2 = INT_MAX, min3 = INT_MAX, min4 = INT_MAX;
-    	int max1 = INT_MAX, max2 = INT_MAX, max3 = INT_MAX, max4 = INT_MAX;
+    	int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN, max4 = INT_MIN;
     	for(int i = 0; i < arr1.size(); ++i)
     	{
     		max1 = max(max1, arr1[i] + arr2[i] + i);
-    		max2 = max(max2,-arr1[i] - arr2[i] - i);
-    		max3 = max(max3,-arr1[i] + arr2[i] + i);
-    		max4 = max(max4, arr1[i] - arr2[i] + i);
-    		min1 = min(min1, arr1[i] + arr2[i] - i);
-    		min2 = min(min2,-arr1[i] - arr2[i] + i);
-    		min3 = min(min3, arr1[i] - arr2[i] - i);
-    		min4 = min(min4,-arr1[i] + arr2[i] - i);
+    		max2 = max(max2,-arr1[i] + arr2[i] + i);
+    		max3 = max(max3, arr1[i] - arr2[i] + i);
+    		max4 = max(max4, arr1[i] + arr2[i] - i);
+    		min1 = min(min1, arr1[i] + arr2[i] + i);
+    		min2 = min(min2,-arr1[i] + arr2[i] + i);
+    		min3 = min(min3, arr1[i] - arr2[i] + i);
+    		min4 = min(min4, arr1[i] + arr2[i] - i);
     	}
-    	return ;
+    	return max(max(max1-min1, max2-min2),max(max3-min3, max4-min4));
     }
 };
