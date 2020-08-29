e12f89a9dff50c5141798629f0a629414e2bc2c0
kobe_24o <mingmingchen1989@163.com>
2020-08-29 23:41:28 +0800

    Update leetcode214.cpp

--- algorithm/leetcode214.cpp
+++ algorithm/leetcode214.cpp
@@ -1,44 +1,19 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        if(s.empty()) return "";
-        if(ispalindrom(s))
-            return s;
-    	int n = s.size(), idx, maxlen = 0;
-        string t;
-        bool odd;
-    	for(int i = 0; i <= n/2; ++i)
+    	long long n = s.size(), i, idx = -1, base = 1, mod = 1e9+7;
+    	long long left = 0, right = 0;
+    	for(i = 0; i < n; i++) 
     	{
-            t = s.substr(i, i+1);
-            reverse(t.begin(), t.end());
-    		if(s.substr(0,i+1) == t )
-    		{
+    		left = (left*131 + s[i])%mod;
+    		right = (right + s[i]*base)%mod;
+    		if(left == right)
     			idx = i;
-                odd = true;
-    		}
-            t = s.substr(i+1, i+1);
-            reverse(t.begin(), t.end());
-            if(s.substr(0,i+1) == t )
-    		{
-    			idx = i+1;
-                odd = false;
-    		}
+    		base *= 131;
+            base %= mod;
     	}
-        if(odd)
-    	    t = s.substr(2*idx+1);
-        else
-            t = s.substr(2*idx);
-    	reverse(t.begin(), t.end());
-    	return t+s;
-    }
-    bool ispalindrom(string &s)
-    {
-        int l = 0, r = s.size()-1;
-        while(l < r)
-        {
-            if(s[l++] != s[r--])
-                return false;
-        }
-        return true;
+    	string A = s.substr(idx+1);
+    	reverse(A.begin(), A.end());
+    	return A+s;
     }
 };
