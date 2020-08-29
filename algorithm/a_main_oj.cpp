01ed1dcd2b630b341016606dadad6a9a5506aca6
kobe_24o <mingmingchen1989@163.com>
2020-08-29 17:04:34 +0800

    Update a_main_oj.cpp

--- algorithm/a_main_oj.cpp
+++ algorithm/a_main_oj.cpp
@@ -3,79 +3,60 @@
 #include<unordered_set>
 #include<random>
 #include<set>
+#include<algorithm>
 using namespace std;
 #include <string>     // std::string, std::stoi
-
-
-#include<iostream>
-#include<vector>
-#include<unordered_set>
-#include<random>
-#include<set>
-using namespace std;
-#include <string>     // std::string, std::stoi
-
+#include <map>
+#include<stack>
 
 #include<iostream>
 #include<string>
 using namespace std;
 
-const int n = 3;
-class Patient
-{
-private:
-    int m_Id;
-    string m_Name;
-    double* m_temperature;//指向包含早中晚3个体温值的堆区数组
+class Solution {
 public:
-    Patient(int = 0, string = "#", double*t = NULL);
-    Patient(const Patient& p);
-    ~Patient();
-
-    //此处添加友元函数4
-    friend ostream& operator<<(ostream& out, Patient& p);
+    /**
+     * @param heights: the heights of buildings.
+     * @param k: the vision.
+     * @param x: the energy to spend of the first action.
+     * @param y: the energy to spend of the second action.
+     * @return: the minimal energy to spend.
+     */
+    long long shuttleInBuildings(vector<int> &heights, int k, int x, int y) {
+        // write your code here.
+        int n = heights.size(), i, j, K;
+        stack<int> s;
+        vector<int> rightTall(n, -1);
+        for(int i = n-1; i >= 0; i--)
+        {
+            while(!s.empty() && heights[s.top()] <= heights[i])
+                s.pop();
+            if(!s.empty() && s.top()-i <= k)
+                rightTall[i] = s.top();
+            s.push(i);
+        }
+        vector<long long> dp(n, LONG_LONG_MAX);
+        dp[0] = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(dp[i] == LONG_LONG_MAX)
+                continue;
+            if(i+1 < n)
+                dp[i+1] = min(dp[i+1], dp[i]+y);
+            if(i+2 < n)
+                dp[i+2] = min(dp[i+2], dp[i]+y);
+            if(rightTall[i] != -1)
+                dp[rightTall[i]] = min(dp[rightTall[i]], dp[i]+x);
+        }
+        return dp[n-1];
+    }
 };
-Patient::Patient(int Id, string Name, double* temperature)//此处添加构造函数，析构函数定义2
-{
-    m_Id = Id;
-    m_Name = Name;
-    if(temperature) {
-        this->m_temperature = new double[3];
-        for (int i = 0; i < 3; i++)
-            this->m_temperature[i] = temperature[i];
-    }
-}
-Patient::Patient(const Patient& p)//此处添加深复制构造函数3
-{
-    m_Id = p.m_Id;
-    m_Name = p.m_Name;
-    if(p.m_temperature) {
-        this->m_temperature = new double[3];
-        for (int i = 0; i < 3; i++)
-            this->m_temperature[i] = p.m_temperature[i];
-    }
-}
-Patient::~Patient()
-{
-    if (m_temperature != NULL)
-    {
-        delete[] m_temperature;
-    }
-}
-//友元函数
-ostream& operator<<(ostream& out, Patient& p)
-{
-    out << "Id:" << p.m_Id << "  名字:" << p.m_Name<<" "<<endl;
-    return out;
-}
 int main()
 {
-    double temp[n] = { 37.5, 38, 37.8 };
-    int id = 0;
-    Patient pat1(1, "Rose", temp), pat2(2, "Tom"), pat3(pat1);
-
-    cout << pat1 << pat2 << pat3;
-
+    vector<int> l = {1,5,4,3,3,5};
+    Solution s;
+    string str = "bacbdab";
+    cout << s.shuttleInBuildings(l, 3,10, 6)<<endl;
     //读出二进制文件中的内容并输出到显示器6
     system("pause");
     return 0;
