4dfd2dda0f4eadfc446a0bb17c8d78d87b6674d2
kobe24o <mingmingchen1989@163.com>
2020-09-04 17:18:15 +0800

    Create leetcode1039.cpp

---
+++ algorithm/leetcode1039.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minScoreTriangulation(vector<int>& A) {
+    	int n = A.size(), len, i, j, k;
+    	vector<vector<int>> dp(n, vector<int>(n, 1));
+    	for(len = 3; len < n; ++len)
+    	{
+    		for(i = 0; i < n; ++i)
+    		{
+    			
+    		}
+    	}
+    }
+};
