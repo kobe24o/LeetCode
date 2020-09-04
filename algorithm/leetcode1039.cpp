c9e6e371d1bc8cdb701c288aeb8abfbc873ce47c
kobe24o <mingmingchen1989@163.com>
2020-09-04 17:35:58 +0800

    Update leetcode1039.cpp

--- algorithm/leetcode1039.cpp
+++ algorithm/leetcode1039.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     int minScoreTriangulation(vector<int>& A) {
     	int n = A.size(), len, i, j, k;
-    	vector<vector<int>> dp(n, vector<int>(n, 1));
-    	for(len = 3; len < n; ++len)
+    	vector<vector<int>> dp(n, vector<int>(n, 0));
+    	for(len = 3; len <= n; ++len)
     	{
     		for(i = 0; i < n; ++i)
     		{
-    			
+    			j = i+len-1;
+    			if(j >= n) continue;
+    			dp[i][j] = INT_MAX;
+    			for(k = i+1; k < j; ++k)
+    			{
+    				dp[i][j] = min(dp[i][j], dp[i][k]+A[i]*A[k]*A[j]+dp[k][j]);
+    			}
     		}
     	}
+    	return dp[0][n-1];
     }
 };
