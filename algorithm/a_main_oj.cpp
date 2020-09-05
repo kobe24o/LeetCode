e5d296dc8936b014d8f34c87c67dffd23a66bb9a
kobe_24o <mingmingchen1989@163.com>
2020-09-05 19:45:32 +0800

    Update a_main_oj.cpp

--- algorithm/a_main_oj.cpp
+++ algorithm/a_main_oj.cpp
@@ -16,41 +16,43 @@ using namespace std;
 class Solution {
 public:
     /**
-     * @param triangle: Coordinates of three points
-     * @param point: Xiaoqi's coordinates
-     * @return: Judge whether you can cast magic
+     * @param a: Left margin
+     * @param b: Right margin
+     * @return: return the greatest common multiple
      */
-    string castMagic(vector<vector<int>> &triangle, vector<int> &point) {
+    long long greatestcommonmultiple(int a, int b) {
         // write your code here
-        for(int i = 0; i < 3; i++)
-        {
-            if(!ok(triangle[i],triangle[(i+1)%3],triangle[(i+2)%3],point))
-                return "No";
-        }
-        return "Yes";
+        int lcm3 = lcm_(b,b-1,b-2);
+        int lcm_3_1 = 0, lcm4 = 0, lcm5 = 0;
+        if(b-3 >= a)
+            int lcm_3_1 = lcm_(b-3, b-1, b-2);
+        if(b%2==1)
+            lcm4 = lcm_(b,b-1,b-2);
+        if(b%2==0 && b-3>=a)
+            lcm5 = lcm_((b),(b-1),(b-3));
+        return max(lcm3, max(lcm_3_1,max(lcm4, lcm5)));
     }
-
-    bool ok(vector<int> &p1, vector<int> &p2, vector<int> &p3, vector<int> &p)
+    int lcm(int a, int b)
     {
-        int x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1], x3 = p3[0], y3 = p3[1];
-        int xi = p[0], yi = p[1];
-        int v1 = (y2-y1)*(x3-x1)-(y3-y1)*(x2-x1);
-        int v2 = (y2-y1)*(xi-x1)-(yi-y1)*(x2-x1);
-        cout << v1 << " " << v2 << endl;
-        if(v1*v2 < 0)//异号，说明，我的位置不在第3点的一侧
-            return false;
-        return true;
+        return a*b/__gcd(a,b);
+    }
+    int lcm_(int a, int b, int c)
+    {
+        int l = lcm(a,b);
+        return lcm(c, l);
     }
 };
+
 int main()
 {
     vector<int> l = {2,1};
-    vector<vector<int>> t = {{0,0},{2,0},{1,1}};
+    vector<vector<int>> t = {{14,2,11},{11,14,5},{14,3,10}};
+    vector<vector<int>> t1 = {{14,7,11},{11,14,5},{14,3,10}};
     Solution s;
     string str = "bacbdab";
-    cout << s.castMagic(t, l) <<endl;
+    cout << s.lcm_(5,7,6) <<endl;
     //读出二进制文件中的内容并输出到显示器6
-    system("pause");
+
     return 0;
 }
 
