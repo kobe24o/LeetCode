9a261eb681387ebbe1ce72cb6a065d42bc8387bf
kobe24o <mingmingchen1989@163.com>
2020-09-08 17:32:39 +0800

    Update leetcode967.cpp

--- algorithm/leetcode967.cpp
+++ algorithm/leetcode967.cpp
@@ -22,3 +22,34 @@ public:
     		dfs(count+1, N, K, num*10+last-K);
     }
 };
+
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> numsSameConsecDiff(int N, int K) {
+        if(N == 1)
+            return {0,1,2,3,4,5,6,7,8,9};
+        queue<int> q;
+        for(int i = 1; i <= 9; ++i)
+            q.push(i);
+        int cur, lastbit, size, len = 1;
+        while(!q.empty())
+        {
+            size = q.size();
+            while(size--)
+            {
+                cur = q.front();
+                q.pop();
+                if(len == N)
+                    ans.push_back(cur);
+                lastbit = cur%10;
+                if(lastbit+K <= 9 && len < N)
+                    q.push(cur*10+lastbit+K);
+                if(lastbit-K >= 0 && len < N && K != 0) // K 为0,跟上面重复了
+                    q.push(cur*10+lastbit-K);
+            }
+            ++len;
+        }
+        return ans;
+    }
+};
