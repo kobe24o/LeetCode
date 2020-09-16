0a8fe26a887da3d9d348f4d6e0d6c9e0419d3b14
kobe24o <mingmingchen1989@163.com>
2020-09-16 10:47:11 +0800

    Update leetcode1031.cpp

--- algorithm/leetcode1031.cpp
+++ algorithm/leetcode1031.cpp
@@ -22,3 +22,23 @@ public:
         return maxsum;
     }
 };
+
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& A, int L, int M) {
+        int n = A.size(), i;
+        vector<int> sum(A);
+        for(i = 1; i < n; i++) 
+            sum[i] = sum[i-1]+A[i];
+        int maxsum = sum[L+M-1], maxsumL = sum[L-1], maxsumM = sum[M-1];
+        for(i = L+M; i < n; i++)
+        {
+            maxsumL = max(maxsumL, sum[i-M]-sum[i-M-L]);
+            maxsumM = max(maxsumM, sum[i-L]-sum[i-L-M]);
+            maxsum = max(maxsum, 
+                            max(maxsumL+sum[i]-sum[i-M], 
+                                maxsumM+sum[i]-sum[i-L]));
+        }
+        return maxsum;
+    }
+};
