ff9897bcfaaa0eb55da2e9693d4d7734cf2815f3
kobe_24o <mingmingchen1989@163.com>
2020-09-26 13:50:06 +0800

    Update a_main_oj.cpp

--- algorithm/a_main_oj.cpp
+++ algorithm/a_main_oj.cpp
@@ -17,56 +17,45 @@ using namespace std;
 typedef long long ll;
 
 class Solution {
-    int local = 0, global = 0;
-    vector<int> tmp;
 public:
-    bool isIdealPermutation(vector<int>& A) {
-        if(A.size() <= 1) return true;
-        for(int i = 0; i < A.size()-1; ++i)
-            if(A[i] > A[i+1])
-                local++;
-        tmp.resize(A.size());
-        mergesort(A, 0, A.size()-1);
-        cout << global << endl;
-        return local == global;
-    }
-    void mergesort(vector<int>& A, int l, int r)
-    {
-        if(l >= r) return;
-        int mid = (l+r)/2;
-        mergesort(A, l, mid-1);
-        mergesort(A, mid, r);
-        int i = l, j = mid, k = 0;
-        while(i < mid && j <= r)
+    /**
+     * @param n: The folding times
+     * @return: the 01 string
+     */
+    string getString(int n) {
+        // Write your code here
+        if(n==1) return "0";
+        string ans, temp = "0";
+        while(--n)
         {
-            if(A[i] <= A[j])
-                tmp[k++] = A[i++];
-            else//前面没出队的比后面已经出来的都大
+            int flag = 0, size = temp.size();
+            for(int i = 0; i < temp.size(); i++)
             {
-                tmp[k++] = A[j++];
-                global += mid-i;
+                ans += string(1, flag+'0')+temp[i];
+                flag = (flag==0 ? 1 : 0);
             }
+            ans += "1";
+            temp = ans;
+            ans = "";
         }
-        while(i < mid)
-        {
-            tmp[k++] = A[i++];
-        }
-        while(j <= r)
-            tmp[k++] = A[j++];
-        k = 0; i = l;
-        while(i <= r)
-            A[i++] = tmp[k++];
+        return temp;
     }
 };
-
+//"0010 0110 0100 111"
+//001001100100111
+//001001100100111
 int main()
 {
     vector<int> l = {6,3,5};
     vector<vector<int>> t = {{1,2},{2,3},{3,4},{4,1},{2,5},{5,6}};
     vector<vector<int>> t1 = {{15,96},{36,2}};
      Solution s;
-    string str = "bacbdab";
-     cout << s.isIdealPermutation(l) <<endl;
+    string str = "abb";
+
+    cout << s.getString(3) <<endl;
+
+
+
     cout << "hello" << endl;
     system("pause");
     return 0;
