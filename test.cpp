b77f2a594a80a9b723733539f9d5f82272668f6c
kobe24o <mingmingchen1989@163.com>
2020-10-13 15:53:33 +0800

    lc

---
+++ test.cpp
@@ -0,0 +1,11 @@
+// 这是一个调试测试文件，您可以直接运行检测配置是否生效
+// 若配置任然无法调试，请查阅README_inProject.md
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Congratulasions! It works!\n");
+    system("pause");
+    return 0;
+}
