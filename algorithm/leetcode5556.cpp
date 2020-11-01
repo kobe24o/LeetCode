c8481bc3816a42539716658544589e2754ffcfc2
kobe_24o <mingmingchen1989@163.com>
2020-11-01 22:22:20 +0800

    Update leetcode5556.cpp

--- algorithm/leetcode5556.cpp
+++ algorithm/leetcode5556.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-    	int l = 0; r = heights.size()-1, ans = 0, mid;
+    	int l = 1, r = heights.size()-1, ans = 0, mid;
     	vector<int> gap(heights.size()-1);
     	for(int i = 1; i < heights.size(); i++)
     		gap[i-1] = heights[i]-heights[i-1];
@@ -20,6 +20,18 @@ public:
     }
     bool canjump(vector<int>& gap, int bricks, int ladder, int dest)
     {
-    	int 
+    	vector<int> g(gap.begin(), gap.begin()+dest);
+    	sort(g.begin(), g.end(), greater<int>());
+        int i = 0;
+    	for(; i < g.size(); i++)
+    	{
+    		if(g[i] > 0 && ladder)
+    			ladder--;
+    		else if(g[i] > 0 && bricks-g[i] >= 0)
+    			bricks -= g[i];
+            else if(ladder==0 && bricks-g[i] < 0)
+                break;
+    	}
+    	return i == g.size();
     }
 };
