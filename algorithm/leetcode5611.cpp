dd26bbf1046efafe3077a360087368e6cba02d64
kobe_24o <mingmingchen1989@163.com>
2020-12-13 00:37:16 +0800

    Update leetcode5611.cpp

--- algorithm/leetcode5611.cpp
+++ algorithm/leetcode5611.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
         int n = aliceValues.size();
-        vector<pair<int, int>> diff(n);
+        vector<pair<int, int>> delta(n);
         for(int i = 0; i < n; i++) 
         {
-            diff[i].first = aliceValues[i]+bobValues[i];
-            diff[i].second = i;
+            delta[i].first = aliceValues[i]+bobValues[i];//和大的优先
+            delta[i].second = i;
         }
-        sort(diff.rbegin(), diff.rend());
-        int a = 0, b = 0, i = 0, j = n-1;
+        sort(delta.rbegin(), delta.rend());//和大的优先
+        int a = 0, b = 0;
         bool alice = true;
-        while(i < n)
+        for(int i = 0; i < n; ++i)
         {
             if(alice)
-            {
-                a += aliceValues[diff[i].second];
-                i++;
-                alice = false;
-            }
+                a += aliceValues[delta[i].second];
             else
-            {
-                b += bobValues[diff[i].second];
-                i++;
-                alice = true;
-            }
+                b += bobValues[delta[i].second];
+            alice = !alice;
         }
         if(a > b) return 1;
         else if(a < b) return -1;
