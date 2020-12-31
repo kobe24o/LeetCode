aadfc3f4f767f0a0b22c9934937b78977fa33f2a
kobe_24o <mingmingchen1989@163.com>
2020-12-31 21:41:26 +0800

    LC

--- algorithm/a_main_oj.cpp
+++ algorithm/a_main_oj.cpp
@@ -12,46 +12,138 @@ void print2Dvector(vector<vector<int>>& a)
     }
 }
 
-struct cmp{
-    bool operator()(char a, char b) const
-    {
-        return a > b;
-    }
-};
-// Solution
-class Solution {
+class node
+{
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        int n = startTime.size();
-        vector<int> id(n);
-        iota(id.begin(), id.end(), 0);
-        sort(id.begin(), id.end(),[&](auto a, auto b){
-            return endTime[a] < endTime[b];
-        });
-        map<int,int> dp;
-        dp[0] = 0;
-        dp[endTime[id[0]]] = profit[id[0]];
-        int ans = profit[id[0]];
-        for(int i = 1; i < n; i++)
-        {
-            int idx = id[i];
-            auto it = dp.upper_bound(startTime[idx]);
-            int dp_prev = (--it)->second;
-            dp[endTime[idx]] = max(dp[endTime[idx]], dp_prev+profit[idx]);
-            ans = max(ans, dp[endTime[idx]]);
-        }
-
-        return ans;
+    int push, bi, bj, pi, pj;
+    node(int n, int a, int b, int c, int d)
+    {
+        push = n;
+        bi = a;
+        bj = b;
+        pi = c;
+        pj = d;
     }
 };
+struct hashf
+{
+    unsigned long long operator()(node v) const
+    {
+        return 1ULL*v.bi*pow(21,3)+v.bj*pow(21,2)+v.pi*21+v.pj;
+    }
+};
+struct eqf
+{
+    bool operator()(node v1, node v2) const//不能写引用
+    {
+        return v1.push==v2.push && v1.bi==v2.bi && v1.bj==v2.bj
+               && v1.pi==v2.pi && v1.pj==v2.pj;
+    }
+};
+struct cmp
+{
+    bool operator()(node& a, node& b) const
+    {
+        return a.push > b.push;//推得次数少的优先
+    }
+};
+class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
+public:
+    int minPushBox(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int targetx, targety, boxi, boxj, peoplei, peoplej, push = 0, size;
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 'S')
+                    peoplei = i, peoplej = j;
+                else if(grid[i][j] == 'B')
+                    boxi = i, boxj = j;
+                else if(grid[i][j] == 'T')
+                    targetx = i, targety = j;
+            }
+        }
+        priority_queue<node, vector<node>, cmp> q;
+        node state(0, boxi, boxj, peoplei, peoplej);
+        q.push(state);
+        unordered_set<node, hashf, eqf> vis;
+        vis.insert(state);
+        while(!q.empty())
+        {
+            int pj = q.top().pj;
+            int pi = q.top().pi;
+            int bj = q.top().bj;
+            int bi = q.top().bi;
+            push = q.top().push;
+            if(bi==targetx && bj==targety)
+                return push;
+            q.pop();
+            int nextbi, nextbj, nextpi, nextpj, pushPosx, pushPosy;
+            for(int d = 0; d < 4; d++)
+            {
+                nextbi = bi+dir[d][0];
+                nextbj = bj+dir[d][1];
+                if(nextbi<0 || nextbi>=m || nextbj<0 || nextbj>=n || grid[nextbi][nextbj] == '#')
+                    continue; // 箱子下一个位置不合法
+                nextpi = bi;
+                nextpj = bj;
+                pushPosx = bi-dir[d][0];//人推动箱子前的位置
+                pushPosy = bj-dir[d][1];
+                if(pushPosx<0 || pushPosx>=m || pushPosy<0 || pushPosy>=n || grid[pushPosx][pushPosy] == '#')
+                    continue; // 推箱子位置不合法
+                vector<vector<bool>> record(m, vector<bool>(n, false));
+                if(!canReachPushPos(grid,pi,pj,bi,bj,pushPosx,pushPosy,record))
+                    continue;//不能从当前位置到达推箱子位置
+                state.push = push+1;
+                state.bi = nextbi;
+                state.bj = nextbj;
+                state.pi = nextpi;
+                state.pj = nextpj;
+                if(vis.find(state) == vis.end())
+                {
+                    vis.insert(state);
+                    cout << state.push << " ";
+                    cout << state.bi  << " ";
+                    cout << state.bj  << " ";
+                    cout << state.pi  << " ";
+                    cout << state.pj  << endl;
+                    q.push(state);
+                }
+            }
+        }
+        return -1;
+    }
+    bool canReachPushPos(vector<vector<char>>& grid, int x0, int y0, int bi, int bj, int tx, int ty, vector<vector<bool>>& vis)
+    {
+        vis[x0][y0] = true;
+        if(x0==tx && y0==ty) return true;
+        int m = grid.size(), n = grid[0].size();
+        for(int k = 0; k < 4; k++)
+        {
+            int nx = x0+dir[k][0];
+            int ny = y0+dir[k][1];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny] != '#'
+               && !(nx==bi && ny==bj) && !vis[nx][ny])
+            {   // box 也不能穿过
+                if(canReachPushPos(grid,nx, ny,bi,bj,tx,ty, vis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
+
 
 int main()
 {
 
     Solution s;
+    vector<vector<char>> g = {{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},{'#','.','.','.','.','.','.','.','.','.','.','.','#','#','#','#'},{'#','.','#','#','#','#','.','#','#','#','#','.','#','#','#','.'},{'#','.','.','.','.','.','.','#','T','#','.','.','#','#','#','.'},{'#','.','.','.','#','.','.','.','.','.','.','.','#','#','#','.'},{'#','.','.','.','.','.','B','.','.','.','.','.','#','#','#','.'},{'#','.','#','#','#','#','#','#','#','#','#','.','#','#','#','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','.','.','.','.','.','.','.','.'},{'#','.','.','.','.','.','.','.','S','.','.','.','.','.','.','.'},{'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}};
     vector<int> a = {4,2,4,8,2}, b = {5,5,5,10,8}, c={1,2,8,10,4};
     string s1 = "D450";
-    cout << s.jobScheduling(a,b,c) << endl;
+    cout << s.minPushBox(g) << endl;
 
 
     return 0;
--- algorithm/leetcode1263.cpp
+++ algorithm/leetcode1263.cpp
@@ -1,122 +1,117 @@
 class node
 {
 public:
-	int push, bi, bj, pi, pj;
-	node(int n, int a, int b, int c, int d)
-	{
-		push = n;
-		bi = a;
-		bj = b;
-		pi = c;
-		pj = d;
-	}
+    int push, bi, bj, pi, pj;
+    node(int n, int a, int b, int c, int d)
+    {
+        push = n;
+        bi = a;
+        bj = b;
+        pi = c;
+        pj = d;
+    }
 };
 struct hashf
 {
-    int operator()(node v) const
+    unsigned long long operator()(node v) const
     {
-        return v.push*pow(21,4)+v.bi*pow(21,3)+v.bj*pow(21,2)+v.pi*21+v.pj;
-    }	
+        return 1ULL*v.bi*pow(21,3)+v.bj*pow(21,2)+v.pi*21+v.pj;
+    }
 };
 struct eqf
 {
-    bool operator()(node& v1, node& v2) const
+    bool operator()(node v1, node v2) const//不能写引用
     {
-        return v1.push==v2.push && v1.bi==v2.bi && v1.bj==v2.bj
-                && v1.pi==v2.pi && v1.pj==v2.pj;
-    }	
+        return v1.bi==v2.bi && v1.bj==v2.bj
+               && v1.pi==v2.pi && v1.pj==v2.pj;//2个物品的位置都相等即set中出现过了
+    }
 };
 struct cmp
 {
-	bool operator()(node& a, node& b) const
-	{
-		return a.push > b.push;//推得次数少的优先
-	}
-};
-class Solution {
-public:
-    int minPushBox(vector<vector<char>>& grid) {
-    	int m = grid.size(), n = grid[0].size(), k = 21;
-    	int targetx, targety, boxi, boxj, peoplei, peoplej, push = 0, size;
-    	for(int i = 0; i < m; i++) 
-    	{
-    		for(int j = 0; j < n; j++)
-    		{
-    			if(grid[i][j] == 'S')
-    				peoplei = i, peoplej = j;
-    			else if(grid[i][j] == 'B')
-    				boxi = i, boxj = j;
-    			else if(grid[i][j] == 'T')
-    				targetx = i, targety = j;
-    		}
-    	}
-    	vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
-    	priority_queue<node, vector<node>, cmp> q; //push次数, box 和 people 的状态压成 4位 32 进制的 int
-    	node state(0, boxi, boxj, peoplei, peoplej);
-    	q.push(state);
-    	unordered_set<node, hashf, eqf> vis;
-    	vis.insert(state);
-    	while(!q.empty())
-    	{
-    		int pj = q.top().pj;
-    		int pi = q.top().pi;
-    		int bj = q.top().bj;
-    		int bi = q.top().bi;
-    		push = q.top().push;
-    		if(bi==targetx && bj==targety)
-    			return push;
-    		q.pop();
-    		int disi = bi-pi;
-    		int disj = bj-pj;
-    		int nextbi, nextbj, nextpi, nextpj;
-    		if(abs(disi)+abs(disj)==1)//人和箱子贴着了
-    		{
-    			nextbi = bi+disi;
-    			nextbj = bj+disj;
-    			state.bi = nextbi;
-    			state.bj = nextbj;
-    			state.pi = bi;
-    			state.pj = bj;
-    			state.push = push+1;
-    			if(grid[nextbi][nextbj] != '#' && vis.find(state) == vis.end())
-    			{	//box下一个位置不为墙
-    				vis.insert(state);
-    				q.push(state);
-    			}
-    		}
-    		else// 人和箱子没有接触
-    		{
-    			for(int d = 0; d < 4; d++)
-    			{
-    				nextpi = pi+dir[d][0];
-    				nextpj = pj+dir[d][1];
-    				state.bi = bi;
-	    			state.bj = bj;
-	    			state.pi = nextpi;
-	    			state.pj = nextpj;
-	    			state.push = push;
-    				if(grid[nextpi][nextpj] != '#' && vis.find(state) == vis.end())
-    				{
-    					vis.insert(state);
-    					q.push(state);
-    				}
-    			}
-    		}
-    	}
-    	return -1;
+    bool operator()(node& a, node& b) const
+    {
+        return a.push > b.push;//推得次数少的优先
     }
 };
-
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#",".","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#","#","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T",".",".","#","#"],["#",".","#","B",".","#"],["#",".",".",".",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#","#"],["#","S","#",".","B","T","#"],["#","#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#",".","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#","#","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T",".",".","#","#"],["#",".","#","B",".","#"],["#",".",".",".",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#","#"],["#","S","#",".","B","T","#"],["#","#","#","#","#","#","#"]]
-
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#",".","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T","#","#","#","#"],["#",".",".","B",".","#"],["#","#","#","#",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#"],["#","T",".",".","#","#"],["#",".","#","B",".","#"],["#",".",".",".",".","#"],["#",".",".",".","S","#"],["#","#","#","#","#","#"]]
-// [["#","#","#","#","#","#","#"],["#","S","#",".","B","T","#"],["#","#","#","#","#","#","#"]]
+class Solution {
+    vector<vector<int>> dir = {{1,0},{0,1},{0,-1},{-1,0}};
+public:
+    int minPushBox(vector<vector<char>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int targetx, targety, boxi, boxj, peoplei, peoplej, push = 0, size;
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 'S')
+                    peoplei = i, peoplej = j;
+                else if(grid[i][j] == 'B')
+                    boxi = i, boxj = j;
+                else if(grid[i][j] == 'T')
+                    targetx = i, targety = j;
+            }
+        }
+        priority_queue<node, vector<node>, cmp> q; 
+        node state(0, boxi, boxj, peoplei, peoplej);
+        q.push(state);
+        unordered_set<node, hashf, eqf> vis;
+        vis.insert(state);
+        while(!q.empty())
+        {
+            int pj = q.top().pj;
+            int pi = q.top().pi;
+            int bj = q.top().bj;
+            int bi = q.top().bi;
+            push = q.top().push;
+            if(bi==targetx && bj==targety)
+                return push;
+            q.pop();
+            int nextbi, nextbj, nextpi, nextpj, pushPosx, pushPosy;
+            for(int d = 0; d < 4; d++)
+            {
+                nextbi = bi+dir[d][0];
+                nextbj = bj+dir[d][1];
+                if(nextbi<0 || nextbi>=m || nextbj<0 || nextbj>=n || grid[nextbi][nextbj] == '#')
+                    continue; // 箱子下一个位置不合法
+                nextpi = bi;
+                nextpj = bj;
+                pushPosx = bi-dir[d][0];//人推动箱子前的位置
+                pushPosy = bj-dir[d][1];
+                if(pushPosx<0 || pushPosx>=m || pushPosy<0 || pushPosy>=n || grid[pushPosx][pushPosy] == '#')
+                    continue; // 推箱子位置不合法
+                vector<vector<bool>> record(m, vector<bool>(n, false));
+                if(!canReachPushPos(grid,pi,pj,bi,bj,pushPosx,pushPosy,record))
+                    continue;//不能从当前位置到达推箱子位置
+                state.push = push+1;
+                state.bi = nextbi;
+                state.bj = nextbj;
+                state.pi = nextpi;
+                state.pj = nextpj;
+                if(vis.find(state) == vis.end())
+                {
+                    vis.insert(state);
+                    q.push(state);
+                }
+            }
+        }
+        return -1;
+    }
+    bool canReachPushPos(vector<vector<char>>& grid, int x0, int y0, int bi, int bj, int tx, int ty, vector<vector<bool>>& vis)
+    {
+        vis[x0][y0] = true;
+        if(x0==tx && y0==ty) return true;
+        int m = grid.size(), n = grid[0].size();
+        for(int k = 0; k < 4; k++)
+        {
+            int nx = x0+dir[k][0];
+            int ny = y0+dir[k][1];
+            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny] != '#' 
+                && !(nx==bi && ny==bj) && !vis[nx][ny])
+            {   // box 也不能穿过
+                if(canReachPushPos(grid,nx, ny,bi,bj,tx,ty, vis))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
