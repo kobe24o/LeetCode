c16f3acfd74f73ff9a3a5b95899f5ca6998ee3f6
kobe_24o <mingmingchen1989@163.com>
2021-02-21 20:56:47 +0800

    Create leetcode5687.cpp

---
+++ algorithm/leetcode5687.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        int n = nums.size(), m = multipliers.size();
+        typedef long long LL;
+        vector<vector<LL>> dp(m+1, vector<LL>(m+2, INT_MIN));
+        dp[0][1] = 0;
+        for(int len = 1; len <= m; len++)//乘了多少数字了
+        {
+            for(int i = 1; i <= len; i++)//剩余数字区间的左端点
+            {
+                int j = n+i-len-1;
+                dp[len][i+1] = max(dp[len][i+1], dp[len-1][i]+1LL*multipliers[len-1]*nums[i-1]);
+                dp[len][i] = max(dp[len][i], dp[len-1][i]+1LL*multipliers[len-1]*nums[j]);
+            }
+        }
+        LL ans = INT_MIN;
+        for(int i = 1; i <= m+1; i++)
+            ans = max(ans, dp[m][i]);
+        return ans;
+    }
+};
