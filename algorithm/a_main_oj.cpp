4a6f792bddab9b9b1f2e38b5e118da01d02657f3
kobe24o <mingmingchen1989@163.com>
2021-04-23 21:40:28 +0800

    Update a_main_oj.cpp

--- algorithm/a_main_oj.cpp
+++ algorithm/a_main_oj.cpp
@@ -12,30 +12,59 @@ void print2Dvector(vector<vector<int>>& a)
     }
 }
 
-class Solution {
-public:
-    int getXORSum(vector<int>& arr1, vector<int>& arr2) {
-        int n1 = arr1.size(), n2 = arr2.size();
-        int ans = 0;
-        for(int i = 0; i < n1; i++) {
-            for(int j = 0; j < n2; j++) {
-                ans ^= arr1[i]&arr2[j];
-            }
+void calNexts(char *b, int m, vector<int>& next)
+{
+    next[0] = -1;
+    int j = 0, k = -1;
+    while(j < m)
+    {
+        if(k == -1 || b[j] == b[k])
+        {
+            j++;k++;
+            next[j] = k;
         }
-        return ans;
+        else
+            k = next[k];
     }
-};
-
+    //    for(j = 0; j < m; ++j)//调试代码
+    //        cout << "next[" << j << "] " << next[j] << endl;
+}
+int str_kmp(char *a, int n, char *b, int m)
+{
+    vector<int> next(m);
+    calNexts(b, m, next);
+    int i = 0, j = 0;
+    while(i < n && j < m)
+    {
+        if(j == -1 || a[i] == b[j])
+        {
+            i++;j++;
+        }
+        else
+        {
+            j = next[j];
+        }
+    }
+    if(j == m)
+    {
+        return i-j;
+    }
+    return -1;
+}
 
 int main()
 {
 
-    Solution s;
+//    Solution s;
     vector<vector<int>> g = {{35,36},{11,7},{15,47},{34,2},{47,19},{16,14},{19,8},{7,34},{38,15},{16,18},{27,22},{7,15},{43,2},{10,5},{5,4},{3,11}};
-    vector<int> a = {1,2,3, 3}, b = {6,5};
+    vector<int> c = {1,2,3, 3}, d = {6,5};
     string str = "9";
     string s1 = "guguuuuuuuuuuuuuuguguuuuguug",
            s2=     "gguggggggguuggguugggggg";
-    cout << s.getXORSum(a,b) << endl;
+//    cout << s.getXORSum(a,b) << endl;
+
+    string a = "hello", b = "ll";
+    int p = str_kmp(&a[0],a.size(),&b[0],b.size());
+    cout << a << " find " << b << " at ： " << p << endl;
     return 0;
 }
